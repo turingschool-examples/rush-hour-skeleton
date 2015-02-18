17fcb7d32218ff778aecd2f302103bfe5e9e5642
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-02-18 13:41:13 -0700

    first passing test

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -7,5 +7,11 @@ module TrafficSpy
     not_found do
       erb :error
     end
+
+    post '/sources' do
+    	User.create(params[:sources])
+    	body 'success'
+    end
+
   end
 end
--- db/migrate/20150218184014_create_user_table.rb
+++ db/migrate/20150218184014_create_user_table.rb
@@ -3,5 +3,6 @@ class CreateUserTable < ActiveRecord::Migration
   	create_table :users do |t|
   		t.text :identifier
   		t.text :rootUrl
+  	end
   end
 end
