cd214a799e0cf410b3ed69a7917c302b2290582f
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-02-18 11:27:04 -0700

    created first test

--- test/controllers/create_user_test.rb
+++ test/controllers/create_user_test.rb
@@ -10,3 +10,12 @@ class CreateUserTest < Minitest::Test
   def teardown
     DatabaseCleaner.clean
   end
+
+
+  def test_it_creates_user
+  	skip
+  	post '/sources', { sources: { identifier: 'jumpstartlab',
+  		                            rootUrl: 'http://jumpstartlab.com'}}
+  end
+
+end
