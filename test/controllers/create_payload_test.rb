749ffece31d2c754cba1195f2ff60c7968610a74
Kyra Weber <kyra.m.weber@gmail.com>
2015-02-19 16:03:13 -0700

    created payload migration

--- test/controllers/create_payload_test.rb
+++ test/controllers/create_payload_test.rb
@@ -19,13 +19,40 @@ class CreatePayloadTest < MiniTest::Test
     DatabaseCleaner.clean
   end
 
+  def register_app
+    post "/sources", identifier
+  end
+
   def test_create_payload_has_correct_parameters
-  	post "/sources", identifier 
+    register_app
   	assert_equal 200, last_response.status
 
-  	post "/sources/IDENTIFIER/data", payload
+  	post "/sources/jumpstartlab/data", payload
   	assert_equal 200, last_response.status
   	assert_equal payload.source_id, source.id
   end
-  	
+
+  def test_request_with_missing_payload_returns_error
+    skip
+    post "/sources/jumpstartlab/data", '" " http://localhost:9393/sources/jumpstartlab/data'
+    assert equal 400, last_response.status
+    assert "missing payload", last_response.body.include?
+  end
+
+  def test_duplicated_request_returns_error
+    skip
+    post "/sources/jumpstartlab/data", payload
+    assert_equal 200, last_response.status
+    post "/sources/jumpstartlab/data", payload
+    assert_equal 403, last_response.status
+    assert_equal "duplicate request", last_response.body
+  end
+
+  def test_unregistered_application_cannot_request_data
+    skip
+    post "/sources/kyrasapp/data", payload={}
+    assert_equal 403, last_response.status
+    assert "not registered application", last_response.body.include?
+  end
+
  end
