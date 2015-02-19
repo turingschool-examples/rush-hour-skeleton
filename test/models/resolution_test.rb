f5d49f37df1602aa8fa493416febb5aee4212003
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-02-19 10:21:55 -0700

    created method for refactoring resolution model test

--- test/models/resolution_test.rb
+++ test/models/resolution_test.rb
@@ -9,6 +9,20 @@ class ResolutionTest < Minitest::Test
 		DatabaseCleaner.clean
 	end
 
+	def create_four_resolutions
+		Resolution.create({ 'resolutionWidth'  => '1920',
+	    									'resolutionHeight' => "1280" })
+
+		Resolution.create({ 'resolutionWidth'  => '1980',
+	    									'resolutionHeight' => "12100" })
+
+		Resolution.create({ 'resolutionWidth'  => '720',
+	    									'resolutionHeight' => "1080" })
+
+		Resolution.create({ 'resolutionWidth'  => '1924',
+	    									'resolutionHeight' => "9999" })
+	end
+
 	def test_it_is_a_valid_creation_with_both_resolution_values
 			resolution = Resolution.create({ 'resolutionWidth'  => '1920',
 	    													 	  'resolutionHeight' => "1280" })
@@ -17,17 +31,17 @@ class ResolutionTest < Minitest::Test
 	end
 
 	def test_it_is_invalid_when_missing_one_value
-		resolution_true = Resolution.create({ 'resolutionWidth'  => '1920',
-	    													 	  'resolutionHeight' => "1280" })
-		assert_equal 1,  Resolution.count
+		create_four_resolutions
+		assert_equal 4,  Resolution.count
 
 		resolution1 = resolution = Resolution.new({ 'resolutionWidth'  => '1920'})
-		assert_equal 1, Resolution.count
+		assert_equal 4, Resolution.count
 		refute resolution1.valid?
 
 		resolution2 = resolution = Resolution.new({ 'resolutionHeight' => "1280" })
-		assert_equal 1, Resolution.count
+		assert_equal 4, Resolution.count
 		refute resolution2.valid?
 	end
+
 
 end
