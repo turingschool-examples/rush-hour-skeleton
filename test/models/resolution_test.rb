dd701de6f57b449d1522d16bd6c23361dbe660e1
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-02-19 10:16:04 -0700

    added test for screen resolution test needs to be refactored so that models can be more accurately tested

--- test/models/resolution_test.rb
+++ test/models/resolution_test.rb
@@ -9,11 +9,25 @@ class ResolutionTest < Minitest::Test
 		DatabaseCleaner.clean
 	end
 
-	def test_it_is_valid_with_both_resolution_values
-			resolution = Resolution.new({ 'resolutionWidth'  => '1920',
+	def test_it_is_a_valid_creation_with_both_resolution_values
+			resolution = Resolution.create({ 'resolutionWidth'  => '1920',
 	    													 	  'resolutionHeight' => "1280" })
 			assert resolution.valid?
+			assert_equal 1, Resolution.count
 	end
 
+	def test_it_is_invalid_when_missing_one_value
+		resolution_true = Resolution.create({ 'resolutionWidth'  => '1920',
+	    													 	  'resolutionHeight' => "1280" })
+		assert_equal 1,  Resolution.count
+
+		resolution1 = resolution = Resolution.new({ 'resolutionWidth'  => '1920'})
+		assert_equal 1, Resolution.count
+		refute resolution1.valid?
+
+		resolution2 = resolution = Resolution.new({ 'resolutionHeight' => "1280" })
+		assert_equal 1, Resolution.count
+		refute resolution2.valid?
+	end
 
 end
