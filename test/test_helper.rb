fee052434b5893d0a88c01b4cfd66f3bd56633d8
Kyra Weber <kyra.m.weber@gmail.com>
2015-02-19 13:26:48 -0700

    fixed merge conflict in test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,8 +8,4 @@ require "capybara"
 Capybara.app = TrafficSpy::Server
 
 require "database_cleaner"
-<<<<<<< HEAD
 DatabaseCleaner.strategy = :truncation, { except: %w[public.schema_migrations] }
-=======
-DatabaseCleaner.strategy = :truncation, { :except => %w[public.schema_migrations]}
->>>>>>> 8f47a150a1f92a41dac4472091fc018005408d20
