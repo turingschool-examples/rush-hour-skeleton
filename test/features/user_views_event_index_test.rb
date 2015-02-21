611847f81cfd8f43d75692adf7a9c944a4e566cf
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-02-21 16:20:09 -0700

    added test for hyperlinks
    Next up, pass these tests. Make methods and views to support the events index

--- test/features/user_views_event_index_test.rb
+++ test/features/user_views_event_index_test.rb
@@ -2,14 +2,24 @@ require './test/test_helper'
 
 class UserViewsApplicationDetailsTest < FeatureTest
 
-  def test_the_page_displays_the_most_requested_URLS_to_least_requested_URLS
+  def test_the_page_displays_the_most_received_events_to_least_received_events
     visit 'http://yourapplication:port/sources/jumpstartlab/events'
     assert_equal '/sources/jumpstartlab/events', current_path
     within('#event-index') do
-      assert page.has_content?('Defined Events:')
+      assert page.has_content?('Events:')
    end
   end
-  
+
+  def test_there_are_hyperlinks_of_each_event_to_display_event_specific_data
+    visit 'http://yourapplication:port/sources/jumpstartlab/events'
+    assert_equal '/sources/jumpstartlab/events', current_path
+    within('#event-index') do
+      click_link_or_button("Event-'name'")
+      assert_equal "/sources/jumpstartlab/events/'name'"
+   end
+
+  end
+
 # As a client
 # When I visit http://yourapplication:port/sources/IDENTIFIER/events
 # And events have been defined
