20371c626d99ddba98766011ba23fb7aa248900c
Lori Culberson <bigskybandit@sbcglobal.net>
2015-02-21 09:17:31 -0700

    WIP event index. feature test and event_index.erb started

---
+++ test/features/event_index_test.rb
@@ -0,0 +1,21 @@
+require "./test/test_helper"
+
+class EventIndexTest < FeatureTest
+  #After I click on the event index link
+  #then I see a list of the events with the number of occurrences
+  def test_user_sees_events_page_greeting
+  	visit "/sources/jumpstartlab/events"
+    within('#events-detail-hour') do
+      assert page.has_content?("Event Index")
+    end
+  end 
+   
+   def test_user_sees_index_and_links_to_details
+    visit "/sources/jumpstartlab/events"
+    within('#events-detail-overall') do
+      assert page.has_link?("Task Index")
+      assert page.has_link?("New Task")
+    end
+  end 
+
+end
