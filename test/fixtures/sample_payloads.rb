035886cd0c55cec55852f08e1f705e5e6cd30613
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-02-21 20:18:02 -0700

    created basic test fixture created a test fixture for mocking payload traffic in the feature tests

---
+++ test/fixtures/sample_payloads.rb
@@ -0,0 +1,60 @@
+module Payload
+
+  PAYLOAD1 = {"url"=>"http://jumpstartlab.com/blog",
+           "requestedAt"=>"2013-02-16 21:38:28 -0700",
+           "respondedIn"=>37,
+           "referredBy"=>"http://jumpstartlab.com",
+           "requestType"=>"GET",
+           "parameters"=>[],
+           "eventName"=>"socialLogin",
+           "userAgent"=>
+           "Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+           "resolutionWidth"=>"1920",
+           "resolutionHeight"=>"1280",
+           "ip"=>"63.29.38.211"
+           }
+
+  PAYLOAD2 = {"url"=>"http://jumpstartlab.com/tutorials",
+           "requestedAt"=>"2013-02-16 21:38:28 -0700",
+           "respondedIn"=>45,
+           "referredBy"=>"http://google.com",
+           "requestType"=>"GET",
+           "parameters"=>[],
+           "eventName"=>"otherEvent",
+           "userAgent"=>
+           "Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_5_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+           "resolutionWidth"=>"1500",
+           "resolutionHeight"=>"1300",
+           "ip"=>"63.29.38.211"
+           }
+
+
+   PAYLOAD3 = {"url"=>"http://jumpstartlab.com/tutorials",
+           "requestedAt"=>"2013-02-16 21:38:28 -0700",
+           "respondedIn"=>12,
+           "referredBy"=>"http://github.com",
+           "requestType"=>"POST",
+           "parameters"=>[],
+           "eventName"=>"eventName",
+           "userAgent"=>
+           "Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_5_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+           "resolutionWidth"=>"1500",
+           "resolutionHeight"=>"1300",
+           "ip"=>"63.22.38.128"
+           }
+
+  PAYLOAD4 = {"url"=>"http://jumpstartlab.com/help",
+           "requestedAt"=>"2013-02-16 21:38:28 -0700",
+           "respondedIn"=>92,
+           "referredBy"=>"http://bing.com",
+           "requestType"=>"POST",
+           "parameters"=>[],
+           "eventName"=>"addUser",
+           "userAgent"=>
+           "Mozilla/4.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17"
+           "resolutionWidth"=>"1500",
+           "resolutionHeight"=>"1900",
+           "ip"=>"61.99.18.911"
+           }
+
+end
