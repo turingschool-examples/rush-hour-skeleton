e17149d0f43abfe979406fdd70f0dae9475f53e2
Austin Wood <i@austinwood.me>
2015-02-22 11:51:11 -0700

    Move feature test payload creation to test helper

--- test/features/url_stats_test.rb
+++ test/features/url_stats_test.rb
@@ -3,12 +3,7 @@ require "./test/test_helper"
 class EventIndexTest < FeatureTest
 
   def setup
-    Source.create({ identifier: "jumpstartlab",
-                    root_url: "http://jumpstartlab.com" })
-    payload_params = '{"url":"http://jumpstartlab.com/contact","requestedAt":"2013-02-16 21:38:28 -0700","respondedIn":37,"referredBy":"http://jumpstartlab.com","requestType":"GET","parameters":[],"eventName": "socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.38.211"}'
-    # require 'pry';binding.pry
-    # params = JSON.parse(payload_params).symbolize_keys
-    PayloadGenerator.call(payload_params, 'jumpstartlab')
+    register_user_and_create_payload
   end
 
   def test_user_sees_response_times
