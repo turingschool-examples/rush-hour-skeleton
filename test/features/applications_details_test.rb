6e1791cfd13e7cff89d0d5ae97df059ef2b5693b
Jeffrey Gu <jeffwgu@gmail.com>
2015-02-21 19:20:03 -0700

    deleted comments

--- test/features/applications_details_test.rb
+++ test/features/applications_details_test.rb
@@ -64,12 +64,6 @@ class ApplicationDetailsTest < Minitest::Test
     assert page.find_link 'yahoo.com/weather'
   end
 
-  #And I should see a Hyperlinks of each url to view url specific data And I should see a Hyperlink to view aggregate event data
-
-  #As a CLIENT
-  #When I visit http://yourapplication:port/sources/IDENTIFIER
-  #And an identifer does not exist for that client
-  #Then I should see the following message ""
   def setup_yahoo
     @payload_1 = 'payload={"url":"http://yahoo.com/weather","requestedAt":"2013-01-13 21:38:28 -0700","respondedIn":37,"referredBy":"http://apple.com","requestType":"GET","parameters":["what","huh"],"eventName": "socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"800","resolutionHeight":"600","ip":"63.29.38.214"}'
     @payload_2 = 'payload={"url":"http://yahoo.com/weather","requestedAt":"2013-01-13 22:38:28 -0700","respondedIn":37,"referredBy":"http://jumpstartlab.com","requestType":"GET","parameters":["what","huh"],"eventName": "beginRegistration","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"500","resolutionHeight":"700","ip":"63.29.38.214"}'
