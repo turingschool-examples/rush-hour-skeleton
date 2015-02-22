bc7d8767fbaefa6f572f4e8f2e6fa6b3078603ec
Teresa Finn <teresa.finn@gmail.com>
2015-02-21 21:38:18 -0700

    Changed :url to :page in url test because test wasnt passing
    Url test is now passing with change. Also had to RACK_ENV=test rake db:drop db:create db:migrate in order to get the database to sync properly again.

--- test/models/url_test.rb
+++ test/models/url_test.rb
@@ -7,7 +7,7 @@ class UrlTest < Minitest::Test
   end
 
   def test_it_has_a_url
-    url = Url.create({ :url => "http://jumpstartlab.com/blog" })
+    url = Url.create({ :page => "http://jumpstartlab.com/blog" })
     assert url.page
   end
 
