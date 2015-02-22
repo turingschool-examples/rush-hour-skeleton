1e5b73b10f4fe47ca8625422ca76d2aead598d12
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-02-21 18:22:48 -0700

    changed the source urls in the tests to reflect the product.

--- test/features/user_views_application_details_test.rb
+++ test/features/user_views_application_details_test.rb
@@ -3,7 +3,7 @@ require './test/test_helper'
 class UserViewsApplicationDetailsTest < FeatureTest
 
   def test_the_page_displays_the_most_requested_URLS_to_least_requested_URLS
-    visit 'http://yourapplication:port/sources/jumpstartlab'
+    visit 'http://localhost:5432/sources/jumpstartlab'
     assert_equal '/sources/jumpstartlab', current_path
     within('#urls') do
       assert page.has_content?('http://jumpstartlab.com')
@@ -12,7 +12,7 @@ class UserViewsApplicationDetailsTest < FeatureTest
 
   def test_the_page_displays_web_browser_breakdown_across_all_requests
     skip
-    visit 'http://yourapplication:port/sources/jumpstartlab'
+    visit 'http://localhost:5432/sources/jumpstartlab'
     assert_equal '/sources/jumpstartlab', current_path
     within('#browser-breakdown') do
       assert page.has_content?('Chrome')
@@ -21,7 +21,7 @@ class UserViewsApplicationDetailsTest < FeatureTest
 
   def test_page_displays_an_OS_breakdown_across_all_requests
     skip
-    visit 'http://yourapplication:port/sources/jumpstartlab'
+    visit 'http://localhost:5432/sources/jumpstartlab'
     assert_equal '/sources/jumpstartlab', current_path
     within('#OS-breakdown') do
       assert page.has_content?('Macintosh')
@@ -30,7 +30,7 @@ class UserViewsApplicationDetailsTest < FeatureTest
 
   def test_page_displays_screen_resolutions_across_all_requests
     skip
-    visit 'http://yourapplication:port/sources/jumpstartlab'
+    visit 'http://localhost:5432/sources/jumpstartlab'
     assert_equal '/sources/jumpstartlab', current_path
     within('#screen-resolutions') do
       assert page.has_content?('1920 x 1080')
@@ -39,7 +39,7 @@ class UserViewsApplicationDetailsTest < FeatureTest
 
   def test_page_displays_longest_average_response_time_per_URL_to_shortest_average_response_time_per_URL
     skip
-    visit 'http://yourapplication:port/sources/jumpstartlab'
+    visit 'http://localhost:5432/sources/jumpstartlab'
     assert_equal '/sources/jumpstartlab', current_path
     within('#response-times') do
       assert page.has_content?('37, http://jumpstartlab.com')
@@ -48,7 +48,7 @@ class UserViewsApplicationDetailsTest < FeatureTest
 
   def test_page_displays_hyperlinks_of_each_url_to_view_url_specific_data
     skip
-    visit 'http://yourapplication:port/sources/jumpstartlab'
+    visit 'http://localhost:5432/sources/jumpstartlab'
     assert_equal '/sources/jumpstartlab', current_path
     within('#url_links') do
       assert page.has_content?('http://jumpstartlab.com/blog')
@@ -57,7 +57,7 @@ class UserViewsApplicationDetailsTest < FeatureTest
 
   def test_page_displays_hyperlink_to_view_aggregate_event_data
     skip
-    visit 'http://yourapplication:port/sources/jumpstartlab'
+    visit 'http://localhost:5432/sources/jumpstartlab'
     assert_equal '/sources/jumpstartlab', current_path
     within('#event_links') do
       assert page.has_content?('http://jumpstartlab.com/events')
@@ -66,7 +66,7 @@ class UserViewsApplicationDetailsTest < FeatureTest
 
   def test_page_displays_message_when_identifier_does_not_exist
     skip
-    visit 'http://yourapplication:port/sources/jumpstartlab'
+    visit 'http://localhost:5432/sources/jumpstartlab'
     assert_equal '/sources/jumpstartlab', current_path
     assert page.has_content?('You havent registered.')
   end
