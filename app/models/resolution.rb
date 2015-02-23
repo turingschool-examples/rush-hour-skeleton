7de571e1e5e2c19dc5c277e25af22ace1b188816
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-02-22 22:39:22 -0700

    Updated models with correct relationships.
    We had the relationships ass-backwards. Next step creating migrations to correct the database.

--- app/models/resolution.rb
+++ app/models/resolution.rb
@@ -1,5 +1,6 @@
 class Resolution < ActiveRecord::Base
 
+	has_many :payloads
 	validates :resolutionWidth, presence: true
 	validates :resolutionHeight, presence: true
 	validates_uniqueness_of :resolutionHeight, :scope => :resolutionWidth
