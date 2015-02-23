2dd73733cc1c1c65d8b8c20b8a5b2e7d072613c3
Teresa Finn <teresa.finn@gmail.com>
2015-02-22 22:50:15 -0700

    Changed the order_by_instances method
    Was getting a false positive in the user model tests, which became apparent when I reordered the url fixtures. Added a reverse so that it puts them out in order from most to least.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,26 @@ class User < ActiveRecord::Base
   def ordered_urls
     # 1. find all urls for user
     # 2. order them from most to least visited.
-		binding.pry
     urls = self.payloads.collect(&:urls).flatten.collect(&:page)
     order_by_instances(urls)
   end
 
   def order_by_instances(urls)
-    urls.each_with_object(Hash.new(0)) { |urls,counts| counts[urls] += 1 }.sort_by{ |k,v| v }.collect(&:first)
+    urls.each_with_object(Hash.new(0)) { |urls,counts| counts[urls] += 1 }.sort_by{ |_k,v| v }.reverse.collect(&:first)
   end
+
+
+  def browsers
+    # This method will return an array of arrays with 3 elements in each array: 1) the browser, 2) the number of requests received from this browser, and 3) percentage of total requests received from this browser.
+
+
+  end
+
+  def operating_system
+    # This method will return an array of arrays with 3 elements in each array: 1) the operating system, 2) the number of requests received from this operating system, and 3) percentage of total requests received from this operating system.
+
+  end
+
+
 
 end
