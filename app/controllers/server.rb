719776e93a324b295d826b7a517925f9c1e7a000
Tess Griffin <tessjgriffin@gmail.com>
2015-04-08 14:07:46 -0600

    Adds payload helper model

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -27,23 +27,9 @@ module TrafficSpy
 
     post '/sources/:identifier/data' do |identifier|
       payload_hash = JSON.parse(params[:payload])
-      if Source.find_by(identifier: identifier) != nil
-        payload = Payload.new(
-          url: payload_hash["url"],
-          requested_at: payload_hash["requestedAt"],
-          responded_in: payload_hash["respondedIn"],
-          referred_by: payload_hash["referredBy"],
-          request_type: payload_hash["requestType"],
-          parameters: payload_hash["parameters"],
-          event_name: payload_hash["eventName"],
-          user_agent: payload_hash["userAgent"],
-          resolution_width: payload_hash["resolutionWidth"],
-          resolution_height: payload_hash["resolutionHeight"],
-          id: payload_hash["id"],
-          source_id: Source.find_by(identifier: identifier).id 
-          )
-        if Payload.find_by(requested_at: payload.requested_at) &&
-           Payload.find_by(source_id: payload.source_id)
+      if PayloadHelper.source_exists?(identifier)
+        payload = PayloadHelper.add_payload(identifier, payload_hash)
+        if PayloadHelper.duplicate_source?(payload)
           status 403
           body "Already Received Request"
         elsif payload.save
---
+++ app/models/payload_helper.rb
@@ -0,0 +1,29 @@
+class PayloadHelper
+
+  def self.source_exists?(identifier)
+    Source.exists?(identifier: identifier)
+  end
+
+  def self.duplicate_source?(payload)
+    Payload.exists?(requested_at: payload.requested_at) &&
+    Payload.exists?(source_id: payload.source_id)
+  end
+
+  def self.add_payload(identifier, payload_hash)
+    Payload.new(
+          url: payload_hash["url"],
+          requested_at: payload_hash["requestedAt"],
+          responded_in: payload_hash["respondedIn"],
+          referred_by: payload_hash["referredBy"],
+          request_type: payload_hash["requestType"],
+          parameters: payload_hash["parameters"],
+          event_name: payload_hash["eventName"],
+          user_agent: payload_hash["userAgent"],
+          resolution_width: payload_hash["resolutionWidth"],
+          resolution_height: payload_hash["resolutionHeight"],
+          id: payload_hash["id"],
+          source_id: Source.find_by(identifier: identifier).id 
+          )
+  end
+
+end
