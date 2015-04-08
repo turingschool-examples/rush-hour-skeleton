39e93450632d57d571f51842a49b9d3c49a4b0ec
Carmer <carmerandrew@gmail.com>
2015-04-08 15:48:51 -0600

    finished query for most visited urls.

--- app/models/application_details.rb
+++ app/models/application_details.rb
@@ -1,8 +1,10 @@
 class ApplicationDetails < ActiveRecord::Base
 
-  def self.sort_tracked_urls
-    tracked_site_ids = Payload.group("tracked_site_id").order("count_tracked_site_id desc").count("tracked_site_id").keys
-    
+  def self.sort_tracked_sites
+    tracked_site_id_list = Payload.group("tracked_site_id").order("count_tracked_site_id desc").count("tracked_site_id").keys
+    tracked_site_id_list.map do |id|
+      TrackedSite.find(id).url
+    end
   end
 
 end
