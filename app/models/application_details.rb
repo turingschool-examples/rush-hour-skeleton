5db3af56cc6b7aabe86e75cf4d80d3a1a4e80305
Carmer <carmerandrew@gmail.com>
2015-04-08 15:42:40 -0600

    active record tracked-site-order.

--- app/models/application_details.rb
+++ app/models/application_details.rb
@@ -1,7 +1,8 @@
 class ApplicationDetails < ActiveRecord::Base
 
   def self.sort_tracked_urls
-    Payload.order(tracked_site_id: :desc)
+    tracked_site_ids = Payload.group("tracked_site_id").order("count_tracked_site_id desc").count("tracked_site_id").keys
+    
   end
 
 end
