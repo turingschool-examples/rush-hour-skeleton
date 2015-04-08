cba65acbbb18bd7c659e0e70bd773e5854002f24
Jack Yeh <jack.yeh@gmail.com>
2015-04-08 17:19:03 -0600

    test payload is not valid when nil passes

--- app/models/payload_parser.rb
+++ app/models/payload_parser.rb
@@ -1,3 +1,37 @@
 class PayloadParser
-  payload =
+  attr_accessor :data, :message, :status_code
+
+  def initialize(data={})
+    @data = data
+    @message = ""
+    @status_code = ""
+  end
+
+  def valid?
+    if data.empty?
+      message = "Payload is missing or empty"
+      status_code = 400
+    end
+  end
+
+  def parse_payload(data)
+
+    conversion(JSON.parse(data[:payload]))
+    #conversion(JSON.parse(params[data.key]))
+  end
+
+  def conversion(data)
+    new_data = data.keys.map do |key|
+      key.chars.map do |char|
+        if char == char.upcase
+          char = "_" + char.downcase
+        else
+          char
+        end
+      end.join.to_sym
+    end
+    data_keys = new_data
+    data_values = data.values
+    new_table_names = Hash[data_keys.zip(data_values)]
+  end
 end
