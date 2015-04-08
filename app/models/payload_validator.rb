955ecf9fe5efa56cab36b5ad105fe45756fee01f
Carmer <carmerandrew@gmail.com>
2015-04-08 11:15:43 -0600

    Updated payload parse to handle empty hash and no payload.

--- app/models/payload_validator.rb
+++ app/models/payload_validator.rb
@@ -1,14 +1,13 @@
 class PayloadValidator < Payload
-  
-  def self.validate(data) 
+
+  def self.validate(data)
     payload = Payload.new(data)
 
     payload_nil = data.each_value.all? {|value| value.nil?}
-
     if payload_nil
-      
+
       {code: 400, message: "Payload cannot be nil"}
-    elsif payload.save  
+    elsif payload.save
       {code: 200}
     elsif Payload.find_by(composite_key: payload.composite_key)
       errors = payload.errors.full_messages
@@ -19,8 +18,8 @@ class PayloadValidator < Payload
     else
       errors = payload.errors.full_messages
       {code: 400, message: errors}
-    end  
-  
+    end
+
   end
 
-end 
+end
