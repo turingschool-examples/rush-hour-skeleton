e867722fe957083ce1fe92a5761f3222e5e8dc7d
Kenny <chenkenn@gmail.com>
2015-04-09 14:48:05 -0600

    making 403 unique payload request pass

--- app/models/payload.rb
+++ app/models/payload.rb
@@ -1,2 +1,4 @@
 class Payload < ActiveRecord::Base
+  validates :ip, uniqueness: { scope: :requested_at,
+  message: "Application has already been received" }
 end
--- test/models/payload_test.rb
+++ test/models/payload_test.rb
@@ -1,7 +1,16 @@
 require './test/test_helper'
-class PayloadTest < Minitest::Test
-  def test_it_validates_application_url
+require 'byebug'
 
+class PayloadTest < Minitest::Unit::TestCase
+
+  def test_uniqueness_validation_ip_as_requested_at
+    assert_equal 0, Payload.count
+    request1 = Payload.create!({ip: 2, requested_at: "today"})
+    assert_equal 1, Payload.count
+    request2 = Payload.new({ip: 2, requested_at: "today"})
+    assert request2.invalid?
+    assert_equal ["Application has already been received"], request2.errors[:ip]
   end
+
 
 end
