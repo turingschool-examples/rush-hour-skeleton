26e2182aab552e9e25c60a8035557dbde39d60fc
end3rs-army <eric.m.dowty@gmail.com>
2015-04-09 17:21:26 -0600

    the curls work now

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -11,8 +11,78 @@ module TrafficSpy
       body validation[:message]
     end
 
-    post '/sources/:identifier/data' do |identifier|
+    get '/sources' do 
+      # binding.pry
+      cool_guy = "kenney"
+      string = Client.all
+      erb :sources, :locals => {:clients => string, guy: cool_guy}
+    end
 
+    get '/sources/:identifier' do |identifier|
+
+    end
+
+    post '/sources/:identifier' do |identifier|
+
+    end
+
+    get '/sources/registration_error' do 
+
+    end
+
+    post '/sources/registration_error' do 
+
+    end
+
+    get '/sources/:identifier/urls/:path' do |identifier, path|
+
+    end
+
+    post '/sources/:identifier/urls/:path' do |identifier, path|
+
+    end
+
+    get '/sources/:identifier/urls/url_error' do |identifier|
+
+    end
+
+    post '/sources/:identifier/urls/url_error' do |identifier|
+
+    end
+
+    get '/sources/:identifier/events' do |identifier|
+
+    end
+
+    post '/sources/:identifier/events' do |identifier|
+
+    end
+
+    get '/sources/:identifier/no_events_error' do |identifier|
+
+    end
+
+    post '/sources/:identifier/no_events_error' do |identifier|
+
+    end
+
+    get '/sources/:identifier/events/:event_name' do |identifier, event_name|
+
+    end
+
+    post '/sources/:identifier/events/:event_name' do |identifier, event_name|
+
+    end
+
+    get '/sources/:identifier/events/event_name_error' do |identifier|
+
+    end
+
+    post '/sources/:identifier/events/event_name_error' do |identifier|
+
+    end
+
+    post '/sources/:identifier/data' do |identifier|
       @payload = PayloadValidator.validate(params["payload"], identifier)
       status @payload[:code]
       body @payload.fetch(:message, nil)
