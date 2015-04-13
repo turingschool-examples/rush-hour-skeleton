68f17b53bdb2a74090b36a11ddffe4d4c56e1023
Kenny <chenkenn@gmail.com>
2015-04-13 09:45:03 -0600

    logic in progress

--- app/models/source.rb
+++ app/models/source.rb
@@ -18,9 +18,31 @@ module TrafficSpy
       end.sort_by{|value| value[1]}.reverse
     end
 
+
+
     def url_objects
       payloads = Payload.all
       payloads.map{|payload| payload.url}.uniq
+    end
+
+    def screen_resolution_data
+      screen_resolution_ids = self.payloads.pluck(:screen_resolution_id)
+      screen_resolution_ids.map do |id|
+        screens = ScreenResolution.find(id)
+        "#{screens.resolution_height} x #{screens.resolution_width}"
+      end
+    end
+
+    def avg_response_time
+      self.payloads.average(:responded_in)
+    end
+
+    def max_response_time
+      self.payloads.maximum(:responded_in)
+    end
+
+    def min_response_time
+      self.payloads.minimum(:responded_in)
     end
 
     def user_agent_data
