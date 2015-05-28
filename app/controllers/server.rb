6f5a0fd36f438ce34ec7f18f89c70d5b438bb827
justin <justin.ryan.holmes@icloud.com>
2015-05-28 10:57:18 -0600

    lala

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -7,15 +7,15 @@ module TrafficSpy
       erb :index
     end
 
-    not_found do
-      erb :error
-    end
-    
     post '/sources' do
       result = SourceCreator.new
       result.result(params)
       status(result.status)
       body(result.body)
+    end
+
+    not_found do
+      erb :error
     end
   end
 end
