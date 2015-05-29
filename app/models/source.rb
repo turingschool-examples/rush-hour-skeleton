52f74d09124e39f9ad490cad96b115d61d66a56b
Andrew Fink <andrewfink8@gmail.com>
2015-05-29 15:31:59 -0600

    working on displaying data for a source

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,6 +1,13 @@
 module TrafficSpy
   class Source < ActiveRecord::Base
-    validates_presence_of :identifier, :rootUrl
+    validates_presence_of :identifier, :root_url
     validates :identifier, uniqueness: true
+
+
+    def find_urls
+
+    end
+
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150528194000) do
+ActiveRecord::Schema.define(version: 20150529194122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20150528194000) do
 
   create_table "sources", force: :cascade do |t|
     t.string "identifier"
-    t.string "rootUrl"
+    t.string "root_url"
   end
 
 end
