3554d2d3acdd4babc3554c96ff511f8235213766
Morgan Miller <m.miller722@gmail.com>
2015-05-29 16:29:49 -0600

    reformatted processing_payload_test.rb

--- test/controllers/processing_payload_test.rb
+++ test/controllers/processing_payload_test.rb
@@ -7,18 +7,19 @@ class ProcessingPayloadTest < ControllerTest
   end
   
   def create_payload_post
-    post('/sources/jumpstartlab/data', { payload: {:url=>"http://jumpstartlab.com/blog",
-        :requestedAt=>"2013-02-16 21:38:28 -0700",
-        :respondedIn=>37,
-        :referredBy=>"http://jumpstartlab.com",
-        :requestType=>"GET",
-        :parameters=>[],
-        :eventName=>"socialLogin",
-        :userAgent=>
-          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
-        :resolutionWidth=>"1920",
-        :resolutionHeight=>"1280",
-        :ip=>"63.29.38.211"}.to_json } )
+    post('/sources/jumpstartlab/data', { payload: 
+        { :url=>              "http://jumpstartlab.com/blog",
+          :requestedAt=>      "2013-02-16 21:38:28 -0700",
+          :respondedIn=>      37,
+          :referredBy=>       "http://jumpstartlab.com",
+          :requestType=>      "GET",
+          :parameters=>       [],
+          :eventName=>        "socialLogin",
+          :userAgent=>        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+          :resolutionWidth=>  "1920",
+          :resolutionHeight=> "1280",
+          :ip=>               "63.29.38.211"
+        }.to_json } )
   end
   
   def test_processes_unique_payload_for_source
