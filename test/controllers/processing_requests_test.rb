d143e3d6336bdc4870cedda15a199937af3b2751
Rick Bacci <rickbacci@gmail.com>
2015-05-28 23:14:30 -0600

    add test for missing payload

--- test/controllers/processing_requests_test.rb
+++ test/controllers/processing_requests_test.rb
@@ -33,4 +33,10 @@ class ProcessingRequestTest < ControllersTest
     assert_equal 403, last_response.status
     assert_equal "Application not registered", last_response.body
   end
+
+  def test_that_no_payload_is_not_parsed
+    post "/sources/jumpstartlab/data" 
+    assert_equal 400, last_response.status
+    assert_equal "Payload missing", last_response.body
+  end
 end
