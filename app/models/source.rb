98b43ec64c834b81f6d05b7cb4bdd879f2525016
Morgan Miller <m.miller722@gmail.com>
2015-05-30 11:57:19 -0600

    add view for browser info and platform info to source dashboard

--- app/models/source.rb
+++ app/models/source.rb
@@ -4,4 +4,31 @@ class Source < ActiveRecord::Base
 
   has_many :payloads
   
+  def user_agent_data
+    payloads.pluck(:user_agent).map { |data| UserAgent.parse(data) }
+  end
+  
+  def browser_info
+    user_agent_data.inject({}) do |browsers, agent|
+      if browsers.has_key?(agent.browser)
+        browsers[agent.browser] += 1
+      else
+        browsers[agent.browser] = 1
+      end
+      browsers
+    end
+  end
+  
+  def platform_info
+    user_agent_data.inject({}) do |platforms, agent|
+      if platforms.has_key?(agent.platform)
+        platforms[agent.platform] += 1
+      else
+        platforms[agent.platform] = 1
+      end
+      platforms
+    end
+  end
+  
+  
 end
