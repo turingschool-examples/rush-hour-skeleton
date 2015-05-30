99ddc271bf360a406ee6e1ac56ced21d4f4b1050
Rick Bacci <rickbacci@gmail.com>
2015-05-30 10:09:35 -0600

    update test to reflect curl JSON

--- test/controllers/processing_requests_test.rb
+++ test/controllers/processing_requests_test.rb
@@ -23,7 +23,10 @@ class ProcessingRequestTest < ControllersTest
 
   def test_a_payload_can_be_received
     post "/sources", { identifier: "jumpstartlab", rootUrl: "http://jumpstartlab.com" }
-    post "/sources/jumpstartlab/data", { "payload" => JSON.parse(payload) }
+   
+    post "/sources/jumpstartlab/data", 'payload={"url":"http://jumpstartlab.com/blog","requestedAt":"2013-02-16 21:38:28 -0700","respondedIn":37,"referredBy":"http://jumpstartlab.com","requestType":"GET","parameters":[],"eventName": "socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.38.211"}'
+
+   # post "/sources/jumpstartlab/data", { "payload" => JSON.parse(payload) }
     assert_equal 200, last_response.status
     assert_equal "success", last_response.body
   end
