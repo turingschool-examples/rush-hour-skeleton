d71044e8a53fbbb0b6ed30ecbcde9c9227fb8552
Rick Bacci <rickbacci@gmail.com>
2015-05-30 10:12:03 -0600

    fix so payload is parsed as JSON

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -1,11 +1,14 @@
 require 'digest'
 require 'byebug'
+require 'json'
+
 module TrafficSpy
   class Server < Sinatra::Base
 
     get '/' do
       erb :index
     end
+
     not_found do
       erb :error
     end
@@ -15,7 +18,7 @@ module TrafficSpy
       # body SourceResponder.new(params).body
 
       source_data = { identifier: params["identifier"],
-                        root_url: params["rootUrl"] }
+                      root_url: params["rootUrl"] }
 
       if Source.exists?(source_data)
         status 403
@@ -30,7 +33,6 @@ module TrafficSpy
           status 400
           body "Missing parameter, the required parameters are 'identifier' and 'rootUrl'"
         end
-
       end
     end
 
@@ -39,7 +41,6 @@ module TrafficSpy
       # body PayloadResponder.new(params).body
 
       if Source.exists?(identifier: identifier)
-
         sha = Payload.generate_sha(params.values.join)
         if Payload.exists?(sha: sha)
           status 403
@@ -48,15 +49,35 @@ module TrafficSpy
           status 400
           body "Payload missing"
         else
+          payload = params['payload']
+         # payload = { "payload" => JSON.parse(payload) }
+          # create ParamsParser class for all of these
+          payload = JSON.parse(payload)
+          url = payload['url']
+         # url = JSON.parse(params[:payload])['url']
           Payload.create({sha: sha})
+          Page.create(url: url) 
           body "success"
         end
-
       else
         status 403
         body "Application not registered"
       end
     end
+
+    #  When an identifer exists return a page that displays the following:
+    #  Most requested URLS to least requested URLS (url)
     
+    get '/sources/:identifier' do |identifier|
+      if Source.exists?(identifier: identifier)
+
+        status 200    
+        erb :index 
+      else
+        @error = "#{identifier} does not exist!"
+        status 404
+        erb :error
+      end
+    end
   end
 end
