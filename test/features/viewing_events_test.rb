99105c1c0283602e6d7de473db5dc5c5b4ecae76
Morgan Miller <m.miller722@gmail.com>
2015-05-31 13:18:38 -0600

    add viewing events tests

--- test/features/viewing_events_test.rb
+++ test/features/viewing_events_test.rb
@@ -6,14 +6,14 @@ class ViewingEventsTest < FeatureTest
     Source.create({identifier: identifier, root_url: "http://#{identifier}.com" })
   end
 
-  def json(identifier)
+  def json(identifier, event)
     { :url=>              "http://#{identifier}.com/blog",
       :requestedAt=>      "2013-02-16 21:38:28 -0700",
       :respondedIn=>      37,
       :referredBy=>       "http://#{identifier}.com",
       :requestType=>      "GET",
       :parameters=>       [],
-      :eventName=>        "socialLogin",
+      :eventName=>        event,
       :userAgent=>        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
       :resolutionWidth=>  "1920",
       :resolutionHeight=> "1280",
@@ -21,13 +21,13 @@ class ViewingEventsTest < FeatureTest
     }.to_json
   end
 
-  def create_payload(identifier)
-    PayloadCreator.new(json(identifier), identifier)
+  def create_payload(identifier, event)
+    PayloadCreator.new(json(identifier, event), identifier)
   end
 
   def test_it_displays_events
     create_source("yahoo")
-    create_payload ("yahoo")
+    create_payload("yahoo", "socialLogin")
     
     visit '/sources/yahoo/events'
     assert page.has_content?("socialLogin: 1")
