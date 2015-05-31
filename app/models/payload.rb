6549842d5f4b52a0d2e7544fd48d373f073c1300
vanegomez <vane.gomez@me.com>
2015-05-30 18:20:31 -0600

    tests for payload attributes

--- app/models/payload.rb
+++ app/models/payload.rb
@@ -1,9 +1,7 @@
 class Payload < ActiveRecord::Base
-  validates :requested_at, uniqueness: true, presence: true
+  validates :requested_at, presence: true
   validates :url, presence: true
-  validates :responded_at, presence: true
-  validates :referred_by, presence: true
-  validates :requested_type, presence: true
+  validates :responded_in, presence: true
   validates :event_name, presence: true
   validates :user_agent, presence: true
   validates :resolution_width, presence: true
