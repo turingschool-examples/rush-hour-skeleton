a343a5204b83c16843d50fbd024a7ca4d377a9e8
Drew Conly <atconly@gmail.com>
2015-07-15 16:12:17 -0600

    passing operating systems test

--- app/models/source.rb
+++ app/models/source.rb
@@ -13,6 +13,10 @@ module TrafficSpy
       sort_browsers
     end
 
+    def os_breakdown
+      sort_operating_systems
+    end
+
     private
 
     def urls
@@ -41,6 +45,20 @@ module TrafficSpy
 
     def sort_browsers
       browsers.uniq.sort_by {|v| browser_counts[v]}.reverse
+    end
+
+    def operating_systems
+      payloads.map do |payload|
+        payload.operating_system.name
+      end
+    end
+
+    def operating_system_counts
+      operating_systems.reduce(Hash.new(0)) {|h, v| h[v] += 1; h}
+    end
+
+    def sort_operating_systems
+      operating_systems.uniq.sort_by {|v| operating_system_counts[v]}.reverse
     end 
 
   end
--- test/models/source_test.rb
+++ test/models/source_test.rb
@@ -4,10 +4,7 @@ module TrafficSpy
   class SourceTest < Minitest::Test
 
     def test_source_returns_most_requested_urls
-      register_application
-      save_urls_to_table
-      save_browsers_to_table
-      save_payloads_to_table
+      populate
       source = Source.find_by(identifier: 'jumpstartlab')
       source.most_requested_urls
       expected = ["http://jumpstartlab.com/apply",
@@ -17,17 +14,30 @@ module TrafficSpy
     end
 
     def test_source_returns_browser_breakdown
-      register_application
-      save_urls_to_table
-      save_browsers_to_table
-      save_payloads_to_table
+      populate
       source = Source.find_by(identifier: 'jumpstartlab')
       source.browser_breakdown
       expected = ["Chrome", "Safari", "Firefox"]
       assert_equal expected, source.browser_breakdown
     end
 
+    def test_source_returns_os_breakdown
+      populate
+      source = Source.find_by(identifier: 'jumpstartlab')
+      source.browser_breakdown
+      expected = ["Macintosh", "Windows", "Linux"]
+      assert_equal expected, source.os_breakdown
+    end
+
     private
+
+    def populate
+      register_application
+      save_urls_to_table
+      save_browsers_to_table
+      save_os_to_table
+      save_payloads_to_table
+    end
 
     def register_application
       Source.create(identifier: "jumpstartlab", root_url: "http://jumpstartlab.com")
@@ -45,6 +55,12 @@ module TrafficSpy
       Browser.create(name: "Safari")
     end
 
+    def save_os_to_table
+      OperatingSystem.create(name: "Macintosh")
+      OperatingSystem.create(name: "Windows")
+      OperatingSystem.create(name: "Linux")
+    end
+
     def save_payloads_to_table
       source = Source.find_by(identifier: "jumpstartlab")
       payload_sample.each do |datum|
@@ -53,12 +69,12 @@ module TrafficSpy
     end
 
     def payload_sample
-      [{"digest":"6", "url_id":find_url_id("http://jumpstartlab.com/apply"), "browser_id":find_browser_id("Chrome")},
-       {"digest":"3", "url_id":find_url_id("http://jumpstartlab.com"), "browser_id":find_browser_id("Chrome")},
-       {"digest":"4", "url_id":find_url_id("http://jumpstartlab.com/blog"), "browser_id":find_browser_id("Firefox")},
-       {"digest":"1", "url_id":find_url_id("http://jumpstartlab.com/apply"), "browser_id":find_browser_id("Safari")},
-       {"digest":"6", "url_id":find_url_id("http://jumpstartlab.com/apply"), "browser_id":find_browser_id("Safari")},
-       {"digest":"6", "url_id":find_url_id("http://jumpstartlab.com"), "browser_id":find_browser_id("Chrome")}]
+      [{"digest":"6", "url_id":find_url_id("http://jumpstartlab.com/apply"), "browser_id":find_browser_id("Chrome"), "operating_system_id":find_os_id("Macintosh")},
+       {"digest":"3", "url_id":find_url_id("http://jumpstartlab.com"), "browser_id":find_browser_id("Chrome"), "operating_system_id":find_os_id("Windows")},
+       {"digest":"4", "url_id":find_url_id("http://jumpstartlab.com/blog"), "browser_id":find_browser_id("Firefox"), "operating_system_id":find_os_id("Macintosh")},
+       {"digest":"1", "url_id":find_url_id("http://jumpstartlab.com/apply"), "browser_id":find_browser_id("Safari"), "operating_system_id":find_os_id("Linux")},
+       {"digest":"6", "url_id":find_url_id("http://jumpstartlab.com/apply"), "browser_id":find_browser_id("Safari"), "operating_system_id":find_os_id("Windows")},
+       {"digest":"6", "url_id":find_url_id("http://jumpstartlab.com"), "browser_id":find_browser_id("Chrome"), "operating_system_id":find_os_id("Macintosh")}]
     end
 
     def find_url_id(url)
@@ -67,6 +83,10 @@ module TrafficSpy
 
     def find_browser_id(url)
       Browser.find_by(name: url).id
+    end
+
+    def find_os_id(url)
+      OperatingSystem.find_by(name: url).id
     end
 
   end
