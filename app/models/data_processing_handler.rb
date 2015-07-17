73a73955b3e46a362c7c96408960489d7a7bb570
Jason Wright <noobjey@gmail.com>
2015-07-16 19:14:06 -0600

    using registration handler

--- app/models/data_processing_handler.rb
+++ app/models/data_processing_handler.rb
@@ -3,7 +3,7 @@ module TrafficSpy
     attr_reader :payload, :identifier, :body, :status
 
     def initialize(raw_payload, identifier)
-      @payload = raw_payload
+      @payload    = raw_payload
       # @payload = PayloadParser.new(raw_payload)
       @identifier = identifier
       # @registration = Registration.find_by(identifier: identifier)
@@ -16,35 +16,35 @@ module TrafficSpy
 
       if !exist
         @status = 403
-        @body = "Application Not Registered - 403 Forbidden"
+        @body   = "Application Not Registered - 403 Forbidden"
       elsif payload[:payload].nil?
         @status = 400
-        @body = "Missing Payload - 400 Bad Request"
+        @body   = "Missing Payload - 400 Bad Request"
       else
         check_for_repeat_payload
       end
     end
 
     def check_for_repeat_payload
-      parser = PayloadParser.new(payload)
+      parser      = PayloadParser.new(payload)
       current_sha = Digest::SHA1.hexdigest(parser.payload.to_s)
       if Payload.exists?(payload_sha: current_sha)
         @status = 403
-        @body = "Already Received Request - 403 Forbidden"
+        @body   = "Already Received Request - 403 Forbidden"
       else
         save_payload(current_sha)
       end
-
     end
+
     def save_payload(current_sha)
       registration = Registration.find_by(:identifier => identifier)
-      parser = PayloadParser.new(payload)
+      parser       = PayloadParser.new(payload)
       registration.payloads.create(url: Url.find_or_create_by(parser.url))
 
       payload = registration.payloads.last
       payload.update(payload_sha: current_sha)
       @status = 200
-      @body = "Success"
+      @body   = "Success"
     end
   end
 end
