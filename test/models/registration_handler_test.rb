d65eb1562adbb23c2b29f11285254bfc9873cc06
Jason Wright <noobjey@gmail.com>
2015-07-16 18:09:55 -0600

    i accidently commited to master, this is try getting it right

--- test/models/registration_handler_test.rb
+++ test/models/registration_handler_test.rb
@@ -1,2 +1,27 @@
-class RegistrationHandlerTest
+require_relative '../test_helper'
+
+class RegistrationHandlerTest < Minitest::Test
+  attr_reader :raw_input
+
+  def setup
+    super
+    @raw_input = { "identifier" => "facebook", "rootUrl" => "http://facebook.com" }
+  end
+
+  def test_returns_400_when_parameters_missing
+    params  = {}
+    handler = RegistrationHandler.new(params)
+
+    assert_equal 400, handler.status
+    assert_equal "Missing Parameters - 400 Bad Request", handler.body
+  end
+
+  # def test_returns_400_when_identifier_missing
+  #   params  = { "rootUrl" => "http://facebook.com" }
+  #   handler = RegistrationHandler.new(params)
+  #
+  #   assert_equal 400, handler.status
+  #   assert_equal "Missing Parameters - 400 Bad Request", handler.body
+  # end
+
 end
