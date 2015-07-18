b49b07c3c692399fac50f18c86536550135adf21
Jason Wright <noobjey@gmail.com>
2015-07-18 15:22:17 -0600

    fixed bug and changed tests to catch it

--- test/models/payload_parser_test.rb
+++ test/models/payload_parser_test.rb
@@ -7,7 +7,7 @@ class PayloadParserTest < Minitest::Test
   def raw_params
     @input_json = "{\"url\":\"http://jumpstartlab.com/blog\",\"requestedAt\":\"2013-02-16 21:38:28 -0700\",\"respondedIn\":37,\"referredBy\":\"http://jumpstartlab.com\",\"requestType\":\"GET\",\"parameters\":[],\"eventName\": \"socialLogin\",\"userAgent\":\"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17\",\"resolutionWidth\":\"1920\",\"resolutionHeight\":\"1280\",\"ip\":\"63.29.38.211\"}"
 
-    { payload: @input_json }
+    { 'payload' => @input_json }
   end
 
   def setup
