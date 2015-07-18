d9367ee6ae24663149475b68272799d6f90c12e5
Jason Wright <noobjey@gmail.com>
2015-07-18 16:23:05 -0600

    all data processing tests use same input payload

--- test/models/data_processing_handler_test.rb
+++ test/models/data_processing_handler_test.rb
@@ -4,10 +4,7 @@ class DataProcessingHandlerTest < Minitest::Test
 
   def setup
     super
-    @raw_payload = { "payload"    => "{\"url\":\"http://jumpstartlab.com/blog\",\"requestedAt\":\"2013-02-16 21:38:28 -0700\",\"respondedIn\":37,\"referredBy\":\"http://jumpstartlab.com\",\"requestType\":\"GET\",\"parameters\":[],\"eventName\": \"socialLogin\",\"userAgent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17\",\"resolutionWidth\":\"1920\",\"resolutionHeight\":\"1280\",\"ip\":\"63.29.38.211\"}",
-                     "splat"      => [],
-                     "captures"   => ["jumpstartlab"],
-                     "identifier" => "jumpstartlab" }
+
     @identifier = 'identifier'
     RegistrationHandler.new({ "identifier" => @identifier, "rootUrl" => "http://facebook.com" })
   end
