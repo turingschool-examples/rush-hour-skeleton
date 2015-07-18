17fc54f6bcab7f7de65a148d003deafcd03a10e0
Samson Brock <SamsonLB@gmail.com>
2015-07-18 16:43:21 -0600

    Refactor JSON Parser

--- test/models/json_parser_test.rb
+++ test/models/json_parser_test.rb
@@ -4,13 +4,16 @@ class JsonParserTest < ControllerTest
 
   def test_parses_a_json_payload
     expected = {
-        :requested_at=>"2013-02-16 21:38:28 -0700",
-        :responded_in=>37, :referred_by=>"http://jumpstartlab.com", :request_type=>"GET", :parameters=>[],
-        :event_name=>"socialLogin",
-        :browser=>"Chrome",
-        :platform=>"Macintosh",
-        :resolution_width=>"1920", :resolution_height=>"1280", :ip=>"63.29.38.211", :path=>"http://jumpstartlab.com/blog"
-    }
+      :path=>"http://jumpstartlab.com/blog",
+      :requested_at=>"2013-02-16 21:38:28 -0700",
+      :responded_in=>37,
+      :referred_by=>"http://jumpstartlab.com",
+      :request_type=>"GET",
+      :event_name=>"socialLogin",
+      :resolution_width=>"1920",
+      :resolution_height=>"1280",
+      :browser=>"Chrome",
+      :platform=>"Macintosh" }
 
     assert_equal expected, JsonParser.parse(payload[:payload])
   end
