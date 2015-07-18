4a1d7364a17cff23c290deee8454fdde2eeb6bf9
Adam Caron <adamwcaron@gmail.com>
2015-07-18 15:32:21 -0600

    calculates and renders event-specific data

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -74,11 +74,55 @@ module TrafficSpy
       erb :event_index
     end
 
-    get '/sources/:indentifier/events/:event_name' do |indentifier, event_name|
-      @indentifier = indentifier
+    get '/sources/:indentifier/events/:event_name' do |identifier, event_name|
+      @identifier = identifier
       @event_name = event_name
+      site = Site.find_by(:identifier => identifier)
+      @event = site.events.find_by(:name => @event_name)
+
+      if @event.blank?
+        @message = "The event, '#{@event_name}', has not been requested."
+
+        erb :message
+      else
+      events = site.payloads.where(:event_id => @event.id)
+      hours = events.map { |event| Time.parse(event[:requested_at]).hour }
+      @hour_count = hours.group_by{ |h| h }
+      @day = {
+        0 => 0,
+        1 => 0,
+        2 => 0,
+        3 => 0,
+        4 => 0,
+        5 => 0,
+        6 => 0,
+        7 => 0,
+        8 => 0,
+        9 => 0,
+        10 => 0,
+        11 => 0,
+        12 => 0,
+        13 => 0,
+        14 => 0,
+        15 => 0,
+        16 => 0,
+        17 => 0,
+        18 => 0,
+        19 => 0,
+        20 => 0,
+        21 => 0,
+        22 => 0,
+        23 => 0,
+      }
+      @day.map do |k, v|
+        if @hour_count[k] != nil
+          @day[k] = @hour_count[k].size
+        end
+      end
 
       erb :event_data
+    end
+
     end
 
     post "/sources/:identifier/data" do |identifier|
