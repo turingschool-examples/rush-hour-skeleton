47ee4cca318e9423e0815140ffa115a1a23726e3
Michael Dorrance <dorrancemd@gmail.com>
2015-07-18 09:25:01 -0600

    fixes view_browser_data setup

--- test/features/client_can_view_browser_data_test.rb
+++ test/features/client_can_view_browser_data_test.rb
@@ -6,8 +6,9 @@ class ClientCanViewBrowserDataTest < FeatureTest
     register_site
     Url.create(path: "http://jumpstartlab.com/blog", site_id: @site.id)
     browser = Browser.create(name: "Chrome")
+    platform = Platform.create(name: "Macintosh")
     Payload.create(url_id: 1, resolution_width: "900", resolution_height: "1100",requested_at: "sometime", responded_in: 37, event_id: 1, referrer_id: 1, browser_id: browser.id,
-                   platform_id: 1, request_type_id: 1, sha: "alskdjflkj")
+                   platform_id: platform.id, request_type_id: 1, sha: "alskdjflkj")
 
     visit '/sources/jumpstartlab'
 
@@ -20,12 +21,13 @@ class ClientCanViewBrowserDataTest < FeatureTest
     browser1 = Browser.create(name: "Mozilla")
     browser2 = Browser.create(name: "Chrome")
     browser3 = Browser.find_or_create_by(name: "Chrome")
+    platform = Platform.create(name: "Macintosh")
     Payload.create(url_id: 1, resolution_width: "900", resolution_height: "1100",requested_at: "sometime", responded_in: 37, event_id: 1, referrer_id: 1, browser_id: browser1.id,
-                   platform_id: 1, request_type_id: 1, sha: "alskdjfdflkj")
+                   platform_id: platform.id, request_type_id: 1, sha: "alskdjfdflkj")
     Payload.create(url_id: 1, resolution_width: "900", resolution_height: "1100",requested_at: "sometime", responded_in: 37, event_id: 1, referrer_id: 1, browser_id: browser2.id,
-                   platform_id: 1, request_type_id: 1, sha: "alskdjfslkj")
+                   platform_id: platform.id, request_type_id: 1, sha: "alskdjfslkj")
     Payload.create(url_id: 1, resolution_width: "900", resolution_height: "1100",requested_at: "sometime", responded_in: 37, event_id: 1, referrer_id: 1, browser_id: browser3.id,
-                   platform_id: 1, request_type_id: 1, sha: "alskdfaedjflkj")
+                   platform_id: platform.id, request_type_id: 1, sha: "alskdfaedjflkj")
 
     visit '/sources/jumpstartlab'
 
