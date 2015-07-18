4dda11c288e31d491958ccb7c181075026b21268
Jason Wright <noobjey@gmail.com>
2015-07-18 16:11:39 -0600

    data processing handler tests

--- test/controllers/identifier_data_path_test.rb
+++ test/controllers/identifier_data_path_test.rb
@@ -1,40 +1,40 @@
-require_relative '../test_helper'
-
-class IdentifierDataPathTest < ControllerTest
-
-  def test_registration_returns_403_when_identifier_not_registered
-    post '/sources/identifier_not_in_database/data', @payload
-
-    assert_equal 403, last_response.status
-    assert_equal 'Application Not Registered - 403 Forbidden', last_response.body
-  end
-
-  def test_if_the_payload_is_missing
-    post '/sources', { "identifier" => "facebook", "rootUrl" => "http://facebook.com" }
-    id = Registration.all.first.identifier
-    post "/sources/#{id}/data", { stuff: "stuff" }
-
-    assert_equal 400, last_response.status
-    assert_equal "Missing Payload - 400 Bad Request", last_response.body
-  end
-
-  def test_url_is_saved
-    post '/sources', { "identifier" => "jumpstartlab", "rootUrl" => "http://jumpstartlab.com" }
-    id = Registration.all.first.identifier
-
-    post "/sources/#{id}/data", @payload
-
-    assert_equal "http://jumpstartlab.com/blog", Url.all.first[:url]
-  end
-
-  def test_if_the_payload_has_already_been_recieved
-    post '/sources', { "identifier" => "facebook", "rootUrl" => "http://facebook.com" }
-    id = Registration.all.first.identifier
-
-    post "/sources/#{id}/data", @payload
-    post "/sources/#{id}/data", @payload
-
-    assert_equal 403, last_response.status
-    assert_equal "Already Received Request - 403 Forbidden", last_response.body
-  end
-end
+# require_relative '../test_helper'
+#
+# class IdentifierDataPathTest < ControllerTest
+#
+#   def test_registration_returns_403_when_identifier_not_registered
+#     post '/sources/identifier_not_in_database/data', @payload
+#
+#     assert_equal 403, last_response.status
+#     assert_equal 'Application Not Registered - 403 Forbidden', last_response.body
+#   end
+#
+#   def test_if_the_payload_is_missing
+#     post '/sources', { "identifier" => "facebook", "rootUrl" => "http://facebook.com" }
+#     id = Registration.all.first.identifier
+#     post "/sources/#{id}/data", { stuff: "stuff" }
+#
+#     assert_equal 400, last_response.status
+#     assert_equal "Missing Payload - 400 Bad Request", last_response.body
+#   end
+#
+#   def test_url_is_saved
+#     post '/sources', { "identifier" => "jumpstartlab", "rootUrl" => "http://jumpstartlab.com" }
+#     id = Registration.all.first.identifier
+#
+#     post "/sources/#{id}/data", @payload
+#
+#     assert_equal "http://jumpstartlab.com/blog", Url.all.first[:url]
+#   end
+#
+#   def test_if_the_payload_has_already_been_recieved
+#     post '/sources', { "identifier" => "facebook", "rootUrl" => "http://facebook.com" }
+#     id = Registration.all.first.identifier
+#
+#     post "/sources/#{id}/data", @payload
+#     post "/sources/#{id}/data", @payload
+#
+#     assert_equal 403, last_response.status
+#     assert_equal "Already Received Request - 403 Forbidden", last_response.body
+#   end
+# end
--- test/models/data_processing_handler_test.rb
+++ test/models/data_processing_handler_test.rb
@@ -2,21 +2,64 @@ require_relative '../test_helper'
 
 class DataProcessingHandlerTest < Minitest::Test
 
-  def test_it
-    assert !!DataProcessingHandler.new('raw_payload', 'identifier')
+  def setup
+    super
+    @raw_payload = { "payload"    => "{\"url\":\"http://jumpstartlab.com/blog\",\"requestedAt\":\"2013-02-16 21:38:28 -0700\",\"respondedIn\":37,\"referredBy\":\"http://jumpstartlab.com\",\"requestType\":\"GET\",\"parameters\":[],\"eventName\": \"socialLogin\",\"userAgent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17\",\"resolutionWidth\":\"1920\",\"resolutionHeight\":\"1280\",\"ip\":\"63.29.38.211\"}",
+                     "splat"      => [],
+                     "captures"   => ["jumpstartlab"],
+                     "identifier" => "jumpstartlab" }
+    @identifier = 'identifier'
+    RegistrationHandler.new({ "identifier" => @identifier, "rootUrl" => "http://facebook.com" })
   end
 
-  def test_identifier_is_nil_if_not_registered
-    skip
-    raw_payload = {"payload"=>
-                     "{\"url\":\"http://jumpstartlab.com/blog\",\"requestedAt\":\"2013-02-16 21:38:28 -0700\",\"respondedIn\":37,\"referredBy\":\"http://jumpstartlab.com\",\"requestType\":\"GET\",\"parameters\":[],\"eventName\": \"socialLogin\",\"userAgent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17\",\"resolutionWidth\":\"1920\",\"resolutionHeight\":\"1280\",\"ip\":\"63.29.38.211\"}",
-                   "splat"=>[],
-                   "captures"=>["jumpstartlab"],
-                   "identifier"=>"jumpstartlab"}
-    identifier = 'facebook'
+  def test_converts_input
+    handler = DataProcessingHandler.new(@raw_payload, @identifier)
 
-    handler = DataProcessingHandler.new(raw_payload, identifier)
+    assert_equal 'http://jumpstartlab.com/blog', handler.payload[:url]
+  end
 
-    assert_equal nil, handler.identifier
+  def test_returns_403_when_identifier_not_registered
+    handler = DataProcessingHandler.new(@raw_payload, 'unregistered_identifier')
+
+    assert_equal 403, handler.status
+    assert_equal 'Application Not Registered - 403 Forbidden', handler.body
+  end
+
+  def test_returns_400_when_payload_is_missing
+    handler = DataProcessingHandler.new({'data' => 'no payload'}, @identifier)
+
+    assert_equal 400, handler.status
+    assert_equal 'Missing Payload - 400 Bad Request', handler.body
+  end
+
+  def test_returns_200_when_post_successful
+    handler = DataProcessingHandler.new(@raw_payload, @identifier)
+
+    assert_equal 200, handler.status
+    assert_equal 'Success', handler.body
+  end
+
+  def test_payload_saved_when_post_successful
+    DataProcessingHandler.new(@raw_payload, @identifier)
+
+    assert_equal  'http://jumpstartlab.com/blog', Url.all.first.url
+    assert_equal  1920, ScreenResolution.first.width
+    assert_equal  'Chrome', Browser.first.name
+    assert_equal  'socialLogin', Event.first.name
+    assert_equal  'Macintosh', OperatingSystem.first.name
+  end
+
+  def test_unique_payload_identifier_saved
+    DataProcessingHandler.new(@raw_payload, @identifier)
+
+    assert_equal 'f5077630df95b6718ac6f032a8360b211f7b3e04', Payload.last.payload_sha
+  end
+
+  def test_returns_403_when_duplicate_post
+    DataProcessingHandler.new(@raw_payload, @identifier)
+    handler = DataProcessingHandler.new(@raw_payload, @identifier)
+
+    assert_equal 403, handler.status
+    assert_equal "Already Received Request - 403 Forbidden", handler.body
   end
 end
