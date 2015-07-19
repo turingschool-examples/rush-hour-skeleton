9ffefd1ba8a40b16b4e16dff60384d5cf2b09afb
mitchashby16 <mitchashby16@gmail.com>
2015-07-19 14:31:04 -0600

    feature tests for urls

--- test/features/application_urls_feature_test.rb
+++ test/features/application_urls_feature_test.rb
@@ -6,12 +6,56 @@ class ApplicationUrlsTest < FeatureTest
     RegistrationHandler.new({ "identifier" => "jumpstartlab", "rootUrl" => "http://jumpstartlab.com" })
     @url = Url.new(:id => 2, :url => "http://jumpstartlab.com/blog", :responded_in => 37)
     @url.save
-    event   = Event.new(:id => 1, :name => "new event")
-    event.save
     payload = Payload.new(:id => 1, :registration_id => 1, :url_id => 2, :event_id => 1)
     payload.save
     visit "/sources/jumpstartlab/urls#{@url.path}"
     assert page.has_content?("URL Statistics")
+  end
+
+  def test_it_has_longest_response
+    RegistrationHandler.new({ "identifier" => "jumpstartlab", "rootUrl" => "http://jumpstartlab.com" })
+    @url = Url.new(:id => 2, :url => "http://jumpstartlab.com/blog", :responded_in => 37)
+    @url.save
+    payload = Payload.new(:id => 1, :registration_id => 1, :url_id => 2, :event_id => 1)
+    payload.save
+    visit "/sources/jumpstartlab/urls#{@url.path}"
+    assert page.has_content?("Longest Response Time: 37")
+  end
+
+  def test_it_has_shortest_response
+    RegistrationHandler.new({ "identifier" => "jumpstartlab", "rootUrl" => "http://jumpstartlab.com" })
+    @url = Url.new(:id => 2, :url => "http://jumpstartlab.com/blog", :responded_in => 37)
+    @url.save
+    @url2 = Url.new(:id => 3, :url => "http://jumpstartlab.com/blog", :responded_in => 7)
+    @url2.save
+    payload = Payload.new(:id => 1, :registration_id => 1, :url_id => 2, :event_id => 1)
+    payload.save
+    visit "/sources/jumpstartlab/urls#{@url.path}"
+    assert page.has_content?("Shortest Response Time: 7")
+  end
+
+  def test_it_has_average_response
+    RegistrationHandler.new({ "identifier" => "jumpstartlab", "rootUrl" => "http://jumpstartlab.com" })
+    @url = Url.new(:id => 2, :url => "http://jumpstartlab.com/blog", :responded_in => 37)
+    @url.save
+    @url2 = Url.new(:id => 3, :url => "http://jumpstartlab.com/blog", :responded_in => 7)
+    @url2.save
+    payload = Payload.new(:id => 1, :registration_id => 1, :url_id => 2, :event_id => 1)
+    payload.save
+    visit "/sources/jumpstartlab/urls#{@url.path}"
+    assert page.has_content?("Average Response Time: 22")
+  end
+
+  def test_it_has_http_verbs
+    RegistrationHandler.new({ "identifier" => "jumpstartlab", "rootUrl" => "http://jumpstartlab.com" })
+    @url = Url.new(:id => 2, :url => "http://jumpstartlab.com/blog", :responded_in => 37, :request_type => "GET")
+    @url.save
+    @url2 = Url.new(:id => 3, :url => "http://jumpstartlab.com/blog", :responded_in => 7, :request_type => "GET")
+    @url2.save
+    payload = Payload.new(:id => 1, :registration_id => 1, :url_id => 2, :event_id => 1)
+    payload.save
+    visit "/sources/jumpstartlab/urls#{@url.path}"
+    assert page.has_content?("Request Type Used: GET")
   end
 
 end
