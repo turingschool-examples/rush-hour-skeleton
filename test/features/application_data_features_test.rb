4238977132be1c9904b0cd10b059ebf11e6401e0
dsshim <daveshim@gmail.com>
2015-07-19 02:40:50 -0600

    css, views and refactored app data

--- test/features/application_data_features_test.rb
+++ test/features/application_data_features_test.rb
@@ -9,13 +9,13 @@ class ApplicationDataTest < FeatureTest
   def test_it_renders_homepage
     visit "/"
 
-    assert page.has_content?("Hello")
+    assert page.has_content?("Traffic Spy")
   end
 
   def test_it_renders_most_visited_urls_for_the_specified_identifier
     visit "/sources/jumpstartlab"
 
-    assert page.has_content?("Most Visited Websites")
+    assert page.has_content?("Most Visited Webpages")
   end
 
   def test_it_renders_most_used_browsers_for_the_specified_identifier
@@ -40,14 +40,12 @@ class ApplicationDataTest < FeatureTest
     visit "/sources/jumpstartlab"
 
     assert page.has_content?("Average Response Time")
-    assert page.has_content?("Longest Response Time")
-    assert page.has_content?("Shortest Response Time")
   end
 
   def test_it_renders_links_visited_for_the_specified_identifier
     visit "/sources/jumpstartlab"
 
-    assert page.has_content?("Links Visited")
+    assert page.has_content?("Webpage Data")
     assert page.has_content?("Event Data Links")
   end
 
