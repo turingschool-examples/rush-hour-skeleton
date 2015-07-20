3744e73bf49fe576715d5f88604eb357f56c68e0
dsshim <daveshim@gmail.com>
2015-07-20 10:01:19 -0600

    fixed tests final

--- test/models/stats_test.rb
+++ test/models/stats_test.rb
@@ -4,48 +4,42 @@ class StatsTest  < Minitest::Test
 
   def test_it_returns_url_stats
     @registration = RegistrationHandler.new({ "identifier" => "jumpstartlab", "rootUrl" => "http://jumpstartlab.com" })
+
     assert AppDataHandler.new("jumpstartlab").url_stats
   end
 
-
   def test_it_returns_browser_stats
     @registration = RegistrationHandler.new({ "identifier" => "jumpstartlab", "rootUrl" => "http://jumpstartlab.com" })
-    @input = "{\"url\":\"http://jumpstartlab.com/blog\",\"requestedAt\":\"2013-02-16 21:38:28 -0700\",\"respondedIn\":37,\"referredBy\":\"http://jumpstartlab.com\",\"requestType\":\"GET\",\"parameters\":[],\"eventName\": \"socialLogin\",\"userAgent\":\"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17\",\"resolutionWidth\":\"1920\",\"resolutionHeight\":\"1280\",\"ip\":\"63.29.38.211\"}"
-    DataProcessingHandler.new(@input, "jumpstartlab")
+    DataProcessingHandler.new(@raw_payload, "jumpstartlab")
 
-    assert AppDataHandler.new("jumpstartlab").browser_stats
+    assert_equal 1, AppDataHandler.new("jumpstartlab").browser_stats.count
   end
 
   def test_it_returns_os_stats
     @registration = RegistrationHandler.new({ "identifier" => "jumpstartlab", "rootUrl" => "http://jumpstartlab.com" })
-    @input = "{\"url\":\"http://jumpstartlab.com/blog\",\"requestedAt\":\"2013-02-16 21:38:28 -0700\",\"respondedIn\":37,\"referredBy\":\"http://jumpstartlab.com\",\"requestType\":\"GET\",\"parameters\":[],\"eventName\": \"socialLogin\",\"userAgent\":\"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17\",\"resolutionWidth\":\"1920\",\"resolutionHeight\":\"1280\",\"ip\":\"63.29.38.211\"}"
-    DataProcessingHandler.new(@input, "jumpstartlab")
+    DataProcessingHandler.new(@raw_payload, "jumpstartlab")
 
-    assert AppDataHandler.new("jumpstartlab").os_stats
+    assert_equal 1, AppDataHandler.new("jumpstartlab").os_stats.count
   end
 
   def test_it_returns_resolution_stats
     @registration = RegistrationHandler.new({ "identifier" => "jumpstartlab", "rootUrl" => "http://jumpstartlab.com" })
-    @input = "{\"url\":\"http://jumpstartlab.com/blog\",\"requestedAt\":\"2013-02-16 21:38:28 -0700\",\"respondedIn\":37,\"referredBy\":\"http://jumpstartlab.com\",\"requestType\":\"GET\",\"parameters\":[],\"eventName\": \"socialLogin\",\"userAgent\":\"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17\",\"resolutionWidth\":\"1920\",\"resolutionHeight\":\"1280\",\"ip\":\"63.29.38.211\"}"
-    DataProcessingHandler.new(@input, "jumpstartlab")
+    DataProcessingHandler.new(@raw_payload, "jumpstartlab")
 
-    assert AppDataHandler.new("jumpstartlab").resolution_stats
+    assert_equal 1, AppDataHandler.new("jumpstartlab").resolution_stats.count
   end
 
   def test_it_returns_response_times
     @registration = RegistrationHandler.new({ "identifier" => "jumpstartlab", "rootUrl" => "http://jumpstartlab.com" })
-    @input = "{\"url\":\"http://jumpstartlab.com/blog\",\"requestedAt\":\"2013-02-16 21:38:28 -0700\",\"respondedIn\":37,\"referredBy\":\"http://jumpstartlab.com\",\"requestType\":\"GET\",\"parameters\":[],\"eventName\": \"socialLogin\",\"userAgent\":\"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17\",\"resolutionWidth\":\"1920\",\"resolutionHeight\":\"1280\",\"ip\":\"63.29.38.211\"}"
-    DataProcessingHandler.new(@input, "jumpstartlab")
+    DataProcessingHandler.new(@raw_payload, "jumpstartlab")
 
-    assert AppDataHandler.new("jumpstartlab").response_times
+    assert AppDataHandler.new("jumpstartlab").response_times.count
   end
 
   def test_it_returns_link_lists
     @registration = RegistrationHandler.new({ "identifier" => "jumpstartlab", "rootUrl" => "http://jumpstartlab.com" })
-    @input = "{\"url\":\"http://jumpstartlab.com/blog\",\"requestedAt\":\"2013-02-16 21:38:28 -0700\",\"respondedIn\":37,\"referredBy\":\"http://jumpstartlab.com\",\"requestType\":\"GET\",\"parameters\":[],\"eventName\": \"socialLogin\",\"userAgent\":\"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17\",\"resolutionWidth\":\"1920\",\"resolutionHeight\":\"1280\",\"ip\":\"63.29.38.211\"}"
-    DataProcessingHandler.new(@input, "jumpstartlab")
+    DataProcessingHandler.new(@raw_payload, "jumpstartlab")
 
-    assert AppDataHandler.new("jumpstartlab").link_list
+    assert AppDataHandler.new("jumpstartlab").link_list.count
   end
-
 end
