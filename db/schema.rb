92bb342438d8e879b85f87db1ddbf166ea22351b
Justin Pease <justinpease2@gmail.com>
2015-09-02 14:04:58 -0600

    adds response table and relationships

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901233413) do
+ActiveRecord::Schema.define(version: 20150902193928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "responses", force: :cascade do |t|
+    t.datetime "requested_at"
+    t.integer  "responded_in"
+    t.inet     "ip"
+    t.integer  "sub_url_id"
+    t.string   "parameters",                array: true
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.text     "identifier"
