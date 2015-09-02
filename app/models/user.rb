78069b6a6ecd69d338500696479d9b69e4b41dce
Bret Doucette <doucette.bret@gmail.com>
2015-09-01 20:28:38 -0600

    Added Validation features on Application Registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-
+ validates :root_url, presence: true, uniqueness: true
+ validates :identifier, presence: true, uniqueness: true
 end
--- test/controllers/create_user_test.rb
+++ test/controllers/create_user_test.rb
@@ -20,6 +20,36 @@ class CreateUserTest < Minitest::Test
     assert_equal 200, last_response.status
   end
 
+  def test_it_will_not_create_a_user_with_a_missing_root_url
+    attributes = { identifier: "jumpstart"}
+    post "/sources", attributes
+
+    assert_equal 0, User.count
+    assert_equal 400, last_response.status
+  end
+
+  def test_it_will_not_create_a_user_with_a_missing_identifier
+    attributes = { rootUrl: "http://jumpstartlab.com"}
+    post "/sources", attributes
+
+    assert_equal 0, User.count
+    assert_equal 400, last_response.status
+  end
+
+  def test_it_will_not_create_a_user_twice
+    attributes = { identifier: "jumpstart",
+                   rootUrl: "http://jumpstartlab.com" }
+    post "/sources", attributes
+    assert_equal 1, User.count
+    assert_equal 200, last_response.status
+
+    attributes = { identifier: "jumpstart",
+                   rootUrl: "http://jumpstartlab.com" }
+    post "/sources", attributes
+    assert_equal 1, User.count
+    assert_equal 403, last_response.status
+  end
+
   def teardown
     DatabaseCleaner.clean
   end
