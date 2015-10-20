225f3162264c05fbe64f3c759f62f9faa5d56933
ShannonPaige <spaige@gmail.com>
2015-10-19 20:17:41 -0600

    added TDD for 8 and 9

---
+++ test/features/user_can_access_their_data_test.rb
@@ -0,0 +1,30 @@
+require_relative '../test_helper'
+
+class ApplicationDetailTest < MiniTest::Test
+
+  def test_user_can_see_thier_data_HAPPY
+    create_user(1)
+    visit '/sources/test_company_1'
+
+    assert_equal '/sources/test_company_1', current_path
+
+    assert page.has_content?("Most Requested URLS")
+    assert page.has_content?("Web Browser Breakdown")
+    assert page.has_content?("OS Breakdown")
+    assert page.has_content?("Screen Resolution")
+    assert page.has_content?("Average Response Time Per URL")
+    assert page.has_content?("Most Requested URLS")
+    assert page.has_content?("Most Requested URLS")
+    #Hyperlinks of each url to view url specific data
+    #Hyperlink to view aggregate event data
+  end
+
+  def test_user_can_see_thier_data_SAD
+    visit '/sources/test_company_1'
+
+    assert_equal '/sources/error', current_path
+
+    assert page.has_content?("Test Company 1 is not a client of traffic spy")
+  end
+
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,3 +8,13 @@ require 'minitest/autorun'
 require 'capybara'
 
 Capybara.app = TrafficSpy::Server
+
+#added by shannon on 10/19 - not sure if we need it with active record?
+class FeatureTest < MiniTest::Test
+  include Capybara::DSL
+
+  def create_user(num)
+    # will create some users here - the INVENTORY variables
+    # I'll give it the name test_company_1 where i is num
+  end
+end
