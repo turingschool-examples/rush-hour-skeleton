e2e7db1e682f4e3b43c4acd83e615650cb27b355
Edgar Duran <edgarduran@Edgars-MacBook-Pro.local>
2015-10-21 17:13:31 -0600

    second section done

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -34,26 +34,26 @@ module TrafficSpy
     end
 
     post '/sources/:identifier/data'  do |identifier|
-      binding.pry
-      data = ParsePayload.parse(params)
-      data["sha"] = Digest::SHA2.hexdigest(params.to_s)
-      data["user_id"] = params["identifier"]
-      data.delete("parameters")
-      payload = Payload.new(data)
-      # if params == {}
-      #   status 400
-      #   body "400 Bad Request - Payload Missing"
-      # els
-      if Payload.all.include?(Payload.find_by("sha" => Digest::SHA2.hexdigest(params.to_s)))
-        status 403
-        body "403 Forbidden - Duplicate Payload"
-      elsif User.find_by(identifier: identifier).nil?
-        status 403
-        body "403 Forbidden - User Does Not Exist"
+      if params["payload"].nil?
+        status 400
+        body "400 Bad Request - Payload Missing"
       else
-        payload.save
-        status 200
-        body "Success"
+        data = ParsePayload.parse(params)
+        data["sha"] = Digest::SHA2.hexdigest(params.to_s)
+        data["user_id"] = params["identifier"]
+        data.delete("parameters")
+        payload = Payload.new(data)
+        if Payload.all.include?(Payload.find_by("sha" => Digest::SHA2.hexdigest(params.to_s)))
+          status 403
+          body "403 Forbidden - Duplicate Payload"
+        elsif User.find_by(identifier: identifier).nil?
+          status 403
+          body "403 Forbidden - User Does Not Exist"
+        else
+          payload.save
+          status 200
+          body "Success"
+        end
       end
     end
   end
--- test/controllers/payload_test.rb
+++ test/controllers/payload_test.rb
@@ -8,7 +8,6 @@ class PayloadTest < Minitest::Test
   end
 
   def test_request_contains_payload
-skip
     post '/sources', {"identifier" => "jumpstartlab", "rootUrl" => "HTTP://Example.com"}
     post '/sources/jumpstartlab/data', {"payload"=> "{\"url\":\"http://jumpstartlab.com/blog\",
                                                       \"requestedAt\":\"2013-02-16 21:38:28 -0700\",
@@ -26,7 +25,6 @@ skip
   end
 
   def test_that_request_denied_if_request_is_not_unique
-skip
     post '/sources', {"identifier" => "jumpstartlab", "rootUrl" => "HTTP://Example.com"}
     post '/sources/jumpstartlab/data', {"payload"=> "{\"url\":\"http://jumpstartlab.com/blog\",
                                                       \"requestedAt\":\"2013-02-16 21:38:28 -0700\",
@@ -56,7 +54,6 @@ skip
   end
 
   def test_that_request_denied_if_user_is_unregistered
-skip
     post '/sources/jumpstartlab/data', {"payload"=> "{\"url\":\"http://jumpstartlab.com/blog\",
                                                       \"requestedAt\":\"2013-02-16 21:38:28 -0700\",
                                                       \"respondedIn\":37,\"referredBy\":\"http://jumpstartlab.com\",
