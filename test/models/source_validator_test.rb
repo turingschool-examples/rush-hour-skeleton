59773939ee2b2ef3e10f86d27ae474371def7ff7
rossedfort <rossedfort@gmail.com>
2015-10-21 20:19:41 -0600

    added some create_sources method to test helper - utilized in one test

--- test/models/source_validator_test.rb
+++ test/models/source_validator_test.rb
@@ -18,9 +18,9 @@ class SourceValidatorTest < Minitest::Test
   def test_source_validator_excludes_sources_with_duplicate_identifiers
     sources = TrafficSpy::Source
     source_one = sources.create({identifier: "jumpstartlab",
-                   root_url: "http://jumpstartlab.com"})
+                                 root_url: "http://jumpstartlab.com"})
     source_two = sources.create({identifier: "jumpstartlab",
-                   root_url: "http://jumpstartlab.com"})
+                                 root_url: "http://jumpstartlab.com"})
 
     assert sources.all.include?(source_one)
     refute sources.all.include?(source_two)
