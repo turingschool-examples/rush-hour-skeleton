95ab6996c723f9e354051c7a354d0a2e7e4b90e4
rossedfort <rossedfort@gmail.com>
2015-10-21 19:49:06 -0600

    got some view functionalality working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151021191738) do
+ActiveRecord::Schema.define(version: 20151022002318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,8 +28,8 @@ ActiveRecord::Schema.define(version: 20151021191738) do
     t.string   "resolutionWidth"
     t.string   "resolutionHeight"
     t.string   "ip"
-    t.integer  "sources_id"
     t.string   "unique_hash"
+    t.integer  "source_id"
   end
 
   create_table "sources", force: :cascade do |t|
