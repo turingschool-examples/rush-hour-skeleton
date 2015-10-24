0f2d833341ce6b1d867a941297a74cddaeb84616
Nicole Hall <nicolealexandrahall@gmail.com>
2015-10-24 16:07:11 -0600

    test added for testing that validator can deal with both payload and source

--- test/models/payload_validator_test.rb
+++ test/models/payload_validator_test.rb
@@ -50,7 +50,24 @@ class PayloadValidatorTest < Minitest::Test
   end
 
   def test_payload_validator_excludes_payloads_for_unregistered_source
-    #does this need to be a test??
+    sources = TrafficSpy::Source
+    registered = sources.create({identifier: "jumpstartlab",
+                                 root_url: "http://jumpstartlab.com"})
+    payloads  = TrafficSpy::Payload
+    payload_of_unregistered_source = payloads.create({:url=>"http://unregistered_source.com/blog",
+                    :requested_at=>"2013-02-16 21:38:28 -0700",
+                    :responded_in=>37,
+                    :referred_by=>"http://unregistered_source.com",
+                    :request_type=>"GET",
+                    :parameters=>[],
+                    :event_name=>"socialLogin",
+                    :user_agent=>"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+                    :resolution_width=>"1920",
+                    :resolution_height=>"1280",
+                    :ip=>"63.29.38.211"})
+
+    assert_equal [403, "App Not Registered!"],TrafficSpy::Validator.validate_payload(registered, payload_of_unregistered_source, sources)
+    #does this need to be a test?
     #it is a test that interacts with both databases so is it not a model test?
   end
 
