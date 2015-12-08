c0028ddaa2dd7968fa07a7ddacfcfaf0868e5b10
Steve Pentler <stevepentler@gmail.com>
2015-12-08 12:58:15 -0700

    2nd test passing

--- test/controllers/register_app_test.rb
+++ test/controllers/register_app_test.rb
@@ -8,4 +8,15 @@ class RegisterAppTest < ControllerTest
     assert_equal "{\"identifier\":\"turing\"}", last_response.body
     assert_equal 1, TrafficSpy::Application.count
   end
-end
+
+  def test_app_registration_fails_without_identifier
+# As a guest,
+  post '/sources', {rootUrl: "http://turing.io"}
+# When I POST to '/sources'
+# With no identifier specified,
+# Then I receive a 400 response
+  assert_equal 400, last_response.status
+  assert_equal "No identifier provided.", last_response.body
+# And I receive a error message saying I'm missing the identifier
+  end
+end 
