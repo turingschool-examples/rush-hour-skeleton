a4dff446f59f41bf984e7b6907d30bc1c586e0ef
Steven Olson <steveoscaro@gmail.com>
2015-12-08 13:08:36 -0700

    x

--- test/controllers/register_new_user_test.rb
+++ test/controllers/register_new_user_test.rb
@@ -2,6 +2,12 @@ require_relative '../test_helper'
 
 class RegisterNewUserTest < TrafficTest
 
+  def test_create_genre_with_valid_attributes
+      post '/sources', { identifier=jumpstartlab&rootUrl=http://jumpstartlab.com }               # post method comes from Rack::Test::Methods. second argument is data params
+
+      assert_equal 200, last_response.status
+      
+    end
 
 
 end
