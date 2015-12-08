3f6d64e8a97fbb01dd59017e44c6126e2816132e
Steve Pentler <stevepentler@gmail.com>
2015-12-08 16:05:28 -0700

    does not process request for unregistered application test passing

--- test/controllers/process_request_test.rb
+++ test/controllers/process_request_test.rb
@@ -1,19 +1,41 @@
 require './test/test_helper'
 
 class ProcessRequestTest < ControllerTest
-  def test_process_request_without_payload
-# As a registered app,
-  TrafficSpy::Application.create({ identifier: "turing", root_url: "http://turing.io"})
-# When I POST to '/sources/MY_ID/data',
-post '/sources/turing/data'
-# And I do not include a payload,
-# Then I receive a 400 Bad Request status,
-assert_equal 400, last_response.status
-assert_equal "Payload can't be blank", last_response.body
-assert_equal 0, TrafficSpy::Payload.count
-
-# And I receive a descriptive error
+  def test_does_not_process_request_without_payload
+    TrafficSpy::Application.create({ identifier: "turing", root_url: "http://turing.io"})
+    post '/sources/turing/data'
+    assert_equal 400, last_response.status
+    assert_equal "Payload can't be blank", last_response.body
+    assert_equal 0, TrafficSpy::Payload.count
   end 
+
+  def test_does_not_process_request_for_unregistered_application
+# As an unregistered app,
+    post '/sources/turing/data', {payload: '{
+      "url":"http://jumpstartlab.com/blog",
+      "requestedAt":"2013-02-16 21:38:28 -0700",
+      "respondedIn":37,
+      "referredBy":"http://jumpstartlab.com",
+      "requestType":"GET",
+      "parameters":[],
+      "eventName": "socialLogin",
+      "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+      "resolutionWidth":"1920",
+      "resolutionHeight":"1280",
+      "ip":"63.29.38.211"
+    }'}
+    assert_equal 403, last_response.status
+    assert_equal "Identifier not registered", last_response.body
+    assert_equal 0, TrafficSpy::Payload.count   
+  end 
+
+#   def test_
+# # As a registered app,
+# # When I POST to '/sources/MY_ID/data',
+# # And the payload I provide has already been received,
+# # Then I receive a 403 Forbidden status,
+# # And I receive a descriptive message
+#   end 
 end 
 
 
