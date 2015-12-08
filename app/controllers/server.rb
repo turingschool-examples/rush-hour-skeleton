acc936bd24eef11c1d3ab937440cf41210358083
ToniRib <tonimarierib@gmail.com>
2015-12-08 11:56:21 -0700

    test: First test for registering app passes

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -1,7 +1,16 @@
+require 'json'
+
 module TrafficSpy
   class Server < Sinatra::Base
     get '/' do
       erb :index
+    end
+
+    post '/sources' do
+      # binding.pry
+      TrafficSpy::Application.create({ identifier_name: params[:applications][:identifier], root_url: params[:applications][:rootUrl] })
+      status 200
+      body JSON.generate(params[:applications].select { |k, v| k == 'identifier' })
     end
 
     not_found do
--- test/controllers/register_app_test.rb
+++ test/controllers/register_app_test.rb
@@ -2,16 +2,10 @@ require './test/test_helper'
 
 class RegisterAppTest < ControllerTest
   def test_register_app_with_valid_attributes
-  # As as guest,
-  # When I POST to '/sources'
-  # 'identifier=jumpstartlab&rootUrl=http://jumpstartlab.com'
-  post '/sources', { applications: { identifier: "turing", rootUrl: "http://turing.io"} }
-  # And I provide an identifier,
-  # And I provide a rootUrl,
-  # Then I receive a status of 200 OK,
-  assert_equal 200, last_response.status
-  assert_equal "{'identifier':'turing'}", last_response.body
-  assert_equal 1, Application.count
-  # And I receive a response of my identifier (as JSON)
+    post '/sources', { applications: { identifier: "turing", rootUrl: "http://turing.io"} }
+
+    assert_equal 200, last_response.status
+    assert_equal "{\"identifier\":\"turing\"}", last_response.body
+    assert_equal 1, TrafficSpy::Application.count
   end
 end
