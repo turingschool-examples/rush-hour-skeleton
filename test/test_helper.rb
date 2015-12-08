cedc0b44e6c4e380a46dd6e42aba3c0525c231e0
GregoryArmstrong <intergalacticgentleman@gmail.com>
2015-12-08 13:08:25 -0700

    added test helper modifications and rake file for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,20 @@ Bundler.require
 require File.expand_path("../../config/environment", __FILE__)
 require 'minitest/autorun'
 require 'capybara'
+require 'database_cleaner'
+
+DatabaseCleaner.strategy = :truncation, { except: %w[public.schema_migrations] }
 
 Capybara.app = TrafficSpy::Server
+
+class Minitest::Test
+
+  def setup
+    DatabaseCleaner.start
+  end
+
+  def teardown
+    DatabaseCleaner.clean
+  end
+  
+end
