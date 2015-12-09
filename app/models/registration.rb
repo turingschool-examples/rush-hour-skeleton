b24fe5c0eaabfc2ad9d3bb86bb2d30f8a8c21b90
Brant Wellman <BrantWellman@gmail.com>
2015-12-09 12:10:48 -0700

    Moved post registration logic to model

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,21 +1,19 @@
 module TrafficSpy
+  class RegistrationParser
 
+    def self.parsing_validating(params)
+      if params[:identifier] && params[:rootUrl]
+        user = User.new(identifier: params[:identifier], root_url: params[:rootUrl])
+        if user.save
+          id = params[:identifier]
+          {'identifier' => id}.to_json
+        else
+          status, body = [403, "Identifier already exists."]
+        end
+      else
+        status, body = [400, "Missing all required details."]
+      end
+    end
+
+  end
 end
-
-
-
-
-
-# if params[:identifier] && params[:rootUrl]
-#   user = User.new(identifier: params[:identifier], root_url: params[:rootUrl])
-#   if user.save
-#     id = params[:identifier]
-#     {'identifier': id}.to_json
-#   else
-#     status(403)
-#     "Identifier already exists."
-#   end
-# else
-#   status(400)
-#   "Missing all required details."
-# end
