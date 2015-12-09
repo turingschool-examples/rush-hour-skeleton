3e2b7719f1f33f660808a64b0b4871fe4454e0f8
Hector Huertas <hectorhuertas@gmail.com>
2015-12-09 12:59:19 -0700

    request parser

---
+++ app/lib/parser.rb
@@ -0,0 +1,55 @@
+class Parser
+attr_reader :payload,
+            :request_hash,
+            :id,
+            :url,
+            :timestamp,
+            :response_time,
+            :referral,
+            :verb,
+            :event,
+            :browser,
+            :os,
+            :resolution
+
+  def initialize(params)
+    # binding.pry
+    @payload      = JSON.parse(params.fetch('payload','{}'))
+    @request_hash = params['payload'] && sha1(params['payload'])
+    @id           = params['id']
+
+    @url            = payload['url']
+    @timestamp      = payload['requestedAt']
+    @response_time  = payload['respondedIn']
+    @referral       = payload['referredBy']
+    @verb           = payload['requestType']
+    @event          = payload['eventName']
+
+    @browser,
+    @os =           get_user_agent(payload)
+    @resolution =   get_resolution(payload)
+    #no using parameters nor ip
+  end
+
+  def sha1(string)
+    Digest::SHA1.hexdigest(string)
+  end
+
+  def get_resolution(payload)
+    width = payload['resolutionWidth']
+    height = payload['resolutionHeight']
+
+    width && height && "#{width}x#{height}"
+  end
+
+  def get_user_agent(payload)
+    # if agent = payload['userAgent']
+    #   user_agent = UserAgentParser.parse(agent)
+    #   [ user_agent.to_s, user_agent.os.to_s ]
+    # end
+
+    return nil unless agent = payload['userAgent']
+    agent = UserAgentParser.parse(agent)
+    [ agent.to_s, agent.os.to_s ]
+  end
+end
