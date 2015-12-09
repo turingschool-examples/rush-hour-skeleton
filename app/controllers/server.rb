8297fe173b8d8887e431b525ae29281f26ad1110
Steven Olson <steveoscaro@gmail.com>
2015-12-08 19:39:24 -0700

    added test and functionality for duplicate payloads

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -27,12 +27,17 @@ module TrafficSpy
     post '/sources/:id/data'  do |id|
       # binding.pry
       p_pams = JSON.parse(params["payload"])
-      identifier = params["id"]
-      user = User.find_by(identifier: identifier) #GET USER FROM TABLE
-      ua = UserAgent.parse(p_pams[:UserAgent])
       p_sha = Digest::SHA1.hexdigest(p_pams.to_s)
-      resolution = Resolution.find_or_create_by(dimension: "#{p_pams["resolutionWidth"]} x #{p_pams["resolutionHeight"]}")
-      Payload.create(resolution_id: resolution.id, user_id: user, url: p_pams["url"], requested_at: p_pams["requestedAt"], responded_in: p_pams["respondedIn"], referred_by: p_pams["referredBy"], request_type: p_pams["requestType"], parameters: p_pams["parameters"], event_name: p_pams["eventName"], user_agent: ua, ip: p_pams["ip"],  payload_sha: p_sha)
+      if Payload.find_by(payload_sha: p_sha)
+        status 403
+        body "This specific payload already exists in the database..."
+      else
+        identifier = params["id"]
+        user_id = User.find_by(identifier: identifier).id
+        ua = UserAgent.parse(p_pams[:UserAgent])
+        resolution = Resolution.find_or_create_by(dimension: "#{p_pams["resolutionWidth"]} x #{p_pams["resolutionHeight"]}")
+        Payload.create(user_id: user_id, resolution_id: resolution.id, url: p_pams["url"], requested_at: p_pams["requestedAt"], responded_in: p_pams["respondedIn"], referred_by: p_pams["referredBy"], request_type: p_pams["requestType"], parameters: p_pams["parameters"], event_name: p_pams["eventName"], user_agent: ua, ip: p_pams["ip"],  payload_sha: p_sha)
+      end
     end
   end
 end
--- test/controllers/user_receives_correct_response_when_submitting_payload.rb
+++ test/controllers/user_receives_correct_response_when_submitting_payload.rb
@@ -24,17 +24,30 @@ class PayloadTest < TrafficTest
     assert_equal 200, last_response.status
     assert_equal 1, (second_count - first_count)
   end
-  # {"url"=>"http://jumpstartlab.com/blog",
-  #  "requestedAt"=>"2013-02-16 21:38:28 -0700",
-  #  "respondedIn"=>37,
-  #  "referredBy"=>"http://jumpstartlab.com",
-  #  "requestType"=>"GET",
-  #  "parameters"=>[],
-  #  "eventName"=>"socialLogin",
-  #  "userAgent"=>
-  #   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
-  #  "resolutionWidth"=>"1920",
-  #  "resolutionHeight"=>"1280",
-  #  "ip"=>"63.29.38.211"}
+
+  def test_duplicate_payload_is_rejected
+    register_user('jumpstartlab')
+    post '/sources/jumpstartlab/data', payload_params
+    post '/sources/jumpstartlab/data', payload_params
+    response = "This specific payload already exists in the database..."
+    
+    assert_equal 403, last_response.status
+    assert_equal response, last_response.body
+
+  end
 
 end
+
+
+# {"url"=>"http://jumpstartlab.com/blog",
+#  "requestedAt"=>"2013-02-16 21:38:28 -0700",
+#  "respondedIn"=>37,
+#  "referredBy"=>"http://jumpstartlab.com",
+#  "requestType"=>"GET",
+#  "parameters"=>[],
+#  "eventName"=>"socialLogin",
+#  "userAgent"=>
+#   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+#  "resolutionWidth"=>"1920",
+#  "resolutionHeight"=>"1280",
+#  "ip"=>"63.29.38.211"}
