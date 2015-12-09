d428cbb9b732cba7bf6b736bbdd9f7323c43b0be
ToniRib <tonimarierib@gmail.com>
2015-12-09 12:25:43 -0700

    test: buff up user can view event index tests by checking for links

--- test/features/user_can_view_event_specific_details_test.rb
+++ test/features/user_can_view_event_specific_details_test.rb
@@ -1,6 +1,6 @@
 require './test/test_helper'
 
-class UserCanViewIndexOfAllEvents < FeatureTest
+class UserCanViewEventSpecificDetails < FeatureTest
   def register_turing_and_send_multiple_payloads
     TrafficSpy::Application.create(identifier: "turing", root_url: "http://turing.io")
 
@@ -92,55 +92,10 @@ class UserCanViewIndexOfAllEvents < FeatureTest
     end
   end
 
-  def test_user_can_view_list_of_all_events_grouped_by_count
+  def test_user_can_view_details_from_event_social_login_A
     register_turing_and_send_multiple_payloads
 
-    # As a registered user,
-    # When I visit '/sources/MY_ID/events',
-    # And events have been defined,
-    # Then I see the most received event to least received event,
-    # And I see hyperlinks of each event to view event specific data
-
-    visit '/sources/turing/events'
-
-    within 'ol li:nth-child(1)' do
-      assert page.has_content?('socialLoginB')
-      assert page.has_css?("a[href~='/sources/turing/events/socialLoginB']")
-    end
-
-    within 'ol li:nth-child(2)' do
-      assert page.has_content?('socialLoginA')
-      assert page.has_css?("a[href~='/sources/turing/events/socialLoginA']")
-    end
-
-    within 'ol li:nth-child(3)' do
-      assert page.has_content?('socialLoginC')
-      assert page.has_css?("a[href~='/sources/turing/events/socialLoginC']")
-    end
-  end
-
-  def test_displays_error_message_when_no_events_defined
-    TrafficSpy::Application.create(identifier: "turing", root_url: "http://turing.io")
-
-    payload_data = {
-      relative_path: "/blog",
-      requested_at: "2013-02-16 21:38:28 -0700",
-      responded_in: 80,
-      referred_by:"http://turing.io",
-      request_type:"GET",
-      operating_system: "Macintosh",
-      browser: "Chrome",
-      resolution: {width: "1920", height: "1280"},
-      ip_address:"63.29.38.211"
-    }
-
-    app = TrafficSpy::Application.find_by(identifier: "turing")
-    app.payloads.create(payload_data)
-
-    visit '/sources/turing/events'
-
-    within 'h2' do
-      assert page.has_content?('No events have been defined')
-    end
+    visit '/sources/turing/events/socialLoginA'
+    
   end
 end
