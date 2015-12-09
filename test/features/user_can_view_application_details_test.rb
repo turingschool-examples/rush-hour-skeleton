9eca514dd4cda96a6affca2994a3af12090ebdcc
Steve Pentler <stevepentler@gmail.com>
2015-12-09 14:36:49 -0700

    view application details test passing 41 assertions

--- test/features/user_can_view_application_details_test.rb
+++ test/features/user_can_view_application_details_test.rb
@@ -72,7 +72,7 @@ class UserCanViewApplicationDetailsTest < FeatureTest
     payload_data_6 = {
       relative_path: "/about",
       requested_at: "2013-02-16 21:38:28 -0700",
-      responded_in: 5,
+      responded_in: 25,
       referred_by:"http://turing.io",
       request_type:"GET",
       event: "socialLogin",
@@ -139,16 +139,62 @@ class UserCanViewApplicationDetailsTest < FeatureTest
       assert page.has_content?('Macintosh')
       assert page.has_content?(4)
     end
-    # And I see web browser breakdown across all requests (userAgent),
-    # And I see OS breakdown across all requests (userAgent),
-    # And I see screen Resolution across all requests (resolutionWidth x resolutionHeight),
-    # And I see the longest, average response time per URL to shortest, average response time per URL
-    # And I see hyperlinks of each url to view url specific data,
-    # And I see hyperlink to view aggregate event data
+
+    within 'ol#operating_system li:nth-child(2)' do
+      assert page.has_content?('Windows')
+      assert page.has_content?(2)
+    end
+
+    within 'ol#screen_resolution li:nth-child(1)' do
+      assert page.has_content?(':width=>"1920"')
+      assert page.has_content?(':height=>"1280"')
+      assert page.has_content?(3)
+    end
+
+    within 'ol#screen_resolution li:nth-child(2)' do
+      assert page.has_content?(':width=>"1920"')
+      assert page.has_content?(':height=>"1080"')
+      assert page.has_content?(1)
+    end
+
+    within 'ol#screen_resolution li:nth-child(3)' do
+      assert page.has_content?(':width=>"1366"')
+      assert page.has_content?(':height=>"768"')
+      assert page.has_content?(1)
+    end
+
+    within 'ol#screen_resolution li:nth-child(4)' do
+      assert page.has_content?(':width=>"600"')
+      assert page.has_content?(':height=>"800"')
+      assert page.has_content?(1)
+    end
+
+    within 'ol#average_response_times li:nth-child(1)' do
+      assert page.has_content?('/blog')
+      assert page.has_content?(55)
+    end
+
+    within 'ol#average_response_times li:nth-child(2)' do
+      assert page.has_content?('/team')
+      assert page.has_content?(40)
+    end
+
+    within 'ol#average_response_times li:nth-child(3)' do
+      assert page.has_content?('/about')
+      assert page.has_content?(25)
+    end
 
   end
 
   def test_user_gets_error_if_they_try_to_view_an_unregistered_page
-    skip
+    visit '/sources/galvanize'
+    assert page.has_content?("Identifier has not been registered")   
   end
+
+  def test_user_gets_error_without_payload 
+    TrafficSpy::Application.create(identifier: "turing", root_url: "http://turing.io")
+    visit '/sources/turing'
+    save_and_open_page
+    assert page.has_content?("No Payload data has been received for this source")
+  end 
 end
