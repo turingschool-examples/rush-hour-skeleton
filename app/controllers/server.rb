d08206b27f6d93f818c355d374756d7d3df4bb82
ToniRib <tonimarierib@gmail.com>
2015-12-09 14:15:10 -0700

    refactor: create ProcessPayload class

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -13,23 +13,10 @@ module TrafficSpy
     end
 
     post '/sources/:id/data' do |id|
-      if params[:payload].nil?
-        status 400
-        body "Payload can't be blank"
-      elsif TrafficSpy::Application.find_by(identifier: id).nil?
-        status 403
-        body "Identifier not registered"
-      else
-        parsed_string = TrafficSpy::ProcessRequestParser.new.parse_request(params[:payload])
-        app = TrafficSpy::Application.find_by(identifier: id)
-        payload = app.payloads.new(parsed_string)
-        if payload.save
-          status 200
-        else
-          status 403
-          body "Already received request"
-        end
-      end
+      response_status, response_body = TrafficSpy::ProcessPayload.new(params[:payload], id).process
+
+      status response_status
+      body response_body
     end
 
     get '/sources/:id' do |id|
