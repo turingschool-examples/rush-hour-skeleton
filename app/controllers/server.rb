2df460e1548d3a608115da11df63886eca21c9b7
Brant Wellman <BrantWellman@gmail.com>
2015-12-10 14:22:57 -0700

    Added first feature test for url stats

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -8,6 +8,13 @@ module TrafficSpy
       erb :error
     end
 
+    get '/sources/:id/urls/:relative_path' do |id, relative_path|
+      @user = TrafficSpy::User.find_by(identifier: id)
+      @full_path = @user.root_url + '/' + relative_path
+      # @user.payloads.url.
+      erb :url_data, locals: { relative_path: relative_path }
+    end
+
     get '/sources/:id' do |id|
       if @user = TrafficSpy::User.find_by(identifier: id)
         if @user.payloads.count == 0
@@ -19,7 +26,6 @@ module TrafficSpy
         erb :identifier_does_not_exist, locals: {id: id}
       end
     end
-
 
     post '/sources' do
       TrafficSpy::RegistrationParser.new(params).parsing_validating
---
+++ test/features/user_visits_url_stats_test.rb
@@ -0,0 +1,25 @@
+require_relative '../test_helper'
+
+class UserVisitsApplicationDetailsTest < FeatureTest
+  include PayloadPrep
+
+  def setup_testing_environment
+    post '/sources', {rootUrl: 'http://jumpstartlab.com', identifier: 'jumpstartlab'}
+
+    post '/sources/jumpstartlab/data', payload_params1
+    post '/sources/jumpstartlab/data', payload_params2
+    post '/sources/jumpstartlab/data', payload_params3
+
+    visit '/sources/jumpstartlab/urls/blog'
+  end
+
+  def test_user_sees_longest_response_time_for_url
+    setup_testing_environment
+
+    within('#longest_response_time') do
+      assert page.has_content?('Longest Response Time')
+      assert page.has_content?(90)
+    end
+  end
+
+end
