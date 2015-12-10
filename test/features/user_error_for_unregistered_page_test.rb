bb70fc2765f99f809e9e6454bd2c9d29a8422485
Steve Pentler <stevepentler@gmail.com>
2015-12-09 17:15:52 -0700

    renamed feature tests to comply with user stories

--- test/features/user_error_for_unregistered_page_test.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../test_helper'
-
-class UserErrorForUnregisteredPageTest < FeatureTest
-  def test_user_gets_error_if_they_try_to_view_an_unregistered_page
-    visit '/sources/galvanize'
-    assert page.has_content?("Identifier has not been registered")
-  end
-end 
--- test/features/user_error_viewing_application_details_with_missing_payload_test.rb
+++ test/features/user_error_viewing_application_details_with_missing_payload_test.rb
@@ -1,6 +1,6 @@
 require_relative '../test_helper'
 
-class UserErrorForMissingPayloadTest < FeatureTest
+class UserErrorViewingApplicationDetailsWithMissingPayloadTest < FeatureTest
   def test_user_gets_error_without_payload
     TrafficSpy::Application.create(identifier: "turing", root_url: "http://turing.io")
     visit '/sources/turing'
