4b6446340ebd0f316e13ddd813b025d0a08d7b83
ToniRib <tonimarierib@gmail.com>
2015-12-10 14:30:43 -0700

    fix: middle of fixing tests

--- test/features/user_error_for_event_without_payload_test.rb
+++ test/features/user_error_for_event_without_payload_test.rb
@@ -1,13 +1,12 @@
-  require './test/test_helper'
-  class UserErrorForEventWithoutPayloadTest < FeatureTest
+require './test/test_helper'
+class UserErrorForEventWithoutPayloadTest < FeatureTest
+  def test_displays_error_message_when_no_payload_submitted
+    TrafficSpy::Application.create(identifier: "turing", root_url: "http://turing.io")
 
-    def test_displays_error_message_when_no_payload_submitted
-      TrafficSpy::Application.create(identifier: "turing", root_url: "http://turing.io")
+    visit '/sources/turing/events'
 
-      visit '/sources/turing/events'
-
-      within 'h5' do
-        assert page.has_content?('No Payload data has been received for this source')
-      end
-    end 
+    within 'h5' do
+      assert page.has_content?('No Payload data has been received for this source')
+    end
   end
+end
