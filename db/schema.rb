4a509c24610153679a788546dbb094cd8189ffec
Beth Secor <elizabeth.a.secor@gmail.com>
2015-12-10 11:56:42 -0700

    test passing for relative_path table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209012722) do
+ActiveRecord::Schema.define(version: 20151210183824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,11 @@ ActiveRecord::Schema.define(version: 20151209012722) do
     t.string   "browser"
     t.string   "resolution"
     t.string   "ip_address"
+    t.integer  "relative_path_id"
+  end
+
+  create_table "relative_paths", force: :cascade do |t|
+    t.string "path"
   end
 
 end
