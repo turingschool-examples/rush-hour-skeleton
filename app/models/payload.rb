fb685851a4e4e82f9d77fbd97b5e0ff950b051c0
James Crockett <jecrockett@gmail.com>
2015-12-10 14:14:15 -0700

    all required info apperas on application details page

--- app/models/payload.rb
+++ app/models/payload.rb
@@ -6,4 +6,12 @@ class Payload < ActiveRecord::Base
   def self.url_requests
     group(:path).order('count_id desc').count('id')
   end
+
+  def self.url_response_times
+    group(:path).order('average_responded_in desc').average('responded_in')
+  end
+
+  def self.url_hyperlinks
+    pluck(:path)
+  end
 end
