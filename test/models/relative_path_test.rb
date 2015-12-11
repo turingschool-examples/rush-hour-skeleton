824481bfc3539da2f185604f266bfd2fa4440500
Beth Secor <elizabeth.a.secor@gmail.com>
2015-12-10 17:41:06 -0700

    begin migration of operating system

--- test/models/relative_path_test.rb
+++ test/models/relative_path_test.rb
@@ -3,19 +3,19 @@ require './test/test_helper'
 class RelativePathTest < ModelTest
   def test_create_relative_path_with_valid_parameters
     payload_data = {
-      relative_path: "/blog",
+      relative_path_string: "/blog",
       requested_at: "2013-02-16 21:38:28 -0700",
       responded_in: 37,
       referred_by:"http://turing.io",
       request_type_string:"GET",
       event: "socialLogin",
-      operating_system: "Macintosh",
+      operating_system_string: "Macintosh",
       browser: "Chrome",
-      resolution: {width: "1920", height: "1280"},
+      resolution_string: {width: "1920", height: "1280"},
       ip_address:"63.29.38.211"
       }
 
-    rel_path = TrafficSpy::RelativePath.find_or_create_by(path: payload_data[:relative_path])
+    rel_path = TrafficSpy::RelativePath.find_or_create_by(path: payload_data[:relative_path_string])
     assert_equal 1, TrafficSpy::RelativePath.count
     assert_equal "/blog", rel_path.path
   end
