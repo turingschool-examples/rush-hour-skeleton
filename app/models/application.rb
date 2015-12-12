f372ad2447475eb49b533ca24cfa32f08ef056f4
ToniRib <tonimarierib@gmail.com>
2015-12-12 12:11:57 -0700

    refactor: application details page

--- app/models/application.rb
+++ app/models/application.rb
@@ -3,6 +3,9 @@ module TrafficSpy
     has_many :payloads
     has_many :relative_paths, through: :payloads
     has_many :browsers, through: :payloads
+    has_many :operating_systems, through: :payloads
+    has_many :resolutions, through: :payloads
+
     validates_presence_of :identifier, :root_url
     validates_uniqueness_of :identifier
 
@@ -12,6 +15,19 @@ module TrafficSpy
 
     def browser_requests
       browsers.group(:browser_name).order({count: :desc, browser_name: :asc}).count
+    end
+
+    def operating_system_requests
+      operating_systems.group(:op_system).order({count: :desc, op_system: :asc}).count
+    end
+
+    def resolution_requests
+      resolutions.group(:width, :height).order({count: :desc, width: :asc}).count
+    end
+
+    def average_response_times
+      paths = payloads.group(:relative_path).average(:responded_in).map { |rel_path, count| [rel_path.path, count]}
+      paths.sort_by{ |_, v| -v }
     end
   end
 end
