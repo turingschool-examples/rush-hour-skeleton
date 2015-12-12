4df2949759d7e937d4348bf6f5967d00aa0077b8
GregoryArmstrong <intergalacticgentleman@gmail.com>
2015-12-12 10:45:11 -0700

    hour_by_hour method now correctly displays just the hour

--- app/models/payload.rb
+++ app/models/payload.rb
@@ -43,7 +43,7 @@ class Payload < ActiveRecord::Base
   end
 
   def self.hour_by_hour_breakdown
-    pluck(:requested_at).first.hour
+    pluck(:requested_at).map { |t| t.to_datetime.hour }
   end
 
 end
