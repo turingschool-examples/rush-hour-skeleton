14db5d50423031830c281001f6953378976e42c4
Dan Winter <dan.j.winter@gmail.com>
2015-12-12 14:35:30 -0700

    pulled out partials for app_stats and events_index

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -21,7 +21,11 @@ module TrafficSpy
 
     get '/sources/:id' do |id|
       if @user = TrafficSpy::User.find_by(identifier: id)
-        payload_routing(@user, id)
+        if @user.payloads.count == 0
+          erb :no_payload_data, locals: {id: id}
+        else
+          erb :application_statistics
+        end
       else
         erb :identifier_does_not_exist, locals: {id: id}
       end
