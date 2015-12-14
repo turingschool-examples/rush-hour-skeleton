3ef85dee10b78268bf1be88d37f768bc91c0bbb4
Hector Huertas <hectorhuertas@gmail.com>
2015-12-13 20:16:29 -0700

    refactor request manager

--- app/lib/request_manager.rb
+++ app/lib/request_manager.rb
@@ -1,29 +1,30 @@
 class RequestManager
-  attr_reader :status, :response
+  attr_reader :status, :response, :parser, :request, :params
 
   def initialize(params)
-    parser = Parser.new(params)
-    request = Request.new(request_hash: parser.request_hash)
+    @params  = params
+    @parser  = Parser.new(params)
+    @request = Request.new(request_hash: parser.request_hash)
 
-    if params[:payload].nil?
-      @status = 400
-      @response =  "Missing payload."
-    else params[:payload]
-      if Application.exists?(identifier: params[:id])
-        if request.valid?
-          Url.create(path: parser.url)
-          Event.create(name: parser.event)
-          Request.create(parser.request_data)
-          @status = 200
-          @response = "Success."
-        else
-          @status = 400
-          @response = "Duplicate entry."
-        end
-      else
-        @status = 403
-        @response = "Application not registered."   # parser
-      end
+    @status, @response = process_request
+  end
+
+  def process_request
+    case
+    when params[:payload].nil? then [400, "Missing payload."]
+    when unregistered_app?     then [403,"Application not registered."]
+    when request.invalid?      then [400, "Duplicate entry."]
+    when request.valid?        then create_records && [200, "Success."]
     end
+  end
+
+  def unregistered_app?
+    !Application.exists?(identifier: params[:id])
+  end
+
+  def create_records
+    Url.create(path: parser.url)
+    Event.create(name: parser.event)
+    Request.create(parser.request_data)
   end
 end
