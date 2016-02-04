2938248c3cb34b662aa8d040916fe8d0a009dc7f
Andrew Carmer <carmerandrew@gmail.com>
2016-02-04 15:24:09 -0700

    edit harness - working through iteration1

--- test/harness_tests/iteration0_test.rb
+++ test/harness_tests/iteration0_test.rb
@@ -1,25 +1,23 @@
-# require './test/test_helper'
-#
-# class Iteration0Test < Minitest::Test
-#
-#   def test_payload_request_exists
-#     assert PayloadRequest.new
-#   end
-#
-#   def test_payload_request_has_correct_attributes
-#     create_payload_request
-#
-#     payload = PayloadRequest.first
-#
-#     assert_respond_to payload, :url
-#     assert_respond_to payload, :requested_at
-#     assert_respond_to payload, :responded_in
-#     assert_respond_to payload, :referred_by
-#     assert_respond_to payload, :request_type
-#     assert_respond_to payload, :event_name
-#     assert_respond_to payload, :user_agent
-#     assert_respond_to payload, :resolution_width
-#     assert_respond_to payload, :resolution_height
-#     assert_respond_to payload, :ip
-#   end
-# end
+require './test/test_helper'
+
+class Iteration0Test < Minitest::Test
+
+  def test_payload_request_exists
+    assert PayloadRequest.new
+  end
+
+  def test_payload_request_has_correct_attributes
+    payload = PayloadRequest.new
+
+    assert_respond_to payload, :url
+    assert_respond_to payload, :requested_at
+    assert_respond_to payload, :responded_in
+    assert_respond_to payload, :referred_by
+    assert_respond_to payload, :request_type
+    assert_respond_to payload, :event_name
+    assert_respond_to payload, :user_agent
+    assert_respond_to payload, :resolution_width
+    assert_respond_to payload, :resolution_height
+    assert_respond_to payload, :ip
+  end
+end
