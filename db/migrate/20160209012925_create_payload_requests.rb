f56ca1a28aeb7e5aad0d93f824763fc279791688
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-02-08 18:56:34 -0700

    iteration 0 starting on Payload Request

--- db/migrate/20160209012925_create_payload_requests.rb
+++ db/migrate/20160209012925_create_payload_requests.rb
@@ -1,4 +1,17 @@
 class CreatePayloadRequests < ActiveRecord::Migration
   def change
+    create_table :payload_requests do |t|
+      t.string  :url
+      t.date    :requested_at
+      t.integer :responded_in
+      t.string  :referred_by
+      t.string  :request_type
+      t.string  :parameters, array: true, default: []
+      t.string  :event_name
+      t.string  :user_agent
+      t.string  :resolution_width
+      t.string  :resolution_height
+      t.string  :ip
+    end
   end
 end
