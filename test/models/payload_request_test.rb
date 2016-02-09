ae98dbe2e58a60520058d5da87204b4e08d2af17
Scott Firestone <scott.firestone.dev@gmail.com>
2016-02-09 11:35:31 -0700

    add attribute value test to payload request test

--- test/models/payload_request_test.rb
+++ test/models/payload_request_test.rb
@@ -21,4 +21,25 @@ class PayloadRequestTest < Minitest::Test
 
     assert_equal 0, PayloadRequest.all.count
   end
+
+  def test_payload_request_has_attribute_values
+    payload = {
+      "url":"http://jumpstartlab.com/blog",
+      "requestedAt":"2013-02-16 21:38:28 -0700",
+      "respondedIn":37,
+      "referredBy":"http://jumpstartlab.com",
+      "requestType":"GET",
+      "parameters":[],
+      "eventName": "socialLogin",
+      "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+      "resolutionWidth":"1920",
+      "resolutionHeight":"1280",
+      "ip":"63.29.38.211"
+    }
+    p = PayloadRequest.new(respondedIn: payload[:respondedIn], 
+                           requestedAt: payload[:requestedAt])
+
+    assert_equal "2013-02-16 21:38:28 -0700", p.requestedAt
+    assert_equal 37, p.respondedIn
+  end
 end
