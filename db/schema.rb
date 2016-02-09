f25d125a1dd0582c889f25a56275bb7978cf007e
Scott Firestone <scott.firestone.dev@gmail.com>
2016-02-09 15:58:28 -0700

    add event name column to payload requests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160209210519) do
+ActiveRecord::Schema.define(version: 20160209224511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160209210519) do
     t.integer "referrer_id"
     t.integer "url_request_id"
     t.integer "user_data_id"
+    t.string  "eventName"
   end
 
   create_table "referrers", force: :cascade do |t|
@@ -34,4 +35,5 @@ ActiveRecord::Schema.define(version: 20160209210519) do
     t.string "requestType"
     t.string "parameters"
   end
+
 end
