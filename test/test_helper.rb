b7991f4eca4552dcfca40c8af832418216d8aa00
Kimiko Kano <kskano@gmail.com>
2016-02-09 10:38:15 -0700

    added rake test, TestHelpers, and tmp folder

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,3 +9,23 @@ require 'minitest/pride'
 require 'capybara/dsl'
 
 Capybara.app = RushHour::Server
+Capybara.save_and_open_page_path = "tmp/capybara"
+
+DatabaseCleaner.strategy = :truncation, {except: %w[public.schema_migrations]}
+
+module TestHelpers
+  def setup
+    DatabaseCleaner.start
+    super
+  end
+
+  def teardown
+    DatabaseCleaner.clean
+    super
+  end
+end
+
+class FeatureTest < Minitest::Test
+  include Capybara::DSL
+  include TestHelpers
+end
