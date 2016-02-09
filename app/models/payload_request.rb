41ee3bdcc7068c68e6c54a46e0f73e05c4290c75
Nathan Venn <natevenn@gmail.com>
2016-02-08 19:15:38 -0700

    validate payload request

--- app/models/payload_request.rb
+++ app/models/payload_request.rb
@@ -1,2 +1,13 @@
 class PayloadRequest < ActiveRecord::Base
+  validates :url, presence: true
+  validates :requested_at, presence: true
+  validates :requested_in, presence: true
+  validates :referred_by, presence: true
+  validates :request_type, presence: true
+  validates :parameters, presence: true
+  validates :event_name, presence: true
+  validates :user_agent, presence: true
+  validates :resolution_width, presence: true
+  validates :resolution_height, presence: true
+  validates :ip, presence: true
 end
