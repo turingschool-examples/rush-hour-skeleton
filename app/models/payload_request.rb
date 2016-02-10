291eb0b6bda29930bb880003b0a9891fa72ebfe4
Kimiko Kano <kskano@gmail.com>
2016-02-10 11:12:41 -0700

    refactored payload request validations

--- app/models/payload_request.rb
+++ app/models/payload_request.rb
@@ -1,17 +1,27 @@
 class PayloadRequest < ActiveRecord::Base
   # belongs_to :url
-
-  validates :url, presence: true
-  validates :requestedAt, presence: true
-  validates :respondedIn, presence: true
-  validates :referredBy, presence: true
-  validates :requestType, presence: true
-  validates :parameters, presence: true
-  validates :eventName, presence: true
-  validates :userAgent, presence: true
-  validates :resolutionWidth, presence: true
-  validates :resolutionHeight, presence: true
-  validates :ip, presence: true
+  validates_presence_of :url,
+                        :requestedAt,
+                        :respondedIn,
+                        :referredBy,
+                        :requestType,
+                        :parameters,
+                        :eventName,
+                        :userAgent,
+                        :resolutionWidth,
+                        :resolutionHeight,
+                        :ip
+  # validates :url, presence: true
+  # validates :requestedAt, presence: true
+  # validates :respondedIn, presence: true
+  # validates :referredBy, presence: true
+  # validates :requestType, presence: true
+  # validates :parameters, presence: true
+  # validates :eventName, presence: true
+  # validates :userAgent, presence: true
+  # validates :resolutionWidth, presence: true
+  # validates :resolutionHeight, presence: true
+  # validates :ip, presence: true
 
   def self.average_response_time
     average(:respondedIn).to_i
