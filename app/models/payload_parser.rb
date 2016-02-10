70fc58f4fca74e65e57aa7c46a0139816c8a72a4
Erinna Chen <erinna.chen@gmail.com>
2016-02-10 13:39:31 -0700

    Added Parser to parse payload

--- app/models/payload_parser.rb
+++ app/models/payload_parser.rb
@@ -1,3 +1,4 @@
+require 'user_agent_parser'
 class PayloadParser
   # attr_reader :data
   #
@@ -6,6 +7,7 @@ class PayloadParser
   # end
 
   def self.parse(data)
+    ua = UserAgentParser.parse(data[:userAgent])
     ({
       url: Url.find_or_create_by(address: data[:url]),
       requested_at: data[:requestedAt],
@@ -13,9 +15,9 @@ class PayloadParser
       referrer: Referrer.find_or_create_by(address: data[:referredBy]),
       request: Request.find_or_create_by(verb: data[:requestType]),
       event: Event.find_or_create_by(name: data[:eventName]),
-      user_agent: UserAgent.find_or_create_by(browser: "Chrome", platform: "Macintosh"),
-      resolution: Resolution.find_or_create_by(width: "1920", height: "1280"),
-      ip: Ip.find_or_create_by(address: "63.29.38.211")
+      user_agent: UserAgent.find_or_create_by(browser: ua.family, platform: ua.os.to_s),
+      resolution: Resolution.find_or_create_by(width: data[:resolutionWidth], height: data[:resolutionHeight]),
+      ip: Ip.find_or_create_by(address: data[:ip])
     })
   end
 
