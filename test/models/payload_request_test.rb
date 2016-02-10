ab00c1b4490e53d4bcf4c743d44bc8a2c8c3f2e9
Brian Rippeto <rippeto@gmail.com>
2016-02-09 17:36:47 -0700

    prettified

--- test/models/payload_request_test.rb
+++ test/models/payload_request_test.rb
@@ -2,7 +2,7 @@ require_relative '../test_helper'
 
 class PayloadRequestTest < Minitest::Test
   include TestHelpers
-  
+
   def test_payload_request_assigned_correctly
     payload = {
       "url": "http://jumpstartlab.com/blog",
@@ -41,11 +41,44 @@ class PayloadRequestTest < Minitest::Test
   end
 
   def create_three_payloads
-    payload1 = {"url":"http://jumpstartlab.com/blog","requestedAt":"2013-02-16 21:38:28 -0700","respondedIn":30,"referredBy":"http://jumpstartlab.com","requestType":"GET","parameters":[],"eventName": "socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.38.211"}
+    payload1 = {"url": "http: //jumpstartlab.com/blog",
+      "requestedAt": "2013-02-16 21: 38: 28 -0700",
+      "respondedIn": 30,
+      "referredBy": "http: //jumpstartlab.com",
+      "requestType": "GET",
+      "parameters": [],
+      "eventName":  "socialLogin",
+      "userAgent": "Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML,
+       like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+      "resolutionWidth": "1920",
+      "resolutionHeight": "1280",
+      "ip": "63.29.38.211"}
 
-    payload2 = {"url":"http://jumpstartlab.com/tutorials","requestedAt":"2013-02-17 20:44:28 -0700","respondedIn":40,"referredBy":"http://jumpstartlab.com","requestType":"GET","parameters":[],"eventName": "socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.38.211"}
+    payload2 = {"url": "http: //jumpstartlab.com/tutorials",
+      "requestedAt": "2013-02-17 20: 44: 28 -0700",
+      "respondedIn": 40,
+      "referredBy": "http: //jumpstartlab.com",
+      "requestType": "GET",
+      "parameters": [],
+      "eventName":  "socialLogin",
+      "userAgent": "Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML,
+       like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+      "resolutionWidth": "1920",
+      "resolutionHeight": "1280",
+      "ip": "63.29.38.211"}
 
-    payload3 = {"url":"http://jumpstartlab.com/about","requestedAt":"2013-02-17 10:34:18 -0700","respondedIn":50,"referredBy":"http://google.com","requestType":"GET","parameters":[],"eventName": "socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.38.211"}
+    payload3 = {"url": "http: //jumpstartlab.com/about",
+      "requestedAt": "2013-02-17 10: 34: 18 -0700",
+      "respondedIn": 50,
+      "referredBy": "http: //google.com",
+      "requestType": "GET",
+      "parameters": [],
+      "eventName":  "socialLogin",
+      "userAgent": "Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML,
+       like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+      "resolutionWidth": "1920",
+      "resolutionHeight": "1280",
+      "ip": "63.29.38.211"}
 
     pr1 = PayloadRequest.create(payload1)
     pr2 = PayloadRequest.create(payload2)
