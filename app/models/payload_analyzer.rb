1eeae8508c958410b4f116c778012d9ae2fccd8e
Allan <amaxwellblair@gmail.com>
2016-02-09 18:23:22 -0700

    fixed parser

--- app/models/payload_analyzer.rb
+++ app/models/payload_analyzer.rb
@@ -2,31 +2,37 @@ require 'user_agent_parser'
 
 class PayloadAnalyzer
 
-  def parse(raw_payload)
-    # incoming_keys = [:requestType, :userAgent, :resolutionWidth, :resolutionHeight]
-    # user_agent = UserAgentParser.parse(raw_payload[:userAgent])
-    # os = user_agent.os.to_s
-    # browser = user_agent.to_s
-    # resolution = resolution_parse(raw_payload)
-    # verb = raw_payload[:requestType]
-    # formatted_payload = {:request_type => verb, :os => os, :browser => browser, :screen_resolution => resolution}
-    # formatted_payload.merge!(raw_payload)
-    # incoming_keys.each do |key|
-    #   formatted_payload.delete(key)
-    # end
-    # return formatted_payload
+  incoming_keys = [:requestType, :userAgent, :resolutionWidth, :resolutionHeight]
+  user_agent = UserAgentParser.parse(raw_payload.delete(:userAgent))
+  os = user_agent.os
+  browser = user_agent.to_s
+  resolution = resolution_parse(raw_payload.delete(:resolutionWidth), raw_payload.delete(:resolutionHeight))
+  verb = raw_payload.delete(:requestType)
+  referred = raw_payload.delete(:referredBy)
+  url = raw_payload.delete(:url)
+  ip = raw_payload.delete(:ip)
+  eventname = raw_payload.delete(:eventName)
 
+  models = [RequestType, ScreenResolution, Referred, Ip, Url, EventName, UserAgent]
+  values = [{verb: verb}, {size: resolution}, {name: referred}, {address: ip}, {route: url}, {name: eventname}, {os: os, browser: browser}]
+  payload = Payload.Create(raw_payload)
 
-    request_type = RequestType.create(requestType: raw_payload["requestType"])
-    # the Rest
-    payload = Payload.create(request_type_id: request_type.id)
-    # request_type.payloads << payload
-
-    # find_or_create_by()
+  models.each_with_index do |model, index|
+    model.find_or_create_by(model.find_or_create_by(values[index]))
   end
 
-  def resolution_parse(raw_payload)
-    raw_payload[:resolutionWidth] + "x" + raw_payload[:resolutionHeight]
+
+  # RequestType.find_or_create_by(verb: verb) << payload
+  # ScreenResolution.find_or_create_by(size: resolution) << payload
+  # Referred.find_or_create_by(name: referred) << payload
+  # Ip.find_or_create_by(address: ip) << payload
+  # Url.find_or_create_by(route: url) << payload
+  # EventName.find_or_create_by(name: eventname) << payload
+  # UserAgent.find_or_create_by(os: os, browser: browser) << payload
+
+
+  def resolution_parse(width, height)
+    width + "x" + height
   end
 
 end
