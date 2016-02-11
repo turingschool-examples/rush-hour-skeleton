bf5844b5ed9833f38da47e3904aaa12fe1c77c52
Scott Firestone <scott.firestone.dev@gmail.com>
2016-02-11 11:08:27 -0700

    add relationship client to referrers through payload requests

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,8 +1,8 @@
 class Client < ActiveRecord::Base
 
   has_many :payload_requests
-  has_many :ip_addresses,              through: :payload_requests
-
+  has_many :ip_addresses,    through: :payload_requests
+  has_many :referrers,       through: :payload_requests
 
   validates :identifier, presence: true
   validates :root_url,   presence: true
--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -53,4 +53,24 @@ class ClientTest < Minitest::Test
     assert client.ip_addresses.pluck(:ip).include?("127.0.0.1")
     assert client.ip_addresses.pluck(:ip).include?("127.0.0.9")
   end
+
+  def test_has_referrrers
+    client = Client.create(identifier: "jumpstartlabs", root_url: "http://www.jumpstartlabs.com")
+
+
+    client.payload_requests.create(requested_at: "first_req",
+                                   responded_in: 30,
+                                   event_name:   "first_event",
+                                   referrer_id: Referrer.create(referred_by: "google.com/1").id)
+
+    client.payload_requests.create(requested_at: "second_req",
+                                   responded_in: 35,
+                                   event_name:   "second_event",
+                                   referrer_id: Referrer.create(referred_by: "google.com/9").id)
+
+    assert_equal 2, client.referrers.size
+    assert          client.referrers.pluck(:referred_by).include?("google.com/1")
+    assert          client.referrers.pluck(:referred_by).include?("google.com/9")
+  end
+
 end
