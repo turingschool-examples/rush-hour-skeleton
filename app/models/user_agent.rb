864552e4cc1186155169219120c3b9e9de4668a9
Nathan Venn <natevenn@gmail.com>
2016-02-11 10:53:16 -0700

    move os breakdown from payload request to user agent

--- app/models/user_agent.rb
+++ app/models/user_agent.rb
@@ -1,6 +1,10 @@
 class UserAgent < ActiveRecord::Base
   has_many :payload_requests
-  
+
   validates :browser, presence: true
   validates :os, presence: true
+
+  def self.os_breakdown
+    group(:os).pluck(:os)
+  end
 end
