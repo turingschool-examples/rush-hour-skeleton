3c70255524080c5c1ff8d0fd5e579083da9881be
Kimiko Kano <kskano@gmail.com>
2016-02-10 19:41:47 -0700

    wrote three methods and tests for url class

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,16 @@
 class Url < ActiveRecord::Base
   has_many :payload_requests
   validates :address, presence: true
+
+  def max_response_time
+    payload_requests.maximum(:responded_in)
+  end
+
+  def min_response_time
+    payload_requests.minimum(:responded_in)
+  end
+
+  def all_response_times
+    payload_requests.group(:responded_in).count.keys.sort.reverse
+  end
 end
--- test/models/payload_request_test.rb
+++ test/models/payload_request_test.rb
@@ -10,51 +10,51 @@ class PayloadRequestTest < Minitest::Test
 
   def create_three_payloads
     payload1 = {
-      url_id:           Url.create(address: "http://jumpstartlab.com/blog").id,
+      url_id:           Url.find_or_create_by(address: "http://jumpstartlab.com/blog").id,
       requested_at:     "2013-02-16 21:38:28 -0700",
       responded_in:     30,
-      referrer_url_id:  ReferrerUrl.create(url_address: "http://jumpstartlab.com").id,
-      request_type_id:  RequestType.create(verb: "GET").id,
+      referrer_url_id:  ReferrerUrl.find_or_create_by(url_address: "http://jumpstartlab.com").id,
+      request_type_id:  RequestType.find_or_create_by(verb: "GET").id,
       parameters:       [],
-      event_name_id:    EventName.create(event_name: "socialLogin").id,
-      user_agent_id:    UserAgent.create(browser: "Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML,
+      event_name_id:    EventName.find_or_create_by(event_name: "socialLogin").id,
+      user_agent_id:    UserAgent.find_or_create_by(browser: "Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML,
        like Gecko) Chrome/24.0.1309.0 Safari/537.17").id,
-      resolution_id:    Resolution.create(
+      resolution_id:    Resolution.find_or_create_by(
                           width: "1920",
                           height: "1280").id,
-      ip_id:            Ip.create(ip_address: "63.29.38.211").id
+      ip_id:            Ip.find_or_create_by(ip_address: "63.29.38.211").id
     }
 
     payload2 = {
-      url_id:           Url.create(address: "http://jumpstartlab.com/tutorials").id,
+      url_id:           Url.find_or_create_by(address: "http://jumpstartlab.com/tutorials").id,
       requested_at:     "2014-02-16 21:38:28 -0700",
       responded_in:     40,
-      referrer_url_id:  ReferrerUrl.create(url_address: "http://jumpstartlab.com").id,
-      request_type_id:  RequestType.create(verb: "POST").id,
+      referrer_url_id:  ReferrerUrl.find_or_create_by(url_address: "http://jumpstartlab.com").id,
+      request_type_id:  RequestType.find_or_create_by(verb: "POST").id,
       parameters:       [],
-      event_name_id:    EventName.create(event_name: "socialLogin").id,
-      user_agent_id:    UserAgent.create(browser: "Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML,
+      event_name_id:    EventName.find_or_create_by(event_name: "socialLogin").id,
+      user_agent_id:    UserAgent.find_or_create_by(browser: "Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML,
        like Gecko) Chrome/24.0.1309.0 Safari/537.17").id,
-      resolution_id:    Resolution.create(
+      resolution_id:    Resolution.find_or_create_by(
                           width: "1920",
                           height: "1280").id,
-      ip_id:            Ip.create(ip_address: "63.29.38.211").id
+      ip_id:            Ip.find_or_create_by(ip_address: "63.29.38.211").id
     }
 
     payload3 = {
-      url_id:           Url.create(address: "http://jumpstartlab.com").id,
+      url_id:           Url.find_or_create_by(address: "http://jumpstartlab.com").id,
       requested_at:     "2015-02-16 21:38:28 -0700",
       responded_in:     50,
-      referrer_url_id:  ReferrerUrl.create(url_address: "http://jumpstartlab.com").id,
-      request_type_id:  RequestType.create(verb: "GET").id,
+      referrer_url_id:  ReferrerUrl.find_or_create_by(url_address: "http://jumpstartlab.com").id,
+      request_type_id:  RequestType.find_or_create_by(verb: "GET").id,
       parameters:       [],
-      event_name_id:    EventName.create(event_name: "socialLogin").id,
-      user_agent_id:    UserAgent.create(browser: "Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML,
+      event_name_id:    EventName.find_or_create_by(event_name: "socialLogin").id,
+      user_agent_id:    UserAgent.find_or_create_by(browser: "Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML,
        like Gecko) Chrome/24.0.1309.0 Safari/537.17").id,
-      resolution_id:    Resolution.create(
+      resolution_id:    Resolution.find_or_create_by(
                           width: "1920",
                           height: "1280").id,
-      ip_id:            Ip.create(ip_address: "63.29.38.211").id
+      ip_id:            Ip.find_or_create_by(ip_address: "63.29.38.211").id
     }
 
     pr1 = PayloadRequest.create(payload1)
@@ -62,9 +62,6 @@ class PayloadRequestTest < Minitest::Test
     pr3 = PayloadRequest.create(payload3)
     [pr1, pr2, pr3]
   end
-
-
-
 
   def test_payload_request_assigns_all_values_correctly
     pr = create_three_payloads[0]
@@ -106,46 +103,5 @@ class PayloadRequestTest < Minitest::Test
     pr1, pr2, pr3 = create_three_payloads
 
     assert_equal 40, PayloadRequest.average_response_time
-  end
-
-  def test_can_return_specific_url_max_response_time
-    skip
-    assert_equal expected, PayloadRequest.url_max_response_time(url)
-  end
-
-  def test_can_return_specific_url_min_response_time
-    skip
-
-    assert_equal expected, PayloadRequest.url_min_response_time(url)
-  end
-
-  def test_returns_all_response_times_from_longest_to_shortest_for_specific_url
-    skip
-
-    assert_equal expected, PayloadRequest.url_all_response_times(url)
-  end
-
-  def test_can_return_average_response_for_specific_url
-    skip
-
-    assert_equal expected, PayloadRequest.url_average_response_times(url)
-  end
-
-  def test_returns_http_verbs_for_specific_url
-    skip
-
-    assert_equal expected, PayloadRequest.url_http_verbs(url)
-  end
-
-  def test_returns_three_most_popular_referrers_for_specific_url
-    skip
-
-    assert_equal expected, PayloadRequest.url_top_three_referrers(url)
-  end
-
-  def test_returns_three_most_popular_user_agents_for_specific_url
-    skip
-
-    assert_equal expected, PayloadRequest.url_top_three_user_agents(url)
   end
 end
