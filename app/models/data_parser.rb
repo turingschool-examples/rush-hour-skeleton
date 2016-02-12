14f0ed726912374ecf1f20d6bc4155fb1af77331
hhoopes <heidih@gmail.com>
2016-02-12 01:16:56 -0700

    Messing around with sources/post view and data parser class

---
+++ app/models/data_parser.rb
@@ -0,0 +1,28 @@
+require 'json'
+require_relative './client'
+
+class DataParser
+  def parse_client(params)
+    if params.has_key?("identifier") && params.has_key?("rootUrl")
+      new_identifier  = params["identifier"]
+      rootUrl    = params["rootUrl"]
+      binding.pry
+      client = Client.select(:clients).where(:identifier => new_identifier).first_or_create do |variable|
+      end
+      # client = create_client(new_identifier, root_url)
+      if client.save
+        [200, '{"identifier":"#{new_identifier}"}']
+      else
+        [403, "Please choose an identifier that's not already in use."]
+      end
+    else
+      [400, "Please provide both a identifer and root url parameter."]
+    end
+  end
+
+  def create_client(new_identifer, root_url)
+    Client.select(:clients).where(:identifier => new_identifier).first_or_create do |variable|
+      client.root_url = root_url
+    end
+  end
+end
---
+++ test/controllers/register_application_test.rb
@@ -0,0 +1,38 @@
+require 'json'
+require_relative '../test_helper'
+
+class RegisterApplication <Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    RushHour::Server
+  end
+
+  def setup
+    DatabaseCleaner.start
+    super
+  end
+
+  def teardown
+    DatabaseCleaner.clean
+    super
+  end
+
+  def test_post_request_with_valid_attributes_returns_200_status
+    post '/sources', {
+        identifier: "jumpstartlab",
+        root_url: "http://jumpstartlab.com"
+      }
+    assert_equal 1, Payload.count
+    assert_equal 200, last_response.status
+    assert_equal '{"identifier":"jumpstartlab"}', last_response.body
+  end
+
+  def test_post_request_with_existing_identifier_returns_403_error
+  end
+
+  def test_post_with_missing_parameter_returns_400_bad_request
+
+  end
+
+end
