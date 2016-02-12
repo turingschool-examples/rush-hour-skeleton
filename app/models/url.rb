fa666f455a94fbdc0c058f23de6089c56d2228cf
David Whitaker <damwhit@gmail.com>
2016-02-11 18:24:02 -0700

    all tests passing except instance methods

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,11 +1,36 @@
 class Url < ActiveRecord::Base
   has_many :payloads
 
-  validates :address, presence: true
+  validates :address, presence: true, uniqueness: true
 
   def self.list_frequency_urls
-    self.group(:address).order("count_address desc").count("address")
+    self.joins(:payloads).group("urls.address").order(count: :desc).count
   end
 
-  
+  def self.max_response_time_given_url(single_url)
+    self.joins(:url).where("address = ?", single_url).maximum(:response_time)
+    # payloads.pluck(:response_time).max - something like this
+  end
+
+  def self.min_response_time_given_url(single_url)
+    self.joins(:url).where("address = ?", single_url).minimum(:response_time)
+  end
+
+  def self.all_response_times_given_url(single_url)
+    self.joins(:url).where("address = ?", single_url).order(:response_time).reverse_order.pluck(:response_time)
+  end
+
+  def self.average_response_time_given_url(single_url)
+    self.joins(:url).where("address = ?", single_url).average(:response_time).to_f
+  end
+
+  def self.verbs_given_url(single_url)
+    self.joins(:url).where("address = ?", single_url).joins(:request_type).pluck(:verb)
+  end
+
+  def self.three_most_popular_referrers(single_url)
+      # self.select("url(address) as url_address, refer(address) as refer_address")
+      self.joins(:url).where("url(address) as url_address = ?", single_url).joins(:refer).order("count_address desc").count(" address")
+      url.refers.max.take(3)
+  end
 end
--- test/models/url_test.rb
+++ test/models/url_test.rb
@@ -29,5 +29,52 @@ class UrlTest < Minitest::Test
     assert_equal expected, Url.list_frequency_urls
   end
 
+  def test_returns_max_response_time_for_given_url
+    client_url = "http://jumpstartlab.com"
+    setup_1
+    time = Url.max_response_time_given_url(client_url)
+    assert_equal 30, time
+  end
+
+  def test_returns_min_response_time_for_given_url
+    skip
+    client_url = "http://jumpstartlab.com/jumps"
+    setup_1
+    time = Url.min_response_time_given_url(client_url)
+    assert_equal 40, time
+  end
+
+  def test_returns_all_response_times_for_given_url_sorted_by_length
+    skip
+    client_url = "http://jumpstartlab.com"
+    setup_1
+    times = Url.all_response_times_given_url(client_url)
+    assert_equal [30, 20], times
+    assert times.first > times.last
+  end
+
+  def test_return_average_response_time_for_given_url
+    skip
+    client_url = "http://jumpstartlab.com"
+    setup_1
+    time = Url.average_response_time_given_url(client_url)
+    assert_equal 25.0, time
+  end
+
+  def test_HTTP_verbs_used_to_acquire_this_url_returns_array
+    skip
+    client_url = "http://jumpstartlab.com"
+    setup_1
+    verbs = Url.verbs_given_url(client_url)
+    assert_equal ["POST", "GET"], verbs
+  end
+
+  def test_three_most_popular_refers
+    skip
+    client_url = "http://jumpstartlab.com"
+    setup_1
+    referrers = Url.three_most_popular_referrers(client_url)
+    assert_equal ["http://google.com", "http://turing.io"], referrers
+  end
 
 end
