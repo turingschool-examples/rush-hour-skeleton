aff7a830640567f5224678474477382d9ffb62c1
Kimiko Kano <kskano@gmail.com>
2016-02-13 18:18:01 -0700

    updated url specific feature test

--- test/features/user_can_view_url_specific_stats.rb
+++ test/features/user_can_view_url_specific_stats.rb
@@ -2,17 +2,11 @@ require_relative '../test_helper'
 
 class UserCanViewUrlSpecificStats < FeatureTest
   def test_successfully
-    # As a non logged in user
-    # When I visit the home page
-    visit '/'
-    # and I enter my user name
-    fill_in('app[identifier]', with 'jumpstartlab')
-    # And I click submit
-    click_button "Submit"
-    # then I should see the stats page
-    assert_equal '/sources/jumpstartlab', current_path
+    # As a registered user
+    # When I visit my user page
+    visit '/sources/jumpstartlab'
     # and I click on a specific url link
-    click_link ('http://jumpstartlab.com/about')
+    click_link ('/about')
     assert_equal '/sources/jumpstartlab/urls/about', current_path
     # then I should see a page that shows the url specific stats
     within("#url-specific-stats") do
@@ -27,20 +21,14 @@ class UserCanViewUrlSpecificStats < FeatureTest
   end
 
   def test_url_does_not_exists
-    # As a non logged in user
-    # When I visit the home page
-    visit '/'
-    # and I enter my user name
-    fill_in('app[identifier]', with 'jumpstartlab')
-    # And I click submit
-    click_button "Submit"
-    # then I should see the stats page
-    assert_equal '/sources/jumpstartlab', current_path
+    # As a registered user
+    # When I visit my user page
+    visit '/sources/jumpstartlab'
     # and if I enter a url that does not exists
     assert_equal '/error', current_path
     # then I should see an error message
     within('#missing-url') do
-      assert page.has_content? "Error, that url does not exist"
+      assert page.has_content? "This url has not been requested"
     end
   end
 end
