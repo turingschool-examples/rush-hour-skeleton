30509553a2c4708b6c238c88ff1c53933cd95fe5
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-02-15 16:17:31 -0700

    adding method to parser-path

--- app/lib/path_parser.rb
+++ app/lib/path_parser.rb
@@ -1,19 +1,16 @@
 require 'pry'
 
-class PathParser < Sinatra::Base
+class PathParser
 
-  # def self.sources_parser(client, params)
-  #   if client.save
-  #     status 200
-  #     body "{\"identifier\":\"#{client.identifier}\"}"
-  #   elsif params["rootUrl"].nil? || params["identifier"].nil?
-  #     status 400
-  #     body "Missing Parameters"
-  #   else
-  #     status 403
-  #     body client.errors.full_messages.join(", ")
-  #   end
-  # end
+  def self.sources_parser(client, params)
+    if client.save
+      {"status" => 200, "body" => "{\"identifier\":\"#{client.identifier}\"}"}
+    elsif params["rootUrl"].nil? || params["identifier"].nil?
+      {"status" => 400, "body" => "Missing Parameters"}
+    else
+      {"status" => 403, "body" => client.errors.full_messages.join(", ")}
+    end
+  end
 
   def self.sources_identifier_parse(payloads, client)
     if client.nil?
@@ -25,4 +22,12 @@ class PathParser < Sinatra::Base
     end
   end
 
+  # def self.relative_path_parser(client, identifier, relative_path)
+  #   url = client.root_url + '/' + relative_path
+  #   unless Url.pluck(:address).include?(url)
+  #     redirect '/missing-url'
+  #   else
+  #     @url_obj = Url.where(address: url).first
+  #     erb :url_stats
+  #   end
 end
