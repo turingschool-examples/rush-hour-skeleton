d7a8a9d560d2b14fd6e804ace74cb74b370ad029
Josh Washke <jwashke@icloud.com>
2016-03-29 15:10:17 -0600

    response time analysis complete

--- app/models/response_time.rb
+++ app/models/response_time.rb
@@ -1,3 +1,15 @@
 class ResponseTime < ActiveRecord::Base
   validates :time, presence: true
+
+  def self.avg
+    self.average("time")
+  end
+
+  def self.max
+    self.maximum("time")
+  end
+
+  def self.min
+    self.minimum("time")
+  end
 end
--- test/models/response_time_test.rb
+++ test/models/response_time_test.rb
@@ -17,4 +17,24 @@ class ResponseTimeTest < Minitest::Test
     assert_equal [], ResponseTime.all.to_a
   end
 
+  def test_it_can_return_average_response_time_across_all_requests
+    ResponseTime.create(time: 100)
+    ResponseTime.create(time: 300)
+
+    assert_equal 200, ResponseTime.avg
+  end
+
+  def test_it_can_return_max_response_time_across_all_requests
+    ResponseTime.create(time: 100)
+    ResponseTime.create(time: 300)
+
+    assert_equal 300, ResponseTime.max
+  end
+
+  def test_it_can_return_min_response_time_across_all_requests
+    ResponseTime.create(time: 100)
+    ResponseTime.create(time: 300)
+
+    assert_equal 100, ResponseTime.min
+  end
 end
