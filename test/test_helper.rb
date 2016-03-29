90aaa4e1d917dcf12c5c5654d28ff6f501502793
Hedy <hedywoo@gmail.com>
2016-03-28 22:20:03 -0600

    set up test helper - screenhero impeded this as a group

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,5 +7,27 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'capybara/dsl'
+require 'database_cleaner'
+require 'erb/tilt'
+require 'pry'
 
 Capybara.app = RushHour::Server
+
+module TestHelpers
+  def setup
+    DatabaseCleaner.start
+    super
+  end
+
+  def teardown
+    DatabaseCleaner.clean
+    super
+  end
+
+  def app
+    RushHour::Server
+  end
+
+end
+
+ DatabaseCleaner.strategy = :truncation, {except: %w[public.schema_migrations]}
