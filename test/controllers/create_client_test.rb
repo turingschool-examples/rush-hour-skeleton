f282bcddba102ec609c5a887231e8a1478966107
Matt Pindell <pindell.m@gmail.com>
2016-03-30 17:11:30 -0600

    controllers tests complete for Client

--- test/controllers/create_client_test.rb
+++ test/controllers/create_client_test.rb
@@ -22,4 +22,47 @@ class CreateClientTest < Minitest::Test
     assert_equal "{\"identifier\":\"name\"}\n", last_response.body
   end
 
+  def test_create_client_with_invalid_params_no_rootUrl
+    assert_equal 0, Client.count
+
+    post '/sources', {
+                       identifier: "name"
+                     }
+    assert_equal 0, Client.count
+    assert_equal 400, last_response.status
+    assert_equal "Rooturl can't be blank\n", last_response.body
+  end
+
+  def test_create_client_with_invalid_params_no_identifier
+    assert_equal 0, Client.count
+
+    post '/sources', {
+                       rootUrl: "www.example.com"
+                     }
+    assert_equal 0, Client.count
+    assert_equal 400, last_response.status
+    assert_equal "Identifier can't be blank\n", last_response.body
+  end
+
+  def test_create_duplicate_client
+    # skip
+    assert_equal 0, Client.count
+
+    post '/sources', {
+                       identifier: "name",
+                       rootUrl: "www.example.com"
+                     }
+
+    assert_equal 1, Client.count
+
+    post '/sources', {
+                       identifier: "name",
+                       rootUrl: "www.example.com"
+                     }
+
+    assert_equal 1, Client.count
+    assert_equal 403, last_response.status
+    assert_equal "Client with identifier: \"name\" already exists!\n", last_response.body
+  end
+
 end
