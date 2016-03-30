c48d23063c81278dd735b9f8411fea22f157d19d
Matt Pindell <pindell.m@gmail.com>
2016-03-30 15:59:29 -0600

    created '/sources' path on server

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -3,5 +3,22 @@ module RushHour
     not_found do
       erb :error
     end
+
+    post '/sources' do
+      client = Client.new(identifier: params[:identifier],
+                          rootUrl: params[:rootUrl])
+
+      if Client.exists?(identifier: client.identifier)
+        status 403
+        body "Client with identifier: \"#{client.identifier}\" already exists!\n"
+      elsif client.save
+        status 200
+        body "{\"identifier\":\"#{client.identifier}\"}\n"
+      else
+        status 400
+        body "#{client.errors.full_messages.join(", ")}\n"
+      end
+    end
+
   end
 end
