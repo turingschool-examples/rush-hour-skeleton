b742d272a3e98b63c64648f966c2eb8a89eb9b67
Jonathan <jonacom@lissismore.com>
2016-03-31 17:59:43 -0600

    minor change

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -124,19 +124,19 @@ module TestHelpers
   end
 
   def get_params
-    params = {
-							"url" => "http://jumpstartlab.com/blog",
-							"requested_at" => "2013-02-16 21:38:28 -0700",
-							"responded_in" => 37,
-							"referrer" => "http://jumpstartlab.com",
-							"request_type" => "GET",
-							"parameters" => [],
-							"event" => "socialLogin",
-							"u_agent" => "Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
-							"resolution_width" => "1920",
-							"resolution_height" => "1280",
-							"ip" => "63.29.38.211"
-						}
+    {
+			"url" => "http://jumpstartlab.com/blog",
+			"requested_at" => "2013-02-16 21:38:28 -0700",
+			"responded_in" => 37,
+			"referrer" => "http://jumpstartlab.com",
+			"request_type" => "GET",
+			"parameters" => [],
+			"event" => "socialLogin",
+			"u_agent" => "Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+			"resolution_width" => "1920",
+			"resolution_height" => "1280",
+			"ip" => "63.29.38.211"
+		}
   end
 end
 
