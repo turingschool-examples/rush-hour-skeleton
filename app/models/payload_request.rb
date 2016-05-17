2dcadbb9e57059c44f3fdf749d9f704b5c31db56
Robbie Jaeger <jaeger.rob@gmail.com>
2016-05-17 15:22:19 -0600

    Remove validations for new payload requests table

--- app/models/payload_request.rb
+++ app/models/payload_request.rb
@@ -4,10 +4,9 @@ class PayloadRequest < ActiveRecord::Base
   validates :responded_in, presence: true
   validates :referred_by, presence: true
   validates :request_type, presence: true
-  validates :parameters, presence: true
+  validates :parameters_id, presence: true
   validates :event_name, presence: true
-  validates :user_agent, presence: true
-  validates :resolution_width, presence: true
-  validates :resolution_height, presence: true
-  validates :ip, presence: true
+  validates :user_agent_id, presence: true
+  validates :resolution_id, presence: true
+  validates :ip_id, presence: true
 end
