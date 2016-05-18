e2ef51b87b758f28cc41aa4aad845448182bf832
Ryan Batty <rgbatty@outlook.com>
2016-05-18 13:58:26 -0600

    Re-added verification to the PayloadRequest model to ensure all id's are present

--- app/models/payload_request.rb
+++ app/models/payload_request.rb
@@ -10,4 +10,11 @@ class PayloadRequest < ActiveRecord::Base
   validates :requested_at, presence: true
   validates :responded_in, presence: true
   validates :parameters, presence: true
+  validates :url_id, presence: true
+  validates :referrer_id, presence: true
+  validates :request_type_id, presence: true
+  validates :event_name_id, presence: true
+  validates :user_agent_id, presence: true
+  validates :resolution_id, presence: true
+  validates :ip_id, presence: true
   end
