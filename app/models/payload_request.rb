934ad94a5853042bab57cd10e6b89569562c2b33
Anna Weisbrodt <aweisbro@gmail.com>
2016-05-18 15:23:36 -0600

    finished request analytics - response times - closes #16

--- app/models/payload_request.rb
+++ app/models/payload_request.rb
@@ -10,4 +10,31 @@ class PayloadRequest < ActiveRecord::Base
   validates :requested_at, presence: true
   validates :responded_in, presence: true
   validates :parameters, presence: true
+
+  def self.average_response_time
+    average("responded_in")
   end
+
+  def self.max_response_time
+    maximum("responded_in")
+  end
+
+  def self.min_response_time
+    minimum("responded_in")
+  end
+
+  def self.counts_request_type_max
+    group("request_type").count.first[1]
+  end
+
+  def self.most_frequent_request_type
+    request_hash = group("request_type").count
+    matching_requests =request_hash.find_all do |request_type, count|
+      count == counts_request_type_max
+    end
+    matching_requests.map do |request_type, count|
+      request_type.verb
+    end.join(", ")
+  end
+
+end
