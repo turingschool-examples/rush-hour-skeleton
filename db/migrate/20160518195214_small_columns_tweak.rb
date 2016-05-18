140459a18b3b664d26ce0435610c05700cb0c90b
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-05-18 14:00:18 -0600

    Buggy migration

---
+++ db/migrate/20160518195214_small_columns_tweak.rb
@@ -0,0 +1,14 @@
+class SmallColumnsTweak < ActiveRecord::Migration
+  def change
+
+    add_column "payload_requests", :requested_at, :datetime
+    add_column "payload_requests", :responded_in, :integer
+    add_column "payload_requests", :parameters, :string, array: true
+    drop_table "times"
+    drop_table "url_times"
+    rename_column "urls", :name, :address
+    rename_column "user_agent_infos", :content, :browser
+    add_column "user_agent_infos", :version
+    add_column "user_agent_infos", :platform
+  end
+end
