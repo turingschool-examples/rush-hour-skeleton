b104ff74c36ff926b62c84027315d8a708e3ed94
Nick Pisciotta <npisciotta@gmail.com>
2016-05-19 15:50:41 -0600

    Trying to fix parser

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,12 @@
 class Client < ActiveRecord::Base
   has_many :payload_requests
-  has_many :user_agent, through: :payload_requests
+  has_many :user_agents, through: :payload_requests
+  has_many :resolutions, through: :payload_requests
+  has_many :requests,    through: :payload_requests
+  has_many :ips,         through: :payload_requests
+  has_many :urls,        through: :payload_requests
+  has_many :referrers,   through: :payload_requests
 
-    validates :url,           presence: true
-    validates :identifier,    presence: true
+  validates :url,           presence: true
+  validates :identifier,    presence: true
 end
