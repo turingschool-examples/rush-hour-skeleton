4bc77b2023ff5b32f0e32145216a5b874e590f4b
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-05-19 00:56:25 -0600

    Iteration 3 completed, minus testing

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160518231225) do
+ActiveRecord::Schema.define(version: 20160519064005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string "identifier"
+    t.string "root_url"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "name"
