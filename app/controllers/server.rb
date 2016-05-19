4a6cec54d5b762f43f09c8b57549dcee05f75fb3
Anna Weisbrodt <aweisbro@gmail.com>
2016-05-19 12:07:51 -0600

    controller path POST in progress

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -3,5 +3,18 @@ module RushHour
     not_found do
       erb :error
     end
+
+    post '/sources' do
+      client = Client.new(params["identifier"], params["rootUrl"])
+      if client.errors.full_messages.include?("Can't be blank")
+        status 400
+      elsif client.errors.full_messages.include?("Identifier: Already Exists")
+        status 403
+      else
+        status 200
+      end
+    end
+
+
   end
 end
