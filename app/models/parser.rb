6339478edb1882bc1fb81efb98931ce7413caa1b
Karina Gonzalez <karinamzalez@gmail.com>
2016-05-22 13:58:28 -0600

    pulled down from master, creating a new branch

--- app/models/parser.rb
+++ app/models/parser.rb
@@ -1,53 +1,46 @@
-class Parser
+module Parser
 
-  def self.json(string)
+  def json(string)
     JSON.parse(string, {:symbolize_names => true})
   end
 
-  def self.parse_client_params(params)
-    {identifier: params[:identifier], root_url: params[:rootURL]}
-  end
-
-  def self.create_resolution(payload)
+  def create_resolution(payload)
     width = payload[:resolutionWidth]
     height = payload[:resolutionHeight]
     Resolution.where(:width=> width).where(:height=> height).first_or_create
   end
 
-  def self.create_user_agent(user_agent)
+  def create_user_agent(user_agent)
     parsed_agent = UserAgent.parse(user_agent)
     browser = parsed_agent.browser
     platform = parsed_agent.platform
     UserAgentB.where(:browser=> browser).where(:platform=> platform).first_or_create
   end
 
-  def self.create_url(url)
+  def create_url(url)
     Url.where(:address=> url).first_or_create
   end
 
-  def self.create_referrer(referrer)
+  def create_referrer(referrer)
     Referrer.where(:address=> referrer).first_or_create
   end
 
-  def self.create_request(request)
+  def create_request(request)
     Request.where(:verb=> request).first_or_create
   end
 
-  def self.create_event(event)
+  def create_event(event)
     Event.where(:name=> event).first_or_create
   end
 
-  def self.create_ip(ip)
+  def create_ip(ip)
     Ip.where(:address=> ip).first_or_create
   end
 
-  def self.find_client_id(identifier)
-    Client.find_by(identifier: identifier).id
-  end
-
-  def self.parse_payload(string, identifier)
+  def parse_payload_request(string)
     payload = json(string)
-    h = PayloadRequest.create(
+    pry
+    PayloadRequest.create(
                            :requested_at=>  payload[:requestedAt],
                            :responded_in=>  payload[:respondedIn],
                            :parameters=>    payload[:parameters],
@@ -57,10 +50,9 @@ class Parser
                            :event=>         create_event(payload[:eventName]),
                            :user_agent_b=>  create_user_agent(payload[:userAgent]),
                            :resolution=>    create_resolution(payload),
-                           :ip=>            create_ip(payload[:ip]),
-                           :client_id =>    find_client_id(identifier)
+                           :ip=>            create_ip(payload[:ip])
+                          #  "id_client":     c
                           )
-                          require 'pry';binding.pry
 
   # payload_request = PayloadRequest.new(requested_at: requested)
   #
