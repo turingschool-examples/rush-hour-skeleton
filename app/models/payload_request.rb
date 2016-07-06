55f96d230ff737f6c4c3348109a818657a9cf4ae
Jason Hanna <jason.hanna01@gmail.com>
2016-07-06 17:07:50 -0600

    removed user agent from payload request validation and added user agent id

--- app/models/payload_request.rb
+++ app/models/payload_request.rb
@@ -1,4 +1,4 @@
 class PayloadRequest < ActiveRecord::Base
-  validates :url, :requested_at, :responded_in, :referred_by, :request_type, :user_agent, :resolution_width, :resolution_height, :ip, presence: true
+  validates :url, :requested_at, :responded_in, :referred_by, :request_type, :resolution_width, :resolution_height, :ip, :user_agent_id, presence: true
 
 end
