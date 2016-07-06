2fe96ef432e352f40de8e3498bbb097f21c9a98c
Jason Hanna <jason.hanna01@gmail.com>
2016-07-06 15:57:00 -0600

    fixed payload request validation to not check for referred_by

--- app/models/payload_request.rb
+++ app/models/payload_request.rb
@@ -1,6 +1,6 @@
 class PayloadRequest < ActiveRecord::Base
   validates :url, :requested_at, :responded_in,
-            :referred_by, :request_type, :user_agent,
+            :request_type, :user_agent,
             :resolution_width, :resolution_height, :ip,
             presence: true
 
