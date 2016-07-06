d6a4bac5621b673b39c361199297f5a24dbf4ea4
David Tinianow <david.tinianow@gmail.com>
2016-07-06 17:17:57 -0600

    [WIP] created request type model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160706222941) do
+ActiveRecord::Schema.define(version: 20160706223705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +38,12 @@ ActiveRecord::Schema.define(version: 20160706222941) do
   create_table "referred_bies", force: :cascade do |t|
     t.text     "root_url"
     t.text     "path"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "request_types", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
