7877246bcabbf76c6ce13377cda5ccbe1f3e945f
Caleb <calebcowen@gmail.com>
2016-07-06 22:48:12 -0600

    url class methods made and tested, closes #14

--- app/models/url.rb
+++ app/models/url.rb
@@ -3,4 +3,19 @@ class Url < ActiveRecord::Base
 
   validates :root_url, presence: true
   validates :path, presence: true
+
+  def self.all_paths
+    Url.pluck(:path).uniq
+  end
+
+  def self.all_roots
+    Url.pluck(:root_url).uniq
+  end
+
+  def self.most_to_least
+    grouped = Url.group([:root_url] + [:path]).count
+
+    grouped.keys.map {|root, path| root + path}
+
+  end
 end
--- test/models/url_test.rb
+++ test/models/url_test.rb
@@ -9,4 +9,35 @@ class UrlTest < Minitest::Test
     assert_equal 'http://www.google.com', url.root_url
     assert url.valid?
   end
+
+  def test_it_returns_all_paths
+    Url.create(root_url: "http://www.google.com", path: "/maps")
+    Url.create(root_url: "http://www.google.com", path: "/translate")
+    Url.create(root_url: "http://www.google.com", path: "/maps")
+
+    assert_equal 3, Url.all.count
+    assert_equal ["/maps", "/translate"], Url.all_paths
+  end
+
+  def test_it_returns_all_root_urls
+    Url.create(root_url: "http://www.facebook.com", path: "/photos")
+    Url.create(root_url: "http://www.google.com", path: "/translate")
+    Url.create(root_url: "http://www.facebook.com", path: "/about")
+
+    assert_equal 3, Url.all.count
+    assert_equal ["http://www.facebook.com", "http://www.google.com"], Url.all_roots
+  end
+
+  def test_it_returns_a_descending_list_of_most_requested
+    Url.create(root_url: "http://www.facebook.com", path: "/photos")
+    Url.create(root_url: "http://www.google.com", path: "/translate")
+    Url.create(root_url: "http://www.facebook.com", path: "/about")
+    Url.create(root_url: "http://www.google.com", path: "/translate")
+    Url.create(root_url: "http://www.facebook.com", path: "/about")
+    Url.create(root_url: "http://www.facebook.com", path: "/about")
+
+    assert_equal 6, Url.all.count
+    assert_equal ["http://www.facebook.com/about", "http://www.google.com/translate", "http://www.facebook.com/photos"], Url.most_to_least
+
+  end
 end
