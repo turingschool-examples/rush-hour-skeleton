7e4bd389c691886e239c65896afb83d807593518
Jason Hanna <jason.hanna01@gmail.com>
2016-07-06 19:51:40 -0600

    Add relationships to payload request and resolution models

--- app/models/payload_request.rb
+++ app/models/payload_request.rb
@@ -1,4 +1,9 @@
 class PayloadRequest < ActiveRecord::Base
   validates :url, :requested_at, :responded_in, :referred_by, :request_type, :user_agent, :resolution_id, :ip, presence: true
 
+  belongs_to :resolutions
+  belongs_to :urls
+  belongs_to :request_types
+  belongs_to :user_agents
+  belongs_to :ips
 end
