cfc8f5ff63d0aabfaa5a7e4cab085d816374f68e
tgisg <soniagupta504@gmail.com>
2016-07-06 19:42:20 -0600

    finished testing payload_request and successfully figured out part of Ryan's method to test raw data to completed payload_request object woot woot

--- app/models/payload_request.rb
+++ app/models/payload_request.rb
@@ -7,14 +7,13 @@ class PayloadRequest < ActiveRecord::Base
   # belongs_to :user_agent
   # belongs_to :referred_by
 
-  validates :url, presence: true
+  validates :url_id, presence: true
   validates :requested_at, presence: true
   validates :responded_in, presence: true
-  validates :referred_by, presence: true
-  validates :request_type, presence: true
-  validates :user_agent, presence: true
-  validates :resolution_width, presence: true
-  validates :resolution_height, presence: true
-  validates :ip, presence: true
+  validates :referred_by_id, presence: true
+  validates :request_type_id, presence: true
+  validates :u_agent_id, presence: true
+  validates :resolution_id, presence: true
+  validates :ip_id, presence: true
 
 end
--- test/models/payload_request_test.rb
+++ test/models/payload_request_test.rb
@@ -1,7 +1,7 @@
 require_relative '../test_helper.rb'
 
 class PayloadRequestTest < Minitest::Test
-  include TestHelpers
+  include TestHelpers, DataProcessor
 
   def test_it_parses_the_payload
     payload_request = JSON.parse('{
@@ -21,23 +21,18 @@ class PayloadRequestTest < Minitest::Test
   end
 
   def test_database_starts_clean
+    date_time = DateTime.new
+
     assert PayloadRequest.all.empty?
-    payload_request = PayloadRequest.create(url: "http://jumpstartlab.com/blog",
-                          requested_at: Date.today,
-                          responded_in: 2,
-                          referred_by: " 2",
-                          request_type: " 2",
-                          user_agent: "2 ",
-                          resolution_width: "1290",
-                          resolution_height: "800",
-                          ip: "23.1.1.1")
-    assert_equal 2, payload_request.responded_in
+    payload_request = PayloadRequest.create(requested_at: date_time, responded_in: 30, url_id: 1, referred_by_id: 2, request_type_id: 3, u_agent_id: 4, resolution_id: 5, ip_id: 6)
+
+    assert_equal 30, payload_request.responded_in
     assert payload_request.valid?
     refute PayloadRequest.all.empty?
   end
 
-  def test_it_can_appropriately_label_data_for_columns
-    raw_data = '{
+  def test_it_can_appropriately_process_data_for_columns
+    request = '{
               "url": "http://jumpstartlab.com/blog",
               "requestedAt": "2013-02-16 21:38:28 -0700",
               "respondedIn": 37,
@@ -48,16 +43,22 @@ class PayloadRequestTest < Minitest::Test
               "resolutionHeight": "1280",
               "ip": "63.29.38.211"
             }'
-    data = parse_it(raw_data)
-    formatted = assign_data(data)
-    payload_request = PayloadRequest.create(formatted)
+    payload_request = process_payload(request)
     assert_equal 37, payload_request.responded_in
   end
 
   def test_it_can_hold_attributes
-    payload_request = PayloadRequest.create(requested_at: 2013-02-16 21:38:28 -0700, responded_in: 30, url_id: 1, referred_by_id: 2, request_type_id: 3, u_agent_id: 4, resolution_id: 5, ip_id: 6)
-    assert_equal '/', referred_by.path
-    assert_equal 'http://www.google.com', referred_by.root_url
-    assert referred_by.valid?
+    date_time = DateTime.new
+    payload_request = PayloadRequest.create(requested_at: date_time, responded_in: 30, url_id: 1, referred_by_id: 2, request_type_id: 3, u_agent_id: 4, resolution_id: 5, ip_id: 6)
+
+    assert_equal date_time, payload_request.requested_at
+    assert_equal 30, payload_request.responded_in
+    assert_equal 1, payload_request.url_id
+    assert_equal 2, payload_request.referred_by_id
+    assert_equal 3, payload_request.request_type_id
+    assert_equal 4, payload_request.u_agent_id
+    assert_equal 5, payload_request.resolution_id
+    assert_equal 6, payload_request.ip_id
   end
+
 end
