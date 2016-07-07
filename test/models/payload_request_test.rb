f84927275336845e40f328ac48772a8f389fbadb
tgisg <soniagupta504@gmail.com>
2016-07-06 18:15:22 -0600

    working on tests for payloadrequest

--- test/models/payload_request_test.rb
+++ test/models/payload_request_test.rb
@@ -54,4 +54,10 @@ class PayloadRequestTest < Minitest::Test
     assert_equal 37, payload_request.responded_in
   end
 
+  def test_it_can_hold_attributes
+    payload_request = PayloadRequest.create(requested_at: 2013-02-16 21:38:28 -0700, responded_in: 30, url_id: 1, referred_by_id: 2, request_type_id: 3, u_agent_id: 4, resolution_id: 5, ip_id: 6)
+    assert_equal '/', referred_by.path
+    assert_equal 'http://www.google.com', referred_by.root_url
+    assert referred_by.valid?
+  end
 end
