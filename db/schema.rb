fa77da5c5c236b3989d9e62916b2f978e500c9a4
Jason Hanna <jason.hanna01@gmail.com>
2016-07-06 20:27:11 -0600

    Rename referred_by_id to referral_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160707021506) do
+ActiveRecord::Schema.define(version: 20160707022437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,9 +64,9 @@ ActiveRecord::Schema.define(version: 20160707021506) do
 
   create_table "urls", force: :cascade do |t|
     t.text     "address"
-    t.integer  "referred_by_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.integer  "referral_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
