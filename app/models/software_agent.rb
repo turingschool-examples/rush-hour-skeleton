f8b1dce4fc0675b27bf31af67adca2b04bcc14c4
Jason Hanna <jason.hanna01@gmail.com>
2016-07-06 20:34:39 -0600

    Add relationships to ip, payload_request, request_type, software_agent, and url models

--- app/models/software_agent.rb
+++ app/models/software_agent.rb
@@ -1,3 +1,4 @@
 class SoftwareAgent < ActiveRecord::Base
   validates :brower, :version, :platform, presence: true
+  has_many :payload_requests
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -2,4 +2,5 @@ class Url < ActiveRecord::Base
   validates :address, :referral_id,
             presence: true
   belongs_to :referrals
+  has_many :payload_requests
 end
