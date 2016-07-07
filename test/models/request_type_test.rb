f27ae28d389d3ccca0dedcc87026a639b659ff2c
David Tinianow <david.tinianow@gmail.com>
2016-07-06 19:53:14 -0600

    [WIP] request type analysis

--- test/models/request_type_test.rb
+++ test/models/request_type_test.rb
@@ -1,4 +1,5 @@
 require_relative '../test_helper.rb'
+require 'pry'
 
 class RequestTypeTest < Minitest::Test
   include TestHelpers
@@ -9,6 +10,21 @@ class RequestTypeTest < Minitest::Test
     assert request_type.valid?
     request_type = RequestType.create({})
     refute request_type.valid?
+  end
+
+  def test_returns_all_request_types
+    RequestType.create(name: "GET")
+    RequestType.create(name: "POST")
+    RequestType.create(name: "GET")
+    assert_equal 3, RequestType.all.count
+    assert_equal ["GET", "POST"], RequestType.all_request_types
+  end
+
+  def test_returns_most_frequent_request_type
+    RequestType.create(name: "GET")
+    RequestType.create(name: "POST")
+    RequestType.create(name: "POST")
+    assert_equal "POST", RequestType.most_frequent_request_type
   end
 
 end
