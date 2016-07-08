6a3fc1d7b9cb9fe2225861085d6b5e5b60efd75b
Kris Sparks <kbs5280@gmail.com>
2016-07-08 11:30:29 -0600

    Additional tests

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,4 +2,10 @@ class Url < ActiveRecord::Base
   validates :address,   presence:true
 
   has_many :payload_requests
+
+  def self.urls_from_most_to_least_requested
+    urls = Url.pluck(:address)
+    frequency = urls.inject(Hash.new(0)) { |hash , value | hash[value] += 1; hash }
+    urls.max_by { |value| frequency[value] }
+  end
 end
