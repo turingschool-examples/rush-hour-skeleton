6737f6b1f9488ca6bd6d357a6e0e21c7eabc3d53
Nicole Marino <nicolekmarino@hotmail.com>
2016-07-07 18:05:02 -0600

    http request methods and tests

--- test/models/request_type_test.rb
+++ test/models/request_type_test.rb
@@ -1,9 +1,27 @@
 require_relative '../test_helper'
 
 class RequestTypeTest < Minitest::Test
+  include TestHelpers
 
   def test_it_can_create_request_type
     type = RequestType.create(verb: "GET")
     assert_equal "GET", type.verb
   end
+
+  def test_most_common_type_of_request
+    RequestType.create(verb: "GET")
+    RequestType.create(verb: "GET")
+    RequestType.create(verb: "GET")
+    RequestType.create(verb: "POST")
+    assert_equal "GET", RequestType.most_frequent_type
+  end
+
+  def test_list_of_http_verbs_used
+    RequestType.create(verb: "GET")
+    RequestType.create(verb: "GET")
+    RequestType.create(verb: "GET")
+    RequestType.create(verb: "POST")
+    assert_equal ["GET", "GET", "GET", "POST"], RequestType.pluck(:verb)
+  end
+
 end
