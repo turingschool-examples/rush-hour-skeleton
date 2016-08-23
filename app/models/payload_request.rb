a48c072195fa730ffcf9c1f6417e586194b12a5d
tgisg <soniagupta504@gmail.com>
2016-08-23 15:55:58 -0600

    closes #3 #4

---
+++ app/models/payload_request.rb
@@ -0,0 +1,11 @@
+class PayloadRequest < ActiveRecord::Base
+  validates :url, presence: true
+  validates :requested_at, presence: true
+  validates :responded_in, presence: true
+  validates :referred_by, presence: true
+  validates :request_type, presence: true
+  validates :u_agent, presence: true
+  validates :resolution_width, presence: true
+  validates :resolution_height, presence: true
+  validates :ip, presence: true
+end
