3198d90b4db95e50483f85590dd9e4600aa0caf6
David Davydov <kctrlv@gmail.com>
2016-08-23 12:25:14 -0600

    added database cleaner and strategy for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,5 +7,8 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'capybara/dsl'
+require 'database_cleaner'
 
 Capybara.app = RushHour::Server
+
+DatabaseCleaner.strategy = :truncation
