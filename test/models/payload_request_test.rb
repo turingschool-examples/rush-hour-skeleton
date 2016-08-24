982377f65023da85854c9efa1bbd134f767c634f
David Davydov <kctrlv@gmail.com>
2016-08-24 15:44:08 -0600

    payloadrequest formatted

--- test/models/payload_request_test.rb
+++ test/models/payload_request_test.rb
@@ -13,6 +13,18 @@ class PayloadRequestTest < Minitest::Test
   # validates :requested_at,      presence: true
   # '''
   def setup
+    # payload = {
+    #   "url":"http://jumpstartlab.com/blog",
+    #   "requestedAt":"2013-02-16 21:38:28 -0700",
+    #   "respondedIn":37,
+    #   "referredBy":"http://jumpstartlab.com",
+    #   "requestType":"GET",
+    #   "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+    #   "resolutionWidth":"1920",
+    #   "resolutionHeight":"1280",
+    #   "ip":"63.29.38.211"
+    # }
+    #
     @snake_cased_data = {
       request_type_id: 1,
       target_url_id: 1,
