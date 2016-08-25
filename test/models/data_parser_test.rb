30082d5a3ebfed4d1a4af94a10fcecfd7cde6668
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-08-25 11:57:02 -0600

    Add DataParser module and tests

---
+++ test/models/data_parser_test.rb
@@ -0,0 +1,49 @@
+require_relative '../test_helper'
+
+class DataParserTest < Minitest::Test
+  include DataParser
+  def raw_payload
+    '{
+      "url":"http://jumpstartlab.com/blog",
+      "requestedAt":"2013-02-16 21:38:28 -0700",
+      "respondedIn":37,
+      "referredBy":"http://jumpstartlab.com",
+      "requestType":"GET",
+      "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+      "resolutionWidth":"1920",
+      "resolutionHeight":"1280",
+      "ip":"63.29.38.211"
+    }'
+  end
+
+  def test_it_parses_raw_json
+    parsed = parsed(raw_payload)
+
+    expected = {"url"=>"http://jumpstartlab.com/blog",
+                   "requestedAt"=>"2013-02-16 21:38:28 -0700",
+                   "respondedIn"=>37,
+                   "referredBy"=>"http://jumpstartlab.com",
+                    "requestType"=>"GET",
+                    "userAgent"=>"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+                    "resolutionWidth"=>"1920",
+                    "resolutionHeight"=>"1280",
+                    "ip"=>"63.29.38.211"}
+
+    assert_equal expected, parsed
+  end
+
+  def test_it_reassigns_column_keys_to_snake_case
+
+    expected = {"url"=>"http://jumpstartlab.com/blog",
+                   "requested_at"=>"2013-02-16 21:38:28 -0700",
+                   "responded_in"=>37,
+                   "referred_by"=>"http://jumpstartlab.com",
+                    "request_type"=>"GET",
+                    "u_agent"=>"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+                    "resolution_width"=>"1920",
+                    "resolution_height"=>"1280",
+                    "ip"=>"63.29.38.211"}
+
+    assert_equal expected, formatted_payload(raw_payload)
+  end
+end
