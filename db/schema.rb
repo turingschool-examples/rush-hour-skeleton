a2b743598f06f2b535413ca798f453ba6099c17d
Ryan Workman <rdavid1099@gmail.com>
2016-08-25 11:54:01 -0600

    Created clients table migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825000618) do
+ActiveRecord::Schema.define(version: 20160825173421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string   "identifier"
+    t.string   "root_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ips", force: :cascade do |t|
     t.string   "address"
--- test/models/tables_integration_test.rb
+++ test/models/tables_integration_test.rb
@@ -1,5 +1,4 @@
 require_relative '../test_helper'
-require 'pry'
 
 class TableIntegrationTest < Minitest::Test
   include TestHelpers
