afc623dc852773cd619297dabe58648fb671dcdd
Bryan Goss <bcgoss@gmail.com>
2016-08-25 14:07:03 -0600

    Merges with changes to Scheme

--- app/models/url.rb
+++ app/models/url.rb
@@ -3,4 +3,34 @@ class Url < ActiveRecord::Base
 
   validates  :web_address, presence: true
   validates :web_address, uniqueness: true
+
+  def response_times
+    payload_requests.map do |request|
+      request.pluck("responded_in")
+    end
+  end
+
+  def min_response_time
+    payload_requests.min_response_time
+  end
+
+  def max_response_time
+    payload_requests.max_response_time
+  end
+
+  def average_response_time
+    payload_requests.average_response_time
+  end
+
+  def http_verbs
+    payload_requests.http_verbs
+  end
+
+  def referalls
+    payload_requests.referrals
+  end
+
+  def user_agents
+
+  end
 end
