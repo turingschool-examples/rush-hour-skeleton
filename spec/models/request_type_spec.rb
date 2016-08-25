b38d37effcea4d85638f2ac3dfb0a0f90dbe1e80
PlanetEfficacy <jspevack@gmail.com>
2016-08-25 13:44:26 -0600

    updated request type spec

--- spec/models/request_type_spec.rb
+++ spec/models/request_type_spec.rb
@@ -31,6 +31,29 @@ RSpec.describe RequestType, type: :model do
     end
   end
 
+  def make_request_types_and_payload_requests_and_urls
+    ["GET", "PUT"].each do |verb|
+      RequestType.create("verb" => verb)
+    end
+
+    ["www.jumpstartlab.com", "www.turing.io", "www.google.com"].each do |address|
+      Url.create("address" => address)
+    end
+
+    [[1,1],[1,1],[1,2],[1,3],[2,1],[1,2]].each do |request_type_url_id|
+      PayloadRequest.create(
+        "url_id"                =>  request_type_url_id[1],
+        "requested_at"          =>  "2013-02-16 21:38:28 -0700",
+        "responded_in"          =>  35,
+        "source_id"             =>  2,
+        "request_type_id"       =>  request_type_url_id[0],
+        "u_agent_id"            =>  5,
+        "screen_resolution_id"  =>  4,
+        "ip_id"                 =>  6)
+    end
+
+  end
+
   it "takes a request_type and returns a request_type object" do
     expect(single_request_type).to be_an_instance_of RequestType
   end
@@ -61,6 +84,16 @@ RSpec.describe RequestType, type: :model do
   it "groups by request type" do
     make_request_types_and_payload_requests
     expect(RequestType.group_by_request_type).to eq({1=>2, 2=>1})
+  end
+
+  it "knows all the request types associated with a given url" do
+    result_1 = { "GET" => 2, "PUT" => 1}
+    result_2 = { "GET" => 1, "PUT" => 1}
+    result_3 = { "GET" => 1, "PUT" => 0}
+
+    expect(verbs_associated_with_url("www.jumpstartlab.com")).to eq(result_1)
+    expect(verbs_associated_with_url("www.turing.io")).to eq(result_2)
+    expect(verbs_associated_with_url("www.google.com")).to eq(result_3)
   end
 
 end
