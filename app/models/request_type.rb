717b437524ddc6d2849c985cbd8a3288866862e2
PlanetEfficacy <jspevack@gmail.com>
2016-08-25 13:02:36 -0600

    making progress on iteration 2

--- app/models/request_type.rb
+++ app/models/request_type.rb
@@ -3,4 +3,17 @@ class RequestType < ActiveRecord::Base
 
   validates :verb, presence: true
   validates :verb, uniqueness: true
+
+  def self.most_frequent_request_type
+    grouping = group_by_request_type
+    RequestType.find_by(:id => grouping[grouping.keys.max]).verb
+  end
+
+  def self.list_verbs
+    RequestType.pluck(:verb)
+  end
+
+  def self.group_by_request_type
+    PayloadRequest.group('request_type_id').count
+  end
 end
--- app/models/u_agent.rb
+++ app/models/u_agent.rb
@@ -1,7 +1,33 @@
 class UAgent < ActiveRecord::Base
   has_many :payload_requests
   validates :browser, presence: true
-  validates :browser, uniqueness: true
+  validates :browser, uniqueness: {scope: :operating_system}
   validates :operating_system, presence: true
-  validates :operating_system, uniqueness: true
+  validates :operating_system, uniqueness: {scope: :browser}
+
+  def self.browser_breakdown
+    breakdown("browser")
+  end
+
+  def self.os_breakdown
+    breakdown("operating_system")
+  end
+
+  def self.breakdown(type)
+    group_by_u_agent.to_a.reduce({}) do |result, grouping|
+      browser = UAgent.find_by(:id => grouping[0]).send(type)
+      result = incrament_browser_count(result, browser, grouping[1])
+    end
+  end
+
+  def self.group_by_u_agent
+    PayloadRequest.group('u_agent_id').count
+  end
+
+  private
+  def self.incrament_browser_count(result, browser, count)
+    result.has_key?(browser) ? result[browser] += count : result[browser] = count
+    result
+  end
+
 end
