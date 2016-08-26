47f64984f7da79e0b77d50dbd74dbc03b632369d
tgisg <soniagupta504@gmail.com>
2016-08-25 18:11:27 -0600

    request types assoc with url test and method

--- test/models/url_test.rb
+++ test/models/url_test.rb
@@ -9,4 +9,216 @@ class UrlTest < Minitest::Test
     assert Url.all.first.valid?
     assert_equal 1, Url.all.count
   end
+
+  def test_it_returns_max_response_time_for_specific_url
+    url = Url.create(url: "www.jumpstartlabs.com")
+
+    payload1 = PayloadRequest.create(url_id: 1,
+              requested_at: "date",
+              responded_in: 34,
+              referred_by_id: 1,
+              request_type_id: 2,
+              u_agent_id: 3,
+              resolution_id: 6,
+              ip_id: 2
+            )
+    payload2 = PayloadRequest.create(url_id: 1,
+                requested_at: "date",
+                responded_in: 50,
+                referred_by_id: 1,
+                request_type_id: 2,
+                u_agent_id: 3,
+                resolution_id: 6,
+                ip_id: 2
+                  )
+    payload3 = PayloadRequest.create(url_id: 1,
+                requested_at: "date",
+                responded_in: 12,
+                referred_by_id: 1,
+                request_type_id: 2,
+                u_agent_id: 3,
+                resolution_id: 6,
+                ip_id: 2
+                            )
+    #payload4 has a different url_id
+    payload4 = PayloadRequest.create(url_id: 2,
+                 requested_at: "date",
+                 responded_in: 12,
+                 referred_by_id: 1,
+                 request_type_id: 2,
+                 u_agent_id: 3,
+                 resolution_id: 6,
+                 ip_id: 2
+                                    )
+
+    # url_1 = PayloadRequest.where(url_id: 1)
+
+    assert_equal 50, url.maximum_response_time
+    assert_equal 3, url.payload_requests.count
+  end
+
+  def test_it_returns_minimum_response_time_for_specific_url
+    url = Url.create(url: "www.jumpstartlabs.com")
+
+    payload1 = PayloadRequest.create(url_id: 1,
+              requested_at: "date",
+              responded_in: 34,
+              referred_by_id: 1,
+              request_type_id: 2,
+              u_agent_id: 3,
+              resolution_id: 6,
+              ip_id: 2
+            )
+    payload2 = PayloadRequest.create(url_id: 1,
+                requested_at: "date",
+                responded_in: 50,
+                referred_by_id: 1,
+                request_type_id: 2,
+                u_agent_id: 3,
+                resolution_id: 6,
+                ip_id: 2
+                  )
+    payload3 = PayloadRequest.create(url_id: 1,
+                requested_at: "date",
+                responded_in: 12,
+                referred_by_id: 1,
+                request_type_id: 2,
+                u_agent_id: 3,
+                resolution_id: 6,
+                ip_id: 2
+                            )
+    #payload4 has a different url_id
+    payload4 = PayloadRequest.create(url_id: 2,
+                 requested_at: "date",
+                 responded_in: 12,
+                 referred_by_id: 1,
+                 request_type_id: 2,
+                 u_agent_id: 3,
+                 resolution_id: 6,
+                 ip_id: 2
+                                    )
+
+    # url_1 = PayloadRequest.where(url_id: 1)
+
+    assert_equal 12, url.minimum_response_time
+    assert_equal 3, url.payload_requests.count
+  end
+
+  def test_it_orders_all_response_times_per_url
+    url = Url.create(url: "www.jumpstartlabs.com")
+
+    payload1 = PayloadRequest.create(url_id: 1,
+              requested_at: "date",
+              responded_in: 34,
+              referred_by_id: 1,
+              request_type_id: 2,
+              u_agent_id: 3,
+              resolution_id: 6,
+              ip_id: 2
+            )
+    payload2 = PayloadRequest.create(url_id: 1,
+                requested_at: "date",
+                responded_in: 50,
+                referred_by_id: 1,
+                request_type_id: 2,
+                u_agent_id: 3,
+                resolution_id: 6,
+                ip_id: 2
+                  )
+    payload3 = PayloadRequest.create(url_id: 1,
+                requested_at: "date",
+                responded_in: 12,
+                referred_by_id: 1,
+                request_type_id: 2,
+                u_agent_id: 3,
+                resolution_id: 6,
+                ip_id: 2
+                            )
+
+    assert_equal [50, 34, 12], url.response_times_by_order
+    refute_equal [12, 34, 50], url.response_times_by_order
+  end
+
+  def test_average_response_time
+    url = Url.create(url: "www.jumpstartlabs.com")
+
+    payload1 = PayloadRequest.create(url_id: 1,
+              requested_at: "date",
+              responded_in: 34,
+              referred_by_id: 1,
+              request_type_id: 2,
+              u_agent_id: 3,
+              resolution_id: 6,
+              ip_id: 2
+            )
+    payload2 = PayloadRequest.create(url_id: 1,
+                requested_at: "date",
+                responded_in: 50,
+                referred_by_id: 1,
+                request_type_id: 2,
+                u_agent_id: 3,
+                resolution_id: 6,
+                ip_id: 2
+                  )
+    payload3 = PayloadRequest.create(url_id: 1,
+                requested_at: "date",
+                responded_in: 12,
+                referred_by_id: 1,
+                request_type_id: 2,
+                u_agent_id: 3,
+                resolution_id: 6,
+                ip_id: 2
+                            )
+
+    assert_equal 32, url.average_response_time
+    refute_equal 5000, url.average_response_time
+  end
+
+  def test_returns_all_request_types
+    url = Url.create(url: "www.jumpstartlabs.com")
+
+    payload1 = PayloadRequest.create(url_id: 1,
+              requested_at: "date",
+              responded_in: 34,
+              referred_by_id: 1,
+              request_type_id: 2,
+              u_agent_id: 3,
+              resolution_id: 6,
+              ip_id: 2
+            )
+    payload2 = PayloadRequest.create(url_id: 1,
+                requested_at: "date",
+                responded_in: 50,
+                referred_by_id: 1,
+                request_type_id: 1,
+                u_agent_id: 3,
+                resolution_id: 6,
+                ip_id: 2
+                  )
+    payload3 = PayloadRequest.create(url_id: 1,
+                requested_at: "date",
+                responded_in: 12,
+                referred_by_id: 1,
+                request_type_id: 1,
+                u_agent_id: 3,
+                resolution_id: 6,
+                ip_id: 2
+                            )
+    payload4 = PayloadRequest.create(url_id: 2,
+                requested_at: "date",
+                responded_in: 12,
+                referred_by_id: 1,
+                request_type_id: 1,
+                u_agent_id: 3,
+                resolution_id: 6,
+                ip_id: 2
+                )
+
+    request_type1 = RequestType.create(verb: "GET")
+
+    request_type2 = RequestType.create(verb: "POST")
+
+    assert_equal 3, url.request_types.count
+    assert_instance_of RequestType, url.request_types.first
+  end
 end
