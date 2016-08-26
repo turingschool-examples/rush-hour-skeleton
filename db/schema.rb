5078e4974482776873f2856ca29d5f562141fbdb
SusiIrwin <susi.irwin@gmail.com>
2016-08-26 07:58:29 -0600

    added in the identifier column to client table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826025419) do
+ActiveRecord::Schema.define(version: 20160826135500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160826025419) do
     t.text     "rootUrl"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "identifier"
   end
 
   create_table "ips", force: :cascade do |t|
