bf5d537f1b51a74daa2fc8c283dfcbc3c8b6dc6d
PlanetEfficacy <jspevack@gmail.com>
2016-08-26 16:43:11 -0600

    starting data parser class

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -2,7 +2,8 @@ module RushHour
   class Server < Sinatra::Base
     attr_reader :params,
                 :parameters,
-                :messages
+                :messages,
+                :exists
 
     not_found do
       erb :error
@@ -12,7 +13,8 @@ module RushHour
       @params = params
       @parameters = client_params
       @messages = client_messages
-      response = params_valid? ? check_if_client_exists : bad_request
+      @exists = client_exists?
+      response = params_valid? ? check_if_exists : bad_request
       status response[:status_msg]
       body response[:message]
     end
@@ -21,7 +23,8 @@ module RushHour
       @params = params
       @parameters = payload_params
       @messages = payload_messages
-      response = params_valid? ? check_if_client_exists : bad_request
+      @exists = payload_request_exists?
+      response = params_valid? ? check_if_exists : bad_request
       # puts "\n\n\n******************************\n\n\n"
       # count = PayloadRequest.all.count
       # puts "\n\n\n******************************\n\n\n"
@@ -32,8 +35,8 @@ module RushHour
 
     private
 
-    def check_if_client_exists
-      client_exists? ? forbidden : ok
+    def check_if_exists
+      exists ? forbidden : ok
     end
 
     def ok
@@ -47,7 +50,7 @@ module RushHour
     end
 
     def forbidden
-      { :status_msg => 403, :message => "Client already exists" }
+      { :status_msg => 403, :message => messages[403] }
     end
 
     def params_valid?
@@ -59,12 +62,13 @@ module RushHour
     end
 
     def client_messages
-
-      {400 => "Parameters must include #{client_params.join(' and ')}."}
+      {400 => "Parameters must include #{client_params.join(' and ')}.",
+       403 => "Client already exists"}
     end
 
     def payload_messages
-      {400 => "Parameters must include #{payload_params[0..7].join(', ')} and #{payload_params[8]}." }
+      {400 => "Parameters must include #{payload_params[0..7].join(', ')} and #{payload_params[8]}.",
+       403 => "Payload request already exists"}
     end
 
     def payload_params
@@ -74,6 +78,11 @@ module RushHour
 
     def client_exists?
       !(Client.find_by identifier: params[:identifier]).nil?
+    end
+
+    def payload_request_exists?
+      # !(PayloadRequest.find_by identifier: params[:identifier]).nil?
+      # !(PayloadRequest.where(...))
     end
   end
 end
--- spec/controllers/server_spec.rb
+++ spec/controllers/server_spec.rb
@@ -45,7 +45,13 @@ RSpec.describe RushHour::Server, type: :model do
     expect(last_response.body).to eq(expected_body)
   end
 
-  def invalid_payload_request(key, value)
+  def invalid_payload_request(key)
+    payload_request = valid_payload_request
+    payload_request[key] = ""
+    return payload_request
+  end
+
+  def valid_payload_request
     payload_request = {"url_id"=>1,
     "requested_at"=>"2013-02-16 21:38:28 -0700",
     "responded_in"=>2,
@@ -55,23 +61,70 @@ RSpec.describe RushHour::Server, type: :model do
     "screen_resolution_id"=>6,
     "ip_id"=>7,
     "client_id"=>8}
-    payload_request[key] = value
-    return payload_request
   end
 
-  it "knows if payload request is valid" do
+  def payload_error_message
+    "Parameters must include url, requestedAt, respondedIn, referredBy, requestType, userAgent, resolutionWidth, resolutionHeight and ip."
+  end
+
+  it "knows if payload request contains all payload request paramters" do
     Client.create("identifier" => "jumpstartlab",
       "root_url" => "www.jumpstartlab.com")
 
-    post '/sources/jumpstartlab/data', invalid_payload_request("url_id", "")
-    error_message = "Parameters must include url, requestedAt, respondedIn, referredBy, requestType, userAgent, resolutionWidth, resolutionHeight and ip."
+    post '/sources/jumpstartlab/data', invalid_payload_request("url_id")
+
     expect(last_response.status).to eq(400)
-    expect(last_response.body).to eq(error_message)
+    expect(last_response.body).to eq(payload_error_message)
+
+    post '/sources/jumpstartlab/data', invalid_payload_request("requested_at")
+
+    expect(last_response.status).to eq(400)
+    expect(last_response.body).to eq(payload_error_message)
+
+    post '/sources/jumpstartlab/data', invalid_payload_request("responded_in")
+
+    expect(last_response.status).to eq(400)
+    expect(last_response.body).to eq(payload_error_message)
+
+    post '/sources/jumpstartlab/data', invalid_payload_request("source_id")
+
+    expect(last_response.status).to eq(400)
+    expect(last_response.body).to eq(payload_error_message)
+
+    post '/sources/jumpstartlab/data', invalid_payload_request("request_type_id")
+
+    expect(last_response.status).to eq(400)
+    expect(last_response.body).to eq(payload_error_message)
+
+    post '/sources/jumpstartlab/data', invalid_payload_request("u_agent_id")
+
+    expect(last_response.status).to eq(400)
+    expect(last_response.body).to eq(payload_error_message)
+
+    post '/sources/jumpstartlab/data', invalid_payload_request("screen_resolution_id")
+
+    expect(last_response.status).to eq(400)
+    expect(last_response.body).to eq(payload_error_message)
+
+    post '/sources/jumpstartlab/data', invalid_payload_request("ip_id")
+
+    expect(last_response.status).to eq(400)
+    expect(last_response.body).to eq(payload_error_message)
+
+    post '/sources/jumpstartlab/data', invalid_payload_request("client_id")
+
+    expect(last_response.status).to eq(400)
+    expect(last_response.body).to eq(payload_error_message)
   end
 
+  skip "returns status 403 if payload request already exists" do
+    PayloadRequest.create(valid_payload_request)
 
 
+    # post '/sources/jumpstartlab/data', {"url" => }
 
-
+    expect(last_response.status).to eq(403)
+    expect(last_response.body).to eq("Client already exists")
+  end
 
 end
