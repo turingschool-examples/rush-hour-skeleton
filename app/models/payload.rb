e717650aed24e6d2bae353255515507a848e8fa0
Alisher Sadikov <alishersadikov@gmailcom>
2016-10-06 17:22:50 -0600

    iteration zero complete

---
+++ app/models/payload.rb
@@ -0,0 +1,12 @@
+class Payload < ActiveRecord::Base
+  validates  :url, presence: true
+  validates  :requested_at, presence: true
+  validates  :responded_in, presence: true
+  validates  :referred_by, presence: true
+  validates  :request_type, presence: true
+  validates  :event_name, presence: true
+  validates  :user_agent, presence: true
+  validates  :resolution_width, presence: true
+  validates  :resolution_height, presence: true
+  validates  :ip, presence: true
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,11 @@ ENV["RACK_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec'
 require 'capybara/dsl'
+require 'database_cleaner'
 
 Capybara.app = RushHour::Server
+
+DatabaseCleaner.strategy = :truncation
 
 RSpec.configure do |c|
   c.include Capybara::DSL
