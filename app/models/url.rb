4d24e7a53b1ed4c9c12704e01644a6dd0482c505
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-10-08 17:13:55 -0600

    add method and test to url for return list sorted from most to least requested

--- app/models/url.rb
+++ app/models/url.rb
@@ -3,4 +3,10 @@ class Url < ActiveRecord::Base
   validates :url_address, uniqueness: true
 
   has_many :payloads
+
+
+  def self.most_to_least_requested
+    Url.joins(:payloads).group(:url_address).order("COUNT(payloads.*) DESC").pluck(:url_address)
+  end
+
 end
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -21,4 +21,43 @@ RSpec.describe "Url" do
       expect(url2).to_not be_valid
     end
   end
+
+  it "lists urls from most requested to least" do
+    url1 = Url.create( url_address: "www.google.com")
+    url2 = Url.create( url_address: "www.yahoo.com")
+
+    payload1 = Payload.create( url_id:            url1.id,
+                              responded_in:       37,
+                              requested_at:       "2013-02-16 21:38:28 -0700",
+                              referral_id:        2,
+                              request_id:         3,
+                              event_id:           4,
+                              user_agent_stat_id: 5,
+                              resolution_id:      6,
+                              visitor_id:         7 )
+
+    payload2 = Payload.create( url_id:            url1.id,
+                              responded_in:       38,
+                              requested_at:       "2014-02-16 21:38:28 -0700",
+                              referral_id:        2,
+                              request_id:         3,
+                              event_id:           4,
+                              user_agent_stat_id: 5,
+                              resolution_id:      6,
+                              visitor_id:         7 )
+
+    payload3 = Payload.create( url_id:            url2.id,
+                              responded_in:       39,
+                              requested_at:       "2015-02-16 21:38:28 -0700",
+                              referral_id:        2,
+                              request_id:         3,
+                              event_id:           4,
+                              user_agent_stat_id: 5,
+                              resolution_id:      6,
+                              visitor_id:         7 )
+                              
+    most_to_least = [url1.url_address, url2.url_address]
+
+    expect(Url.most_to_least_requested).to eq(most_to_least)
+  end
 end
