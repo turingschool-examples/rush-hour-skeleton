95abe00f5a27bb4a5ab2d76b6768917d68a5698d
Anna Dolan <anna.dolan@gmail.com>
2016-10-08 20:15:30 -0600

    added method to list http verbs

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,4 +2,9 @@ class Request < ActiveRecord::Base
   validates :request_type, presence: true
 
   has_many :payloads
+
+  def self.list_all_http_verbs_used
+    all_verbs = select(:request_type).distinct.group(:id)
+    all_verbs.map { |verb| verb.request_type }
+  end
 end
