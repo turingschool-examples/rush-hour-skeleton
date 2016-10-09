5b783b80d2eba622275b2b2ab050acf6bb3a7dea
Daniel Shin <shin.daniel.d@gmail.com>
2016-10-08 20:50:26 -0600

    developed iteration 4. still need testing

--- db/migrate/20161008183617_create_clients.rb
+++ db/migrate/20161008183617_create_clients.rb
@@ -1,7 +1,7 @@
 class CreateClients < ActiveRecord::Migration
   def change
     create_table :clients do |t|
-      t.text :identifer
+      t.text :identifier
       t.text :root_url
       t.timestamps null: false
     end
