d9cb72b7c9c2941bdc4e9fab9aea108e1ce32824
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-10-08 18:13:24 -0600

    add methods for finding max, min, and average response times for specific urls

--- app/models/url.rb
+++ app/models/url.rb
@@ -9,4 +9,34 @@ class Url < ActiveRecord::Base
     Url.joins(:payloads).group(:url_address).order("COUNT(payloads.*) DESC").pluck(:url_address)
   end
 
+#note that these aren't ``.self` methods (class methods) because they reference a specific url, and not the class Url.
+
+  def max_response_time(url)
+    url.payloads.maximum("responded_in")
+  end
+
+  def min_response_time(url)
+    url.payloads.minimum("responded_in")
+  end
+
+  def average_response_time(url)
+    url.payloads.average("responded_in")
+  end
+
+  def longest_to_shortest_response_time
+
+  end
+
+  def list_of_http_verbs
+
+  end
+
+  def three_most_popular_referrers
+
+  end
+
+  def three_most_popular_user_agents
+
+  end
+
 end
