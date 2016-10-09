4092001758e42eb4521546d11f1844e485fcd5b9
Noah Berman <noah@noahberman.org>
2016-10-09 15:12:56 -0600

    adds basic controller functions on sources

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,4 +5,14 @@ class Client < ActiveRecord::Base
   validates :identifier, presence: true
   validates :root_url, presence: true
   
+  def self.missing_parameters(params)
+    return false if Client.new(params).valid? 
+    return true
+  end
+  
+  def self.identifier_exists(params)
+    return true if Client.pluck(:identifier).include?(params[:identifier])
+    return false
+  end
+  
 end
