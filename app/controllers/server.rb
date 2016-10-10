69f644c83e8c60dc11baa9b1d97b72da4c1a1c8d
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-10-10 14:23:55 -0600

    parsing things and stuff

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -1,27 +1,32 @@
 require_relative '../models/client'
+require_relative '../models/processor'
 
 module RushHour
   class Server < Sinatra::Base
     not_found do
       erb :error
     end
-  
+
     get "/" do
       erb :dashboard
     end
-  
+
     get "/sources" do
       status 200
     end
-    
+
     post "/sources" do
       client_params = Hash.new
       client_params[:identifier] = params["identifier"]
       client_params[:root_url] = params["rootUrl"]
-      status 400 if Client.missing_parameters(client_params)
-      status 403 if Client.identifier_exists(client_params)
-      status 200 unless Client.missing_parameters(client_params) || Client.identifier_exists(client_params)
+      status 400 unless Client.new(params).valid?
+      status 403 if Client.pluck(:identifier).include?(params[:identifier])
     end
-        
+
+    post "/sources/:identifier/data" do |identifier|
+      status 400 unless params.keys.include?("payload")
+      Processor.parse(params[:payload], identifier)
+    end
+
   end
 end
