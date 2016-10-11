d83a188f988e477fe504c8999101740b7592471a
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-10-11 15:38:30 -0600

    fixed 3 most popular referrers

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,6 +2,7 @@ class Url < ActiveRecord::Base
 
   has_many :payloads
   has_many :request_types, through: :payloads
+  has_many :referred_bies, through: :payloads
 
   validates :url, presence: true
 
@@ -36,6 +37,23 @@ class Url < ActiveRecord::Base
       result << object.url.url
       result
     end.uniq
+  end
+
+  def three_most_popular_referrers
+    referred_bies.map { |obj| [obj.referred_by, Payload.pluck(:referred_by_id).count(obj.id)] }.uniq.sort_by { |k| k[1] }.reverse.first(3)
+  end
+
+  def self.three_most_popular_user_agents(input_url)
+    result = Payload.pluck(:agent_id, :url_id).reduce({}) do |r, agent_type|
+      r[Agent.find(agent_type[0]).agent] = (Payload.pluck(:agent_id)).count(agent_type[0]) if Url.find(agent_type[1]).url == input_url
+      r
+    end
+
+    sorted = result.sort_by { |k, v| v }.reverse.first(3)
+
+    sorted.map do |ua|
+      [UserAgent.os(ua.to_s), UserAgent.browser_name(ua.to_s).to_s]
+    end
   end
 
 end
