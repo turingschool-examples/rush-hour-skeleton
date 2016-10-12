2792077b79b2c99e53f037897a6bdf6b4779a567
Dave Junta <djunta@gmail.com>
2016-10-12 15:44:00 -0600

    capy error fix

--- spec/features/user_can_see_own_data_spec.rb
+++ spec/features/user_can_see_own_data_spec.rb
@@ -305,14 +305,12 @@ RSpec.describe "when a user visits /sources/:IDENTIFIER" do
 
   it "they redirect when clicking url" do
 
-    Client.create(identifier: "jumpstartlab", root_url: "http://jumpstartlab")
+    Client.create(identifier: "jumpstartlab", root_url: "http://jumpstartlab.com")
 
-    params ="{\"url\":\"http://jumpstartlab.com/thing\",\"requestedAt\":\"2013-02-16 21:38:28 -0700\",\"respondedIn\":37,\"referredBy\":\"http://jumpstartlab.com/thing\",\"requestType\":\"POST\",\"eventName\": \"socialLogin\",\"userAgent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17\",\"resolutionWidth\":\"1920\",\"resolutionHeight\":\"1280\",\"ip\":\"63.29.38.211\"}"
+    params ="{\"url\":\"http://jumpstartlab.com/thing\",\"requestedAt\":\"2013-02-16 21:38:28 -0700\",\"respondedIn\":37,\"referredBy\":\"http://jumpstartlab.com\",\"requestType\":\"POST\",\"eventName\": \"socialLogin\",\"userAgent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17\",\"resolutionWidth\":\"1920\",\"resolutionHeight\":\"1280\",\"ip\":\"63.29.38.211\"}"
     Processor.parse(params, "jumpstartlab")
-
     visit('/sources/jumpstartlab')
     click_link("http://jumpstartlab.com/thing")
-    save_and_open_page
     expect(page).to have_content("Data for http://jumpstartlab.com/thing")
   end
 
