e17e64a50d699ec28a8d02b02af65b66ddb9bdd5
Daniel Shin <shin.daniel.d@gmail.com>
2016-10-12 23:15:02 -0600

    final testing

--- spec/features/user_visits_identifier_page.rb
+++ spec/features/user_visits_identifier_page.rb
@@ -1,42 +1,74 @@
 require_relative '../spec_helper'
 
 describe "When a user visits '/sources/:IDENTIFIER'" do
-  it "sees respond time on sources page" do
+  it "sees respond time on identifier page" do
     create_single_client_and_payload
     visit("/sources/google")
-    expect(page).to have_content("12")
+    within(".avg-response") do
+      expect(page).to have_content("12")
+    end
   end
 
-  it "sees requested at time on sources page" do
+  it "sees max response time on identifier page" do
     create_single_client_and_payload
     visit("/sources/google")
-    expect(page).to have_content("GET")
+    within(".max-response") do
+      expect(page).to have_content("12")
+    end
   end
 
-  it "sees refered by list on sources page" do
+  it "sees min response time on identifier page" do
     create_single_client_and_payload
     visit("/sources/google")
-    expect(page).to have_content("google.com/images")
+    within(".min-response") do
+      expect(page).to have_content("12")
+    end
   end
 
-  it "sees browser on sources page" do
+  it "sees most frequent request type on identifier page" do
     create_single_client_and_payload
     visit("/sources/google")
-    expect(page).to have_content("Safari")
+    within(".min-response") do
+      expect(page).to have_content("12")
+    end
   end
 
-  it "sees operating system on sources page" do
+  it "sees refered by list on identifier page" do
     create_single_client_and_payload
     visit("/sources/google")
-    expect(page).to have_content("OSX")
+    within(".most-frequent") do
+      expect(page).to have_content("GET")
+    end
   end
 
-  it "sees screen resolution on sources page" do
+  it "sees browser on identifier page" do
     create_single_client_and_payload
     visit("/sources/google")
-    expect(page).to have_content("1200")
-    expect(page).to have_content("1800")
-    expect(page).to have_content("1")
+    within(".browser") do
+      expect(page).to have_content("Safari: 1")
+    end
+  end
+
+  it "sees operating system on identifier page" do
+    create_single_client_and_payload
+    visit("/sources/google")
+    within(".os") do
+      expect(page).to have_content("OSX: 1")
+    end
+  end
+
+  it "sees screen resolution on identifier page" do
+    create_single_client_and_payload
+    visit("/sources/google")
+    within(".res-height") do
+      expect(page).to have_content("1800")
+    end
+    within(".res-width") do
+      expect(page).to have_content("1200")
+    end
+    within(".res-count") do
+      expect(page).to have_content("1")
+    end
   end
 
 end
--- spec/features/user_visits_url_page.rb
+++ spec/features/user_visits_url_page.rb
@@ -1,28 +1,36 @@
 require_relative '../spec_helper'
 
 describe "When a user visits '/sources/:IDENTIFIER/urls/:RELATIVEPATH'" do
-  it "sees response time on relative path page" do
+  it "sees average response time on relative path page" do
     create_multiple_payloads_for_single_client
     visit("/sources/google/urls/images")
-    expect(page).to have_content("20")
+    within(".avg-response") do
+      expect(page).to have_content("20")
+    end
   end
 
   it "sees max response time at time on relative path page" do
     create_multiple_payloads_for_single_client
     visit("/sources/google/urls/images")
-    expect(page).to have_content("30")
+    within(".max-response") do
+      expect(page).to have_content("30")
+    end
   end
 
   it "sees min response time at time on relative path page" do
     create_multiple_payloads_for_single_client
     visit("/sources/google/urls/images")
-    expect(page).to have_content("10")
+    within(".min-response") do
+      expect(page).to have_content("10")
+    end
   end
 
   it "sees list of http verbs" do
     create_multiple_payloads_for_single_client
     visit("/sources/google/urls/images")
-    expect(page).to have_content("GET")
+    within(".all-verbs") do
+      expect(page).to have_content("GET")
+    end
   end
 
   it "sees list of top three referrers" do
@@ -30,10 +38,13 @@ describe "When a user visits '/sources/:IDENTIFIER/urls/:RELATIVEPATH'" do
     visit("/sources/google/urls/images")
     expect(page).to have_content("google.com/images")
   end
+
   it "sees list of top three agents" do
     create_multiple_payloads_for_single_client
     visit("/sources/google/urls/images")
-    expect(page).to have_content("Safari OSX")
+    within(".agents") do
+      expect(page).to have_content("Safari OSX")
+    end
   end
 
 end
